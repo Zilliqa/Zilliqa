db1ff12b2cefbe672eab7797945da972aea568a9
Jun Hao Tan <tanjunhao@outlook.com>
2018-01-04 18:47:59 +0800

    Build fixes and copyright updates (#12)
    * Remove Cmake autogen files
    
    * Change copyright year to reflect 2018

--- src/libData/BlockData/BlockHeader/CMakeFiles/BlockHeader.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libBlockHeader.a"
-)
--- src/libData/BlockData/BlockHeader/DSBlockHeader.cpp
+++ src/libData/BlockData/BlockHeader/DSBlockHeader.cpp
@@ -1,5 +1,5 @@
 /**
-* Copyright (c) 2017 Zilliqa 
+* Copyright (c) 2018 Zilliqa 
 * This source code is being disclosed to you solely for the purpose of your participation in 
 * testing Zilliqa. You may view, compile and run the code for that purpose and pursuant to 
 * the protocols and algorithms that are programmed into, and intended by, the code. You may 
