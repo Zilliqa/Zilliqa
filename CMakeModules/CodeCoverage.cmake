c85e576dcc337118c8130c1a874a9d10e88d49c1
Sam Harwell <sam@tunnelvisionlabs.com>
2018-01-21 17:48:00 -0600

    Update coverage data collection for CI builds

--- CMakeModules/CodeCoverage.cmake
+++ CMakeModules/CodeCoverage.cmake
@@ -155,7 +155,7 @@ FUNCTION(SETUP_TARGET_FOR_COVERAGE _targetname _testrunner _outputname)
 
 		# Capturing lcov counters and generating report
 		COMMAND ${LCOV_PATH} --gcov-tool ${GCOV_PATH} --directory . --capture --output-file ${coverage_info}
-		COMMAND ${LCOV_PATH} --gcov-tool ${GCOV_PATH} --remove ${coverage_info} 'tests/*' '/usr/*' --output-file ${coverage_cleaned}
+		COMMAND ${LCOV_PATH} --gcov-tool ${GCOV_PATH} --remove ${coverage_info} '/usr/*' --output-file ${coverage_cleaned}
 		COMMAND ${GENHTML_PATH} -o ${_outputname} ${coverage_cleaned}
 		COMMAND ${CMAKE_COMMAND} -E remove ${coverage_info} ${coverage_cleaned}
 
