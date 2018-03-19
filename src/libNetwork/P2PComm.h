33561d9077a8c3e80a9f9cd8e86a1acf11d5c266
Gong Deli <gnnnnng@gmail.com>
2018-03-20 04:13:39 +0800

    fix -Wunused-private-field

--- src/libNetwork/P2PComm.h
+++ src/libNetwork/P2PComm.h
@@ -42,7 +42,6 @@ class P2PComm
     const static uint32_t MAXRETRYCONN = 3;
     const static uint32_t MAXPUMPMESSAGE = 128;
     const static uint32_t PUMPMESSAGE_MILLISECONDS = 1000;
-    uint32_t m_counterMessagePump;
 
     void SendMessageCore(const Peer & peer, const std::vector<unsigned char> & message, unsigned char start_byte, const std::vector<unsigned char> & msg_hash);
     bool SendMessageSocketCore(const Peer & peer, const std::vector<unsigned char> & message, unsigned char start_byte, const std::vector<unsigned char> & msg_hash);
