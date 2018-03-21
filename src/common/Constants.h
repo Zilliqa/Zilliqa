ac7ca2d810c152f1775a08c990a55e810cca11ef
Gong Deli <gnnnnng@gmail.com>
2018-03-21 18:25:47 +0800

    Add a separate module for Constants

--- src/common/Constants.h
+++ src/common/Constants.h
@@ -85,24 +85,19 @@ static unsigned int ReadFromConstantsFile(std::string propertyName)
     return pt.get<unsigned int>("node.constants." + propertyName);
 }
 
-const unsigned int DS_MULTICAST_CLUSTER_SIZE(ReadFromConstantsFile("DS_MULTICAST_CLUSTER_SIZE"));
-const unsigned int COMM_SIZE(ReadFromConstantsFile("COMM_SIZE"));
-static const unsigned int MAX_POW1_WINNERS(ReadFromConstantsFile("MAX_POW1_WINNERS"));
-static const unsigned int POW1_WINDOW_IN_SECONDS(ReadFromConstantsFile("POW1_WINDOW_IN_SECONDS"));
-static const unsigned int POW1_BACKUP_WINDOW_IN_SECONDS(
-	ReadFromConstantsFile("POW1_BACKUP_WINDOW_IN_SECONDS"));
-static const unsigned int LEADER_SHARDING_PREPARATION_IN_SECONDS(
-	ReadFromConstantsFile("LEADER_SHARDING_PREPARATION_IN_SECONDS"));
-static const unsigned int LEADER_POW2_WINDOW_IN_SECONDS(
-	ReadFromConstantsFile("LEADER_POW2_WINDOW_IN_SECONDS"));
-static const unsigned int BACKUP_POW2_WINDOW_IN_SECONDS(
-	ReadFromConstantsFile("BACKUP_POW2_WINDOW_IN_SECONDS"));
-static const unsigned int NEW_NODE_POW2_TIMEOUT_IN_SECONDS(
-	ReadFromConstantsFile("NEW_NODE_POW2_TIMEOUT_IN_SECONDS"));
-static const unsigned int POW_SUB_BUFFER_TIME(ReadFromConstantsFile("POW_SUB_BUFFER_TIME")); //milliseconds
-static const unsigned int POW1_DIFFICULTY(ReadFromConstantsFile("POW1_DIFFICULTY"));
-static const unsigned int POW2_DIFFICULTY(ReadFromConstantsFile("POW2_DIFFICULTY"));
-static const unsigned int NUM_FINAL_BLOCK_PER_POW(ReadFromConstantsFile("NUM_FINAL_BLOCK_PER_POW"));
-static const uint32_t MAXMESSAGE(ReadFromConstantsFile("MAXMESSAGE"));
+extern const unsigned int DS_MULTICAST_CLUSTER_SIZE;
+extern const unsigned int COMM_SIZE;
+extern const unsigned int MAX_POW1_WINNERS;
+extern const unsigned int POW1_WINDOW_IN_SECONDS;
+extern const unsigned int POW1_BACKUP_WINDOW_IN_SECONDS;
+extern const unsigned int LEADER_SHARDING_PREPARATION_IN_SECONDS;
+extern const unsigned int LEADER_POW2_WINDOW_IN_SECONDS;
+extern const unsigned int BACKUP_POW2_WINDOW_IN_SECONDS;
+extern const unsigned int NEW_NODE_POW2_TIMEOUT_IN_SECONDS;
+extern const unsigned int POW_SUB_BUFFER_TIME;
+extern const unsigned int POW1_DIFFICULTY;
+extern const unsigned int POW2_DIFFICULTY;
+extern const unsigned int NUM_FINAL_BLOCK_PER_POW;
+extern const uint32_t MAXMESSAGE;
 
 #endif // __CONSTANTS_H__
