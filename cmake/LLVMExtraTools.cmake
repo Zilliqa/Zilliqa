3c89d2753ee03ee73853db0aef14bc7b3716c28d
Gong Deli <gnnnnng@gmail.com>
2018-03-24 16:09:24 +0800

    update cmake/LLVMExtraTools.cmake

--- cmake/LLVMExtraTools.cmake
+++ cmake/LLVMExtraTools.cmake
@@ -1,17 +1,24 @@
 # Additional targets to perform clang-format/clang-tidy
 # It requires clang-format/clang-tidy 5.0.0+
 
+set(BREW_LLVM5_PATH /usr/local/opt/llvm@5/bin)
+
 # Get all project files
-file(GLOB_RECURSE ALL_CXX_SOURCES *.cpp *.h)
+file(GLOB_RECURSE ALL_CXX_SOURCES ${CMAKE_SOURCE_DIR}/src/*.cpp ${CMAKE_SOURCE_DIR}/tests/*.h)
 
 # Exclude third-party libraries in src/depends
 list(FILTER ALL_CXX_SOURCES EXCLUDE REGEX "^.*src/depends.*$")
 
-# Adding clang-format target if executable is found
+####################### clang-format ##############################
+
+if(NOT EXISTS ${CMAKE_SOURCE_DIR}/.clang-format)
+    message(AUTHOR_WARNING ".clang-format is missing in the project root")
+endif()
+
 find_program(
     CLANG_FORMAT
     NAMES clang-format-5.0 clang-format
-    PATHS /usr/local/opt/llvm@5/bin # The brew version on MacOS
+    PATHS ${BREW_LLVM5_PATH} # The brew version on MacOS
 )
 
 if(CLANG_FORMAT)
@@ -30,7 +37,6 @@ if(CLANG_FORMAT)
             clang-format
             COMMAND ${CLANG_FORMAT}
             -i
-            -sort-includes
             -style=file
             ${ALL_CXX_SOURCES}
         )
@@ -39,11 +45,22 @@ if(CLANG_FORMAT)
     endif()
 endif()
 
-# Adding clang-tidy target if executable is found
+##################### clang-tidy #############################
+
+if(NOT EXISTS ${CMAKE_SOURCE_DIR}/.clang-tidy)
+    message(AUTHOR_WARNING ".clang-tidy is missing in the project root")
+endif()
+
 find_program(
     CLANG_TIDY
     NAMES clang-tidy-5.0 clang-tidy
-    PATHS /usr/local/opt/llvm@5/bin # The brew version on MacOS
+    PATHS ${BREW_LLVM5_PATH} # The brew version on MacOS
+)
+
+find_program(
+    CLANG_APPLY_REPLACEMENTS
+    NAMES clang-apply-replacements-5.0 clang-apply-replacements
+    PATHS ${BREW_LLVM5_PATH} # The brew version on MacOS
 )
 
 find_program(
@@ -67,10 +84,22 @@ if(CLANG_TIDY)
         add_custom_target(
             clang-tidy
             COMMAND "${RUN_CLANG_TIDY}"
+            -clang-tidy-binary ${CLANG_TIDY}
             -config=''
-            -format-style='file'
+            -style='file'
             ${ALL_CXX_SOURCES}
         )
+        if(CLANG_APPLY_REPLACEMENTS)
+            add_custom_target(
+                clang-tidy-apply
+                COMMAND "${RUN_CLANG_TIDY}"
+                -clang-tidy-binary ${CLANG_TIDY}
+                -clang-apply-replacements-binary ${CLANG_APPLY_REPLACEMENTS}
+                -config=''
+                -style='file'
+                ${ALL_CXX_SOURCES}
+            )
+        endif()
     else()
         message(AUTHOR_WARNING "clang-tidy version (${CLANG_TIDY_VERSION}) not satisify (>5.0.0)")
     endif()
