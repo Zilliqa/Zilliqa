c7ab8d9248dee6141321d08ee6619f6f87ee0be5
Gong Deli <gnnnnng@gmail.com>
2018-03-24 12:03:13 +0800

    add cmake module about LLVM dev tools

---
+++ cmake/LLVMExtraTools.cmake
@@ -0,0 +1,77 @@
+# Additional targets to perform clang-format/clang-tidy
+# It requires clang-format/clang-tidy 5.0.0+
+
+# Get all project files
+file(GLOB_RECURSE
+     ALL_CXX_SOURCE_FILES
+     src
+     *.[chi]pp *.[chi]xx *.cc *.hh *.ii *.[CHI]
+     )
+
+# Adding clang-format target if executable is found
+find_program(
+    CLANG_FORMAT
+    NAMES clang-format-5.0 clang-format
+    PATHS /usr/local/opt/llvm@5/bin # The brew version on MacOS
+)
+
+if(CLANG_FORMAT)
+    execute_process(
+        COMMAND "${CLANG_FORMAT}" --version
+        OUTPUT_VARIABLE CLANG_FORMAT_VERSION_OUTPUT
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        RESULT_VARIABLE _CLANG_FORMAT_VERSION_RESULT
+    )
+
+    string(REGEX REPLACE "^.*version ([^ ]*) .*$" "\\1" CLANG_FORMAT_VERSION ${CLANG_FORMAT_VERSION_OUTPUT})
+
+    if("${CLANG_FORMAT_VERSION}" VERSION_GREATER "5.0.0")
+        # message(${CLANG_FORMAT_VERSION})
+        add_custom_target(
+            clang-format
+            COMMAND ${CLANG_FORMAT}
+            -i
+            -style=file
+            ${ALL_CXX_SOURCE_FILES}
+        )
+    else()
+        message(AUTHOR_WARNING "clang-format version (${CLANG_FORMAT_VERSION}) not satisify (>5.0.0)")
+    endif()
+endif()
+
+# Adding clang-tidy target if executable is found
+find_program(
+    CLANG_TIDY
+    NAMES clang-tidy-5.0 clang-tidy
+    PATHS /usr/local/opt/llvm@5/bin # The brew version on MacOS
+)
+
+find_program(
+    RUN_CLANG_TIDY
+    NAMES run-clang-tidy.py
+    PATHS "${CMAKE_SOURCE_DIR}/scripts/"
+)
+
+if(CLANG_TIDY)
+    execute_process(
+        COMMAND "${CLANG_TIDY}" --version
+        OUTPUT_VARIABLE CLANG_TIDY_VERSION_OUTPUT
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        RESULT_VARIABLE _CLANG_TIDY_VERSION_RESULT
+    )
+
+    string(REGEX REPLACE "^.*version ([^ ]*).*$" "\\1" CLANG_TIDY_VERSION ${CLANG_TIDY_VERSION_OUTPUT})
+
+    if("${CLANG_TIDY_VERSION}" VERSION_GREATER "5.0.0")
+        # message(${CLANG_TIDY_VERSION})
+        add_custom_target(
+            clang-tidy
+            COMMAND "${RUN_CLANG_TIDY}"
+            -config=''
+            -format-style='file'
+            ${ALL_CXX_SOURCE_FILES}
+        )
+    else()
+        message(AUTHOR_WARNING "clang-tidy version (${CLANG_TIDY_VERSION}) not satisify (>5.0.0)")
+    endif()
+endif()
