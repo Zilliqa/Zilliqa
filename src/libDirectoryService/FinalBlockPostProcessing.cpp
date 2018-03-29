fdb76dae5122a3f3b723b304d05eb0eff6094bfe
bb111189 <tanjunhao@outlook.com>
2018-03-29 20:57:57 +0800

    More ds members to send dsblock, sharding structure and final block to lookup nodes

--- src/libDirectoryService/FinalBlockPostProcessing.cpp
+++ src/libDirectoryService/FinalBlockPostProcessing.cpp
@@ -282,13 +282,17 @@ void DirectoryService::ProcessFinalBlockConsensusWhenDone()
     m_mediator.UpdateDSBlockRand();
     m_mediator.UpdateTxBlockRand();
 
-    LOG_MESSAGE2(to_string(m_mediator.m_currentEpochNum).c_str(),
-                 "Final Block to be sent to the lookup nodes");
-    if (m_mode == PRIMARY_DS)
+    // TODO: Refine this
+    unsigned int nodeToSendToLookUpLo = COMM_SIZE / 2;
+    unsigned int nodeToSendToLookUpHi = COMM_SIZE + TX_SHARING_CLUSTER_SIZE;
+
+    if (m_consensusMyID > nodeToSendToLookUpLo
+        && m_consensusMyID < nodeToSendToLookUpHi)
     {
-        LOG_MESSAGE2(to_string(m_mediator.m_currentEpochNum).c_str(),
-                     "I the primary DS will soon be sending the Final Block to "
-                     "the lookup nodes");
+        LOG_MESSAGE2(
+            to_string(m_mediator.m_currentEpochNum).c_str(),
+            "I the DS folks that will soon be sending the Final Block to "
+            "the lookup nodes");
         SendFinalBlockToLookupNodes();
     }
 
