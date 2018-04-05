90748600c0bce92a1026b9b3f0e63422c79605c1
kaikawaliu <liuhaichuan@Hotmail.sg>
2018-04-05 14:31:07 +0800

    add space

--- tests/Zilliqa/test_zilliqa_lookup_local_recovery.py
+++ tests/Zilliqa/test_zilliqa_lookup_local_recovery.py
@@ -126,7 +126,7 @@ def run_start():
 	for x in range(0, count):
 		keypair = keypairs[x].split(" ")
 		abs_binary_path = os.path.abspath("lzilliqa")
-		os.system('cd ' + LOCAL_RUN_FOLDER + testfolders_list[x] + '; echo \"' + keypair[0] + ' ' + keypair[1] + '\" > mykey.txt' + '; ulimit -n 65535; ulimit -Sc unlimited; ulimit -Hc unlimited;'+ abs_binary_path + keypair[1] + ' ' + keypair[0] + ' ' + '127.0.0.1' +' ' + str(NODE_LISTEN_PORT + x) + ' 0 0 1 > ./error_log_zilliqa 2>&1 &')
+		os.system('cd ' + LOCAL_RUN_FOLDER + testfolders_list[x] + '; echo \"' + keypair[0] + ' ' + keypair[1] + '\" > mykey.txt' + '; ulimit -n 65535; ulimit -Sc unlimited; ulimit -Hc unlimited;'+ abs_binary_path + ' ' + keypair[1] + ' ' + keypair[0] + ' ' + '127.0.0.1' +' ' + str(NODE_LISTEN_PORT + x) + ' 0 0 1 > ./error_log_zilliqa 2>&1 &')
 
 if __name__ == "__main__":
 	main()
