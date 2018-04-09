601d4c9693f1ff66f92df9816b4e2d72d7c0550f
Gong Deli <gnnnnng@gmail.com>
2018-04-09 12:58:52 +0000

    libServer: lookup node shares to one normal node

--- src/libServer/Server.cpp
+++ src/libServer/Server.cpp
@@ -123,7 +123,7 @@ string Server::CreateTransaction(const Json::Value& _json)
 
             auto it = shardMembers.begin();
             for (unsigned int i = 0;
-                 i < NUM_PEERS_TO_SEND_IN_A_SHARD && it != shardMembers.end();
+                 i < 1 && it != shardMembers.end();
                  i++, it++)
             {
                 toSend.push_back(it->second);
