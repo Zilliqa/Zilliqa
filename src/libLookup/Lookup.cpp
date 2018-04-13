5bf785d685d81908531173117218d97ae2b58f26
kaikawaliu <liuhaichuan@Hotmail.sg>
2018-04-13 22:24:42 +0800

    Refine logic

--- src/libLookup/Lookup.cpp
+++ src/libLookup/Lookup.cpp
@@ -1577,6 +1577,12 @@ bool Lookup::InitMining()
 {
     LOG_MARKER();
 
+    if (m_startedPoW2)
+    {
+        LOG_MESSAGE("Already submitting PoW2");
+        return false;
+    }
+
     m_mediator.m_currentEpochNum
         = (uint64_t)m_mediator.m_txBlockChain.GetBlockCount();
 
@@ -1618,6 +1624,7 @@ bool Lookup::InitMining()
         {
             // DS block has been generated.
             // Attempt PoW2
+            m_startedPoW2 = true;
             m_mediator.UpdateDSBlockRand();
             dsBlockRand = m_mediator.m_dsBlockRand;
             txBlockRand = {};
@@ -1652,7 +1659,6 @@ bool Lookup::InitMining()
     {
         LOG_MESSAGE2(to_string(m_mediator.m_currentEpochNum).c_str(),
                      "I have successfully join the network");
-#ifndef IS_LOOKUP_NODE
         LOG_MESSAGE("Clean TxBodyDB except the last one");
         int size_txBodyDBs
             = (int)BlockStorage::GetBlockStorage().GetTxBodyDBSize();
@@ -1660,8 +1666,8 @@ bool Lookup::InitMining()
         {
             BlockStorage::GetBlockStorage().PopFrontTxBodyDB(true);
         }
-#endif // IS_LOOKUP_NODE
     }
+    m_startedPoW2 = false;
 
     return true;
 }
