4ba5a555aa8d9bb3fab64104d8b205519ed10e6e
Gong Deli <gnnnnng@gmail.com>
2018-04-14 19:47:51 +0000

    cmake: better integration with travis

---
+++ scripts/ci_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# The script is supposed to be run in project root directory
+
+if [ "$1" = "lookup" ]
+then
+    CMAKE_EXTRA_OPTIONS="-DIS_LOOKUP_NODE=1"
+fi
+
+mkdir build && cd build
+cmake ${CMAKE_EXTRA_OPTIONS} -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+make -j$(nproc)
+make clang-format
+
