008f00bd6acf50f430f5b169b784907901aac677
Gong Deli <gnnnnng@gmail.com>
2018-04-15 05:56:16 +0000

    travis: enable ctest

--- scripts/ci_build.sh
+++ scripts/ci_build.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+#
+# This script is dedicated for CI use
+#
 
 set -e
-
-# The script is supposed to be run in project root directory
 
 if [ "$1" = "lookup" ]
 then
     CMAKE_EXTRA_OPTIONS="-DIS_LOOKUP_NODE=1"
 fi
 
+# assume that it is run from project root directory
 mkdir build && cd build
 cmake ${CMAKE_EXTRA_OPTIONS} -DCMAKE_BUILD_TYPE=RelWithDebInfo -DTESTS=ON ..
 make -j$(nproc)
 make clang-format
+ctest --output-on-failure
 
