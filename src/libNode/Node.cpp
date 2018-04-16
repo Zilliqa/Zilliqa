7ac82ae91387d4d44c3cd574978ace317f0fdef3
kaikawaliu <liuhaichuan@Hotmail.sg>
2018-04-16 19:42:37 +0800

    Remove clean-up of createdTxns and TxnNonce

--- src/libNode/Node.cpp
+++ src/libNode/Node.cpp
@@ -1249,14 +1249,14 @@ bool Node::CleanVariables()
         std::lock_guard<mutex> lock(m_mutexMicroBlock);
         m_microblock.reset();
     }
-    {
-        std::lock_guard<mutex> lock(m_mutexCreatedTransactions);
-        m_createdTransactions.clear();
-    }
-    {
-        std::lock_guard<mutex> lock(m_mutexTxnNonceMap);
-        m_txnNonceMap.clear();
-    }
+    // {
+    //     std::lock_guard<mutex> lock(m_mutexCreatedTransactions);
+    //     m_createdTransactions.clear();
+    // }
+    // {
+    //     std::lock_guard<mutex> lock(m_mutexTxnNonceMap);
+    //     m_txnNonceMap.clear();
+    // }
     // {
     //     std::lock_guard<mutex> lock(m_mutexPrefilledTxns);
     //     m_nRemainingPrefilledTxns = 0;
