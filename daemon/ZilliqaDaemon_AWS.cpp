665e1e361756ca4829951a021b2e1619b98ec29c
Kaustubh <iit2014147@iiita.ac.in>
2018-04-24 09:34:12 +0530

    format fix

--- daemon/ZilliqaDaemon_AWS.cpp
+++ daemon/ZilliqaDaemon_AWS.cpp
@@ -1,37 +1,35 @@
-#include <sys/types.h>
 #include <algorithm>
+#include <array>
+#include <cstdio>
 #include <dirent.h>
 #include <errno.h>
-#include <vector>
-#include <string>
-#include <iostream>
 #include <fstream>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <cstdio>
-#include <unordered_map>
-#include <unistd.h>
+#include <iostream>
 #include <memory>
-#include <stdexcept>
-#include <array>
 #include <signal.h>
-
+#include <stdexcept>
+#include <stdlib.h>
+#include <string>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
-
-const vector<string> programName={"zilliqa"};
+const vector<string> programName = {"zilliqa"};
 
 const string restart_zilliqa = "python tests/Zilliqa/daemon_restart.py";
 
-unordered_map <int, string> PrivKey;
-unordered_map <int, string> PubKey;
-unordered_map <int, string> Port;
-unordered_map <int, string> Path;
+unordered_map<int, string> PrivKey;
+unordered_map<int, string> PubKey;
+unordered_map<int, string> Port;
+unordered_map<int, string> Path;
 
 #ifndef IS_LOOKUP_NODE
-	unordered_map <int, bool> isDS;
+unordered_map<int, bool> isDS;
 #endif //IS_LOOKUP_NODE
 
 const string logName = "epochinfo-00001-log.txt";
@@ -45,35 +43,34 @@ enum SyncType : unsigned int
     LOOKUP_SYNC,
 };
 
-string ReadLastLine(string filePath, ofstream & log)
+string ReadLastLine(string filePath, ofstream& log)
 {
     ifstream logFile;
 
-    logFile.open(filePath+"/"+logName);
+    logFile.open(filePath + "/" + logName);
 
-    if(logFile.is_open())
+    if (logFile.is_open())
     {
-        logFile.seekg(-1,ios_base::end);
+        logFile.seekg(-1, ios_base::end);
     }
     else
     {
         return "";
     }
-    
 
     bool keepLooping = true;
 
-    while(keepLooping)
+    while (keepLooping)
     {
         char ch;
         logFile.get(ch);
 
-        if((int)logFile.tellg() <= 1)
+        if ((int)logFile.tellg() <= 1)
         {
             logFile.seekg(0);
             keepLooping = false;
         }
-        else if(ch == ']' )
+        else if (ch == ']')
         {
             keepLooping = false;
         }
@@ -89,16 +86,14 @@ string ReadLastLine(string filePath, ofstream & log)
     logFile.close();
 
     return lastLine;
-
 }
-
 
 SyncType getRestartValue(pid_t pid)
 {
 
 #ifndef IS_LOOKUP_NODE
-    
-    if(isDS[pid])
+
+    if (isDS[pid])
     {
         return DS_SYNC;
     }
@@ -114,17 +109,15 @@ SyncType getRestartValue(pid_t pid)
 #endif //IS_LOOKUP_NODE
 }
 
-
 #ifndef IS_LOOKUP_NODE
 
 void setIsDs(const pid_t& pid, ofstream& log)
 {
     string s = ReadLastLine(Path[pid], log);
 
-    if(s == " PROMOTED TO DS")
+    if (s == " PROMOTED TO DS")
     {
         isDS[pid] = true;
-
     }
     else
     {
@@ -134,18 +127,17 @@ void setIsDs(const pid_t& pid, ofstream& log)
 
 #endif //IS_LOOKUP_NODE
 
-
-vector<pid_t> getProcIdByName(string procName, ofstream & log)
+vector<pid_t> getProcIdByName(string procName, ofstream& log)
 {
-    vector <pid_t> result;
+    vector<pid_t> result;
     result.clear();
 
     // Open the /proc directory
-    DIR *dp = opendir("/proc");
+    DIR* dp = opendir("/proc");
     if (dp != NULL)
     {
         // Enumerate all entries in directory until process found
-        struct dirent *dirp;
+        struct dirent* dirp;
         while ((dirp = readdir(dp)))
         {
             // Skip non-numeric entries
@@ -169,7 +161,7 @@ vector<pid_t> getProcIdByName(string procName, ofstream & log)
                     string path = "";
                     if (pos != string::npos)
                     {
-                        path = fullLine.substr(0,pos);
+                        path = fullLine.substr(0, pos);
                         cmdLine = cmdLine.substr(pos + 1);
                         fullLine = fullLine.substr(pos + 1);
                     }
@@ -180,32 +172,30 @@ vector<pid_t> getProcIdByName(string procName, ofstream & log)
                         size_t pubkey_pos = fullLine.find('\0');
                         fullLine = fullLine.substr(pubkey_pos + 1);
                         size_t privkey_pos = fullLine.find('\0');
-                        string publicKey = fullLine.substr(0,privkey_pos);
+                        string publicKey = fullLine.substr(0, privkey_pos);
                         PubKey[id] = publicKey;
 
-                        fullLine = fullLine.substr(privkey_pos+1);
+                        fullLine = fullLine.substr(privkey_pos + 1);
                         size_t privkey_pos_end = fullLine.find('\0');
                         string privKey = fullLine.substr(0, privkey_pos_end);
 
                         PrivKey[id] = privKey;
 
-                        fullLine = fullLine.substr(privkey_pos_end+1);
+                        fullLine = fullLine.substr(privkey_pos_end + 1);
                         size_t ip_end = fullLine.find('\0');
-                        string ip = fullLine.substr(0,ip_end);
+                        string ip = fullLine.substr(0, ip_end);
 
-                        fullLine = fullLine.substr(ip_end+1);
+                        fullLine = fullLine.substr(ip_end + 1);
                         size_t port_end = fullLine.find('\0');
-                        string port = fullLine.substr(0,port_end);
+                        string port = fullLine.substr(0, port_end);
                         Port[id] = port;
 
                         Path[id] = path;
-                        log<<" id: "<<id<<" Path: "<<path<<endl;
+                        log << " id: " << id << " Path: " << path << endl;
 
 #ifndef IS_LOOKUP_NODE
                         setIsDs(id, log);
 #endif
-
-
                     }
                 }
             }
@@ -216,129 +206,137 @@ vector<pid_t> getProcIdByName(string procName, ofstream & log)
 
     return result;
 }
-string execute(string cmd) {
+string execute(string cmd)
+{
 
     array<char, 128> buffer;
     string result;
     shared_ptr<FILE> pipe(popen(cmd.c_str(), "r"), pclose);
-    if (!pipe) throw std::runtime_error("popen() failed!");
-    while (!feof(pipe.get())) 
+    if (!pipe)
+        throw std::runtime_error("popen() failed!");
+    while (!feof(pipe.get()))
     {
         if (fgets(buffer.data(), 128, pipe.get()) != nullptr)
             result += buffer.data();
     }
     return result;
-    
 }
 
-
-
-void initialize(unordered_map <string, vector<pid_t> > & pids, unordered_map <pid_t ,bool> & died, ofstream & log)
+void initialize(unordered_map<string, vector<pid_t>>& pids,
+                unordered_map<pid_t, bool>& died, ofstream& log)
 {
 
     bool isProcesstoTrack = false;
-    for(auto v: programName)
+    for (auto v : programName)
     {
         vector<pid_t> tmp = getProcIdByName(v, log);
-        if(tmp.size() > 0)
+        if (tmp.size() > 0)
         {
             isProcesstoTrack = true;
             pids[v] = tmp;
-            log<<"Process "<<v<<" exists in "<<pids[v].size()<<" instances"<<endl;
-            log<<"Pids: ";
-            for(auto i : pids[v])
+            log << "Process " << v << " exists in " << pids[v].size()
+                << " instances" << endl;
+            log << "Pids: ";
+            for (auto i : pids[v])
             {
-                log<<i<<" ";
+                log << i << " ";
                 died[i] = false;
             }
-            log<<endl;
-
+            log << endl;
         }
         else
         {
-            log<<"Process "<<v<<" does not exist"<<endl;
+            log << "Process " << v << " does not exist" << endl;
             //What to do??
         }
     }
-    if(!isProcesstoTrack)
+    if (!isProcesstoTrack)
     {
-        log<<"No Process to Track\n"<<" Exiting ..."<<endl;
+        log << "No Process to Track\n"
+            << " Exiting ..." << endl;
         exit(EXIT_SUCCESS);
     }
-    
 }
 
-
-void MonitorProcess(unordered_map <string, vector<pid_t> > & pids, unordered_map <pid_t, bool> & died, ofstream & log)
+void MonitorProcess(unordered_map<string, vector<pid_t>>& pids,
+                    unordered_map<pid_t, bool>& died, ofstream& log)
 {
 
-    for(string name : programName )
+    for (string name : programName)
     {
-        for(pid_t pid : pids[name])
+        for (pid_t pid : pids[name])
         {
 
 #ifndef IS_LOOKUP_NODE
-           setIsDs(pid, log);
+            setIsDs(pid, log);
 #endif //IS_LOOKUP_NODE
 
             int w = kill(pid, 0);
 
-            if( w < 0)
+            if (w < 0)
             {
-                if(errno == EPERM)
+                if (errno == EPERM)
                 {
-                    log<<"Daemon does not have permission "<<"Name: "<<name<<" Id: "<<pid<<endl;
+                    log << "Daemon does not have permission "
+                        << "Name: " << name << " Id: " << pid << endl;
                 }
-                else if(errno == ESRCH)
+                else if (errno == ESRCH)
                 {
-                    log<<"Process died "<<"Name: "<<name<<" Id: "<<pid<<endl;
+                    log << "Process died "
+                        << "Name: " << name << " Id: " << pid << endl;
                     died[pid] = true;
                 }
                 else
                 {
-                    log<<"Kill failed due to "<<errno<<" Name: "<<name<<"Id: "<<pid<<endl;
+                    log << "Kill failed due to " << errno << " Name: " << name
+                        << "Id: " << pid << endl;
                 }
-
-                
             }
 
-            if(died[pid])
+            if (died[pid])
             {
-            	unsigned int v = getRestartValue(pid);
-            	
+                unsigned int v = getRestartValue(pid);
 
-               
-                
                 auto it = find(pids[name].begin(), pids[name].end(), pid);
-                if(it != pids[name].end())
+                if (it != pids[name].end())
                 {
-                    log<<"Not monitoring "<<pid<<" of "<<name<<endl;
+                    log << "Not monitoring " << pid << " of " << name << endl;
                     pids[name].erase(it);
                 }
                 try
                 {
-                    
-                    log<<"Trying to restart .."<<endl;
-                    log<<"Params: "<<PubKey[pid]+" "+PrivKey[pid]+" "+Port[pid]+" "+to_string(v)+" "+Path[pid]+" 2>&1"<<endl;
-                    log<<"Output from shell...(if any)"<<endl<<endl;
-                    log<<"\" "<<execute(restart_zilliqa+" "+PubKey[pid]+" "+PrivKey[pid]+" "+Port[pid]+" "+to_string(v)+" "+Path[pid]+" 2>&1")<<" \""<<endl;
-                    log<<PubKey[pid]<<" "<<PrivKey[pid]<<" "<<Port[pid]<<endl;
-                    log<<endl;
-                }
-                catch(runtime_error & e)
-                {
-                    log<<"ERROR!! "<<e.what()<<endl;
-                }
-                vector <pid_t> tmp = getProcIdByName(programName[0], log);
 
-                for(pid_t i : tmp)
+                    log << "Trying to restart .." << endl;
+                    log << "Params: "
+                        << PubKey[pid] + " " + PrivKey[pid] + " " + Port[pid]
+                            + " " + to_string(v) + " " + Path[pid] + " 2>&1"
+                        << endl;
+                    log << "Output from shell...(if any)" << endl << endl;
+                    log << "\" "
+                        << execute(restart_zilliqa + " " + PubKey[pid] + " "
+                                   + PrivKey[pid] + " " + Port[pid] + " "
+                                   + to_string(v) + " " + Path[pid] + " 2>&1")
+                        << " \"" << endl;
+                    log << PubKey[pid] << " " << PrivKey[pid] << " "
+                        << Port[pid] << endl;
+                    log << endl;
+                }
+                catch (runtime_error& e)
                 {
-                    auto it = find(pids[programName[0]].begin(), pids[programName[0]].end(), i);
-                    if(it == pids[programName[0]].end())
+                    log << "ERROR!! " << e.what() << endl;
+                }
+                vector<pid_t> tmp = getProcIdByName(programName[0], log);
+
+                for (pid_t i : tmp)
+                {
+                    auto it = find(pids[programName[0]].begin(),
+                                   pids[programName[0]].end(), i);
+                    if (it == pids[programName[0]].end())
                     {
                         died[i] = false;
                         pids[programName[0]].push_back(i);
-                        log<<"Started new process "<<programName[0]<<" with PiD: "<<i<<endl;  
+                        log << "Started new process " << programName[0]
+                            << " with PiD: " << i << endl;
                     }
                 }
                 died.erase(pid);
@@ -352,27 +350,26 @@ void MonitorProcess(unordered_map <string, vector<pid_t> > & pids, unordered_map
             }
         }
     }
-
 }
 
 int main()
 {
-    
+
     pid_t pid_parent, sid;
     ofstream log;
-    log.open("daemon-log.txt", fstream::out | fstream::trunc );
-       
+    log.open("daemon-log.txt", fstream::out | fstream::trunc);
+
     pid_parent = fork();
 
-    if( pid_parent < 0 )
+    if (pid_parent < 0)
     {
-        log<<"Failed to fork "<<endl;
+        log << "Failed to fork " << endl;
         exit(EXIT_FAILURE);
     }
 
-    if(pid_parent > 0)
+    if (pid_parent > 0)
     {
-        log<<"Started daemon successfully"<<endl;
+        log << "Started daemon successfully" << endl;
         exit(EXIT_SUCCESS);
     }
 
@@ -380,15 +377,15 @@ int main()
 
     sid = setsid();
 
-    if(sid < 0)
+    if (sid < 0)
     {
-        log<<"Unable to set sid"<<endl;
+        log << "Unable to set sid" << endl;
         exit(EXIT_FAILURE);
     }
 
-    if((chdir("..")) < 0 )
+    if ((chdir("..")) < 0)
     {
-        log<<"Failed to chdir"<<endl;
+        log << "Failed to chdir" << endl;
         exit(EXIT_FAILURE);
     }
 
@@ -396,13 +393,12 @@ int main()
     close(STDOUT_FILENO);
     close(STDERR_FILENO);
 
-
-    unordered_map <string, vector<pid_t> > pids;
-    unordered_map <pid_t ,bool> died;
+    unordered_map<string, vector<pid_t>> pids;
+    unordered_map<pid_t, bool> died;
 
     initialize(pids, died, log);
-    
-    while(1)
+
+    while (1)
     {
         MonitorProcess(pids, died, log);
         sleep(5);
