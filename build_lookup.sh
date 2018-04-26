dec47136aae5a8edccc325f9c95ebbe1ce11a3d7
Yaoqi <yaoqi@zilliqa.com>
2018-04-26 17:14:57 +0800

    Script: Removed redundant commands

--- build_lookup.sh
+++ build_lookup.sh
@@ -16,7 +16,6 @@
 git submodule update --init --recursive --remote
 mkdir -p build_lookup
 cd build_lookup
-find . -name "CMakeCache.txt" -delete
 cmake -DIS_LOOKUP_NODE=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DTESTS=ON ..
 make -j4
 make clang-format-fix
