5ad80e5a65a65c1f19f60d44b547b0ebc699f3e8
Clark Yang <clark@zilliqa.com>
2018-05-04 15:04:48 +0800

    Replace magic number

--- src/common/Constants.cpp
+++ src/common/Constants.cpp
@@ -85,6 +85,9 @@ const unsigned int POW2_DIFFICULTY{ReadFromConstantsFile("POW2_DIFFICULTY")};
 const unsigned int MICROBLOCK_TIMEOUT{
     ReadFromConstantsFile("MICROBLOCK_TIMEOUT")};
 const unsigned int VIEWCHANGE_TIME{ReadFromConstantsFile("VIEWCHANGE_TIME")};
+const unsigned int SHARDING_TIMEOUT{ReadFromConstantsFile("SHARDING_TIMEOUT")};
+const unsigned int CONSENSUS_OBJECT_TIMEOUT{
+    ReadFromConstantsFile("CONSENSUS_OBJECT_TIMEOUT")};
 const unsigned int NUM_FINAL_BLOCK_PER_POW{
     ReadFromConstantsFile("NUM_FINAL_BLOCK_PER_POW")};
 const unsigned int NUM_DS_KEEP_TX_BODY{
