9d5ec2b64e7f7c940e2a4de33901eaacb8f1371f
Kaustubh <iit2014147@iiita.ac.in>
2018-05-23 04:17:39 +0530

    Decrease nonce before

--- src/libServer/Server.cpp
+++ src/libServer/Server.cpp
@@ -440,6 +440,7 @@ Json::Value Server::GetSmartContracts(const string& address)
         do
         {
             Address contractAddr = Account::GetAddressForContract(addr, nonce);
+            nonce--;
             const Account* contractAccount
                 = AccountStore::GetInstance().GetAccount(contractAddr);
             if (contractAccount != nullptr)
@@ -453,7 +454,6 @@ Json::Value Server::GetSmartContracts(const string& address)
                 tmpJson["state"] = contractAccount->GetStorageJson();
                 _json.append(tmpJson);
             }
-            nonce--;
         } while (nonce > 0);
         return _json;
     }
