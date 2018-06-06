4d2edc908edef34d93027e326606ba29c71aeafe
bb111189 <junhao@zilliqa.com>
2018-06-06 20:48:15 +0800

    Added post view change buffer and fix view change for ds block

--- src/common/Constants.h
+++ src/common/Constants.h
@@ -123,8 +123,8 @@ extern const uint32_t MAXMESSAGE;
 extern const unsigned int MAXSUBMITTXNPERNODE;
 extern const unsigned int TX_SHARING_CLUSTER_SIZE;
 extern const unsigned int NEW_NODE_POW_DELAY;
-
 extern const unsigned int N_PREFILLED_PER_ACCOUNT;
+extern const unsigned int POST_VIEWCHANGE_BUFFER;
 extern const std::vector<std::string> GENESIS_WALLETS;
 extern const std::vector<std::string> GENESIS_KEYS;
 #endif // __CONSTANTS_H__
