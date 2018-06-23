38b183d82c74b0b337dce0871f8a168fe5a8fa25
Gong Deli <gnnnnng@gmail.com>
2018-06-23 17:36:39 +0800

    cmake: save building time for g3log
    - do not fetch submodule if g3log source code is available
    - do not run cmake if g3log cmakefiles are generated
    - do not build if g3log is already built (lib, header, cmake module)

--- cmake/InstallG3log.cmake
+++ cmake/InstallG3log.cmake
@@ -8,22 +8,24 @@ if(NOT EXISTS "${G3LOG_SOURCE_DIR}/.git")
     )
 endif()
 
-execute_process(
-    COMMAND ${CMAKE_COMMAND}
-        -H${G3LOG_SOURCE_DIR}
-        -B${G3LOG_BINARY_DIR}
-        -DUSE_DYNAMIC_LOGGING_LEVELS=ON
-        -DG3_SHARED_LIB=OFF
-        -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}
-        -DADD_FATAL_EXAMPLE=OFF
-        -Wno-dev
-)
-execute_process(
-    COMMAND ${CMAKE_COMMAND} --build ${G3LOG_BINARY_DIR}
-)
-execute_process(
-    COMMAND ${CMAKE_COMMAND} --build ${G3LOG_BINARY_DIR} --target install
-)
+if (NOT EXISTS "${G3LOG_BINARY_DIR}")
+    execute_process(COMMAND ${CMAKE_COMMAND}
+            -H${G3LOG_SOURCE_DIR}
+            -B${G3LOG_BINARY_DIR}
+            -DUSE_DYNAMIC_LOGGING_LEVELS=ON
+            -DG3_SHARED_LIB=OFF
+            -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}
+            -DADD_FATAL_EXAMPLE=OFF
+            -Wno-dev
+    )
+endif()
+
+if ((NOT EXISTS "${CMAKE_BINARY_DIR}/lib/libg3logger.a") OR
+    (NOT EXISTS "${CMAKE_BINARY_DIR}/lib/cmake/g3logger/g3loggerConfig.cmake") OR
+    (NOT EXISTS "${CMAKE_BINARY_DIR}/include/g3log"))
+    execute_process(COMMAND ${CMAKE_COMMAND} --build ${G3LOG_BINARY_DIR})
+    execute_process(COMMAND ${CMAKE_COMMAND} --build ${G3LOG_BINARY_DIR} --target install)
+endif()
 
 list(APPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR})
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_BINARY_DIR}/lib/cmake/g3logger")
