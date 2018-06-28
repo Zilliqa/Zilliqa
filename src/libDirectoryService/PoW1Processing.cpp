1c1cf7e62968a0a479b3e759be08b3bcc0b3622d
bb111189 <junhao@zilliqa.com>
2018-06-28 11:24:36 +0800

    Check whitelist (for testnet mode) before verifying pow soln

--- src/libDirectoryService/PoW1Processing.cpp
+++ src/libDirectoryService/PoW1Processing.cpp
@@ -155,6 +155,14 @@ bool DirectoryService::ParseMessageAndVerifyPOW1(
     }
     curr_offset += PUB_KEY_SIZE;
 
+    if (TEST_NET_MODE
+        && not Whitelist::GetInstance().IsNodeInDSWhiteList(peer, key))
+    {
+        LOG_EPOCH(WARNING, to_string(m_mediator.m_currentEpochNum).c_str(),
+                  "Submitted PoW1 but node is not in DS whitelist. Hence, "
+                  "not accepted!");
+    }
+
     // Todo: Reject PoW1 submissions from existing members of DS committee
 
     if (CheckWhetherMaxSubmissionsReceived(peer, key))
@@ -212,18 +220,7 @@ bool DirectoryService::ParseMessageAndVerifyPOW1(
                 return false;
             }
 
-            if (TEST_NET_MODE
-                && not Whitelist::GetInstance().IsNodeInDSWhiteList(peer, key))
-            {
-                LOG_EPOCH(
-                    WARNING, to_string(m_mediator.m_currentEpochNum).c_str(),
-                    "Submitted PoW1 but node is not in DS whitelist. Hence, "
-                    "not accepted!");
-            }
-            else
-            {
-                m_allPoW1s.push_back(make_pair(key, nonce));
-            }
+            m_allPoW1s.push_back(make_pair(key, nonce));
         }
     }
     else
