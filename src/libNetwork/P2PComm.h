9c14cc72d079d6f2ae8a54bac9ee5bcb84f2bea0
Clark Yang <clark@zilliqa.com>
2018-07-10 12:44:39 +0800

    Use 2 different container to avoid timing issue

--- src/libNetwork/P2PComm.h
+++ src/libNetwork/P2PComm.h
@@ -37,7 +37,8 @@ class P2PComm
 {
     std::set<std::vector<unsigned char>> m_broadcastHashes;
     std::mutex m_broadcastHashesMutex;
-    std::unordered_map<time_t, std::vector<unsigned char>> m_broadcastToRemoved;
+    std::vector<std::pair<std::vector<unsigned char>, time_t>>
+        m_broadcastToRemoved;
     std::mutex m_broadcastToRemovedMutex;
     std::mutex m_broadcastCoreMutex;
     std::mutex m_startMessagePumpMutex;
