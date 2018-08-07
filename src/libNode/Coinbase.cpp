735088c6ae6c001df91f0f2f372469392f16165b
kaikawaliu <liuhaichuan@Hotmail.sg>
2018-08-07 13:44:48 +0800

    Use shared_ptr for m_myShardMembers

--- src/libNode/Coinbase.cpp
+++ src/libNode/Coinbase.cpp
@@ -70,20 +70,20 @@ bool Node::Coinbase(const BlockBase& lastMicroBlock, const TxBlock& lastTxBlock)
         LOG_GENERAL(WARNING, "B2 and DS pub keys size do not match");
         return false;
     }
-    if (m_myShardMembers.size() != lastMicroBlock.GetB1().size())
+    if (m_myShardMembers->size() != lastMicroBlock.GetB1().size())
     {
         LOG_GENERAL(WARNING,
                     "B1 and shard members pub keys size do not match "
                         << lastMicroBlock.GetB1().size() << "  "
-                        << m_myShardMembers.size());
+                        << m_myShardMembers->size());
         return false;
     }
-    if (m_myShardMembers.size() != lastMicroBlock.GetB2().size())
+    if (m_myShardMembers->size() != lastMicroBlock.GetB2().size())
     {
         LOG_GENERAL(WARNING,
                     "B2 and shard members pub keys size do not match "
                         << lastMicroBlock.GetB2().size() << "  "
-                        << m_myShardMembers.size());
+                        << m_myShardMembers->size());
         return false;
     }
 
@@ -103,7 +103,8 @@ bool Node::Coinbase(const BlockBase& lastMicroBlock, const TxBlock& lastTxBlock)
 
     deque<PubKey> toKeys;
 
-    for (auto it = m_myShardMembers.begin(); it != m_myShardMembers.end(); ++it)
+    for (auto it = m_myShardMembers->begin(); it != m_myShardMembers->end();
+         ++it)
     {
         toKeys.emplace_back(it->first);
     }
