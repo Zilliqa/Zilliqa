5117eb0ddd9fb8705de02b7341a9d4cd7d5fde9c
bb111189 <junhao@zilliqa.com>
2018-08-24 15:30:53 +0800

    Refactor rep manager unit tests
    Make rep manager unit test more modular. Each function test only a single test

--- tests/Network/Test_ReputationManager.cpp
+++ tests/Network/Test_ReputationManager.cpp
@@ -30,19 +30,38 @@ using namespace std;
 
 BOOST_AUTO_TEST_SUITE(reputationManager)
 
-BOOST_AUTO_TEST_CASE(test_fundamental)
+boost::multiprecision::uint128_t node1, node2;
+
+void setup()
 {
-    INIT_STDOUT_LOGGER();
+    // INIT_STDOUT_LOGGER();
+
     ReputationManager& rm = ReputationManager::GetInstance();
     rm.Clear();
 
-    boost::multiprecision::uint128_t node1
-        = IPConverter::ToNumericalIPFromStr("127.0.0.1");
-    boost::multiprecision::uint128_t node2
-        = IPConverter::ToNumericalIPFromStr("192.168.1.1");
+    node1 = IPConverter::ToNumericalIPFromStr("127.0.0.1");
+    node2 = IPConverter::ToNumericalIPFromStr("192.168.1.1");
     // Setup
     rm.AddNodeIfNotKnown(node1);
     rm.AddNodeIfNotKnown(node2);
+}
+
+void tearDown()
+{
+    ReputationManager& rm = ReputationManager::GetInstance();
+    rm.Clear();
+}
+
+void banNode1()
+{
+    ReputationManager& rm = ReputationManager::GetInstance();
+    rm.PunishNode(node1, ReputationManager::ScoreType::REPTHRESHOLD - 1);
+}
+
+BOOST_AUTO_TEST_CASE(test_check_new_node_rep)
+{
+    setup();
+    ReputationManager& rm = ReputationManager::GetInstance();
 
     // Check reputation of new node
     int32_t result = rm.GetReputation(node1);
@@ -52,26 +71,42 @@ BOOST_AUTO_TEST_CASE(test_fundamental)
                             + to_string(result)
                             + ". Expected: " + to_string(expected) + ".");
 
-    // Check status of node that are not ban
+    tearDown();
+}
+
+BOOST_AUTO_TEST_CASE(test_check_new_node_ban_status)
+{
+    setup();
+    ReputationManager& rm = ReputationManager::GetInstance();
     bool result_bool = rm.IsNodeBanned(node1);
     BOOST_CHECK_MESSAGE(result_bool == false,
                         "Test ban for unban node. Expected: false");
+    tearDown();
+}
 
-    // Check status of banned node
-    LOG_GENERAL(INFO,
-                IPConverter::ToStrFromNumericalIP(node1) + ": "
-                    << rm.GetReputation(node1));
+BOOST_AUTO_TEST_CASE(test_banned_node_status)
+{
+    setup();
+    banNode1();
+
+    ReputationManager& rm = ReputationManager::GetInstance();
     rm.PunishNode(node1, ReputationManager::ScoreType::REPTHRESHOLD - 1);
-    LOG_GENERAL(INFO,
-                IPConverter::ToStrFromNumericalIP(node1) + ": "
-                    << rm.GetReputation(node1));
-    result_bool = rm.IsNodeBanned(node1);
-    BOOST_CHECK_MESSAGE(result_bool == true,
+    BOOST_CHECK_MESSAGE(rm.IsNodeBanned(node1) == true,
+                        "Test ban for banned node. Expected: true");
+    tearDown();
+}
+
+BOOST_AUTO_TEST_CASE(test_banned_node_rep)
+{
+    setup();
+    banNode1();
+
+    ReputationManager& rm = ReputationManager::GetInstance();
+    BOOST_CHECK_MESSAGE(rm.IsNodeBanned(node1) == true,
                         "Test ban for banned node. Expected: true");
 
-    // Check new reputation
-    result = rm.GetReputation(node1);
-    expected = 0 + ReputationManager::ScoreType::REPTHRESHOLD - 1
+    int32_t result = rm.GetReputation(node1);
+    int32_t expected = 0 + ReputationManager::ScoreType::REPTHRESHOLD - 1
         - (ReputationManager::ScoreType::BAN_MULTIPLIER
            * ReputationManager::ScoreType::AWARD_FOR_GOOD_NODES);
 
@@ -79,61 +114,95 @@ BOOST_AUTO_TEST_CASE(test_fundamental)
                         "Check rep score after getting banned. Result: "
                             << result << ". Expected: " << expected);
 
-    // Check ban status after awarding rep score
+    tearDown();
+}
+
+BOOST_AUTO_TEST_CASE(test_banned_node_status_after_reward)
+{
+    setup();
+    banNode1();
+
+    ReputationManager& rm = ReputationManager::GetInstance();
     rm.AwardAllNodes();
-    LOG_GENERAL(INFO,
-                IPConverter::ToStrFromNumericalIP(node1) + ": "
-                    << rm.GetReputation(node1));
+
     BOOST_CHECK_MESSAGE(
         rm.IsNodeBanned(node1) == true,
         IPConverter::ToStrFromNumericalIP(node1)
             << " ban status after 1 round of reward) expected to be "
                "true but false was obtained.");
+    tearDown();
+}
 
-    LOG_GENERAL(INFO,
-                IPConverter::ToStrFromNumericalIP(node2) + ": "
-                    << rm.GetReputation(node2));
+BOOST_AUTO_TEST_CASE(test_unban_node_status_after_reward)
+{
+    setup();
+    banNode1();
+
+    ReputationManager& rm = ReputationManager::GetInstance();
+    rm.AwardAllNodes();
+
     BOOST_CHECK_MESSAGE(
         rm.IsNodeBanned(node2) == false,
         IPConverter::ToStrFromNumericalIP(node2)
             << " ban status after 1 round of reward) expected to be "
                "false but true was obtained.");
 
+    tearDown();
+}
+
+BOOST_AUTO_TEST_CASE(test_node_unbanning)
+{
+    setup();
+    banNode1();
+
+    ReputationManager& rm = ReputationManager::GetInstance();
+    rm.AwardAllNodes();
+
     // Reward till node unban and check status of unban node
     for (int i = 0; i < ReputationManager::ScoreType::BAN_MULTIPLIER; i++)
     {
         rm.AwardAllNodes();
     }
-    LOG_GENERAL(INFO,
-                IPConverter::ToStrFromNumericalIP(node1) + ": "
-                    << rm.GetReputation(node1));
-    result_bool = rm.IsNodeBanned(node1);
-    BOOST_CHECK_MESSAGE(result_bool == false,
+    BOOST_CHECK_MESSAGE(rm.IsNodeBanned(node1) == false,
                         "Test ban for unban node. Expected: false");
+    tearDown();
+}
 
-    // Test upper bound of reputation score
-    result = rm.GetReputation(node2);
-    expected = ReputationManager::ScoreType::UPPERREPTHRESHOLD;
+BOOST_AUTO_TEST_CASE(test_rep_upperbound)
+{
+    setup();
+    ReputationManager& rm = ReputationManager::GetInstance();
+    // Reward till node unban and check status of unban node
+    for (int i = 0; i < ReputationManager::ScoreType::BAN_MULTIPLIER; i++)
+    {
+        rm.AwardAllNodes();
+    }
+    int32_t result = rm.GetReputation(node2);
+    int32_t expected = ReputationManager::ScoreType::UPPERREPTHRESHOLD;
 
     BOOST_CHECK_MESSAGE(result == expected,
                         "Upper bound of reputation test: "
                             << result << ". Expected: " << expected);
+    tearDown();
+}
 
-    // Check lower bound (underflow test)
-    // Expect no change in value due to underflow
-    expected = rm.GetReputation(node1);
+BOOST_AUTO_TEST_CASE(test_rep_underflow)
+{
+    setup();
+    banNode1();
+
+    ReputationManager& rm = ReputationManager::GetInstance();
+
+    // Attempt to underflow the rep. Expected result: Nothing will be change.
+    int32_t expected = rm.GetReputation(node1);
     rm.PunishNode(node1, std::numeric_limits<int32_t>::min());
-    LOG_GENERAL(INFO,
-                IPConverter::ToStrFromNumericalIP(node1) + ": "
-                    << rm.GetReputation(node1));
-    result = rm.GetReputation(node1);
+    int32_t result = rm.GetReputation(node1);
 
     BOOST_CHECK_MESSAGE(
         result == expected,
         "rep underflow test. Result: " << result << ". Expected: " << expected);
 
-    // Clean up
-    rm.Clear();
+    tearDown();
 }
 
 BOOST_AUTO_TEST_SUITE_END()
