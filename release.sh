950f1fd1ccb39354202e8a26ecc293e6db8edc89
Clark Yang <clark@zilliqa.com>
2018-08-28 12:27:43 +0800

    Generate deb package file

--- release.sh
+++ release.sh
@@ -14,8 +14,8 @@
 # and which include a reference to GPLv3 in their program files.
 
 # Configuration settings
-
-fileName="VERSION"
+packageName="D24"
+versionFile="VERSION"
 majorLine=2
 minorLine=4
 fixLine=6
@@ -23,46 +23,47 @@ DSLine=8
 commitLine=10
 
 # Read current version information from version file
-defaultMajor="$(sed -n ${majorLine}p ${fileName})"
-defaultMinor="$(sed -n ${minorLine}p ${fileName})"
-defaultFix="$(sed -n ${fixLine}p ${fileName})"
-defaultDS="$(sed -n ${DSLine}p ${fileName})"
-defaultCommit="$(sed -n ${commitLine}p ${fileName})"
+defaultMajor="$(sed -n ${majorLine}p ${versionFile})"
+defaultMinor="$(sed -n ${minorLine}p ${versionFile})"
+defaultFix="$(sed -n ${fixLine}p ${versionFile})"
+defaultDS="$(sed -n ${DSLine}p ${versionFile})"
+defaultCommit="$(sed -n ${commitLine}p ${versionFile})"
+currentVer=${defaultMajor}.${defaultMinor}.${defaultFix}.${defaultDS}.${defaultCommit}
 
 # Ask user to input new version information
-echo -e "Current software version: ${defaultMajor}.${defaultMinor}.${defaultFix}.${defaultDS}.${defaultCommit}"
-
+echo -e "Current software version:  ${currentVer}"
 read -p "Please enter major version [${defaultMajor}]: " major
 major=${major:-${defaultMajor}}
-
 read -p "Please enter minor version [${defaultMinor}]: " minor
 minor=${minor:-${defaultMinor}}
-
 read -p "Please enter fix version [${defaultFix}]: " fix
 fix=${fix:-${defaultFix}}
-
 read -p "Please enter expected DS epoch [${defaultDS}]: " DS
 DS=${DS:-${defaultDS}}
-
 commit="$(git describe --always)"
 
 # Write new version information into version file
-echo -e "Writing new software version into ${fileName}..."
-
-sed -i "${majorLine}s/.*/${major}/" ${fileName}
-sed -i "${minorLine}s/.*/${minor}/" ${fileName}
-sed -i "${fixLine}s/.*/${fix}/" ${fileName}
-sed -i "${DSLine}s/.*/${DS}/" ${fileName}
-sed -i "${commitLine}s/.*/${commit}/" ${fileName}
-
-echo -e "New software version: ${major}.${minor}.${fix}.${DS}.${commit} is written into ${fileName} successfully.\n"
+echo -e "Writing new software version into ${versionFile}..."
+sed -i "${majorLine}s/.*/${major}/" ${versionFile}
+sed -i "${minorLine}s/.*/${minor}/" ${versionFile}
+sed -i "${fixLine}s/.*/${fix}/" ${versionFile}
+sed -i "${DSLine}s/.*/${DS}/" ${versionFile}
+sed -i "${commitLine}s/.*/${commit}/" ${versionFile}
+newVer=${major}.${minor}.${fix}.${DS}.${commit}
+export ZIL_VER=${newVer}
+export ZIL_PACK_NAME=${packageName}
+echo -e "New software version: ${newVer} is written into ${versionFile} successfully.\n"
 
 # Use cpack to making deb file
-
-echo -e "Make deb file..."
+echo -e "Make deb package..."
+git clean -dfx
+./build.sh; cd build; make package; cd -
+./build_lookup.sh; cd build_lookup; make package; cd -
+echo -e "Deb packages are generated successfully.\n"
 
 # Make SHA-256 & multi-signature
 
 echo -e "Calculate SHA-256..."
+
 echo -e "Make multi-signature..."
 
