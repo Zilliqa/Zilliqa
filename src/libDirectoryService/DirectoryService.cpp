d4d6a4ee55b77fabec55f6b3fbff505c6d74b380
Kaustubh <iit2014147@iiita.ac.in>
2018-09-04 02:32:10 +0530

    Update Leader ID

--- src/libDirectoryService/DirectoryService.cpp
+++ src/libDirectoryService/DirectoryService.cpp
@@ -32,6 +32,7 @@
 #include "libNetwork/Whitelist.h"
 #include "libUtils/DataConversion.h"
 #include "libUtils/DetachedFunction.h"
+#include "libUtils/HashUtils.h"
 #include "libUtils/Logger.h"
 #include "libUtils/SanityChecks.h"
 #include "libUtils/TxnRootComputation.h"
@@ -246,7 +247,10 @@ bool DirectoryService::ProcessSetPrimary(
         }
         m_consensusMyID++;
     }
-    m_consensusLeaderID = 0;
+    m_consensusLeaderID = HashUtils::SerializableToHash16Bits(
+                              m_mediator.m_txBlockChain.GetLastBlock())
+        % m_mediator.m_DSCommittee->size();
+
     LOG_EPOCH(INFO, to_string(m_mediator.m_currentEpochNum).c_str(),
               "START OF EPOCH " << m_mediator.m_dsBlockChain.GetLastBlock()
                                        .GetHeader()
