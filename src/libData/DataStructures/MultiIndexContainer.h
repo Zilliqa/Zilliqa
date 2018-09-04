c4c1b79ca975882ad35742e270fc041b066da768
kaikawaliu <liuhaichuan@Hotmail.sg>
2018-09-04 17:09:28 +0800

    Use pubkey in MIC composite key

--- src/libData/DataStructures/MultiIndexContainer.h
+++ src/libData/DataStructures/MultiIndexContainer.h
@@ -27,7 +27,7 @@ enum MULTI_INDEX_KEY : unsigned int
 {
     GAS_PRICE = 0,
     TXN_ID,
-    ADDR_NONCE
+    SENDER_NONCE
 };
 
 typedef boost::multi_index::ordered_non_unique<
@@ -43,12 +43,12 @@ typedef boost::multi_index::hashed_unique<boost::multi_index::const_mem_fun<
 
 typedef boost::multi_index::ordered_unique<boost::multi_index::composite_key<
     Transaction,
-    boost::multi_index::const_mem_fun<Transaction, Address,
-                                      &Transaction::GetSenderAddr>,
+    boost::multi_index::const_mem_fun<Transaction, const PubKey&,
+                                      &Transaction::GetSenderPubKey>,
     boost::multi_index::const_mem_fun<Transaction,
                                       const boost::multiprecision::uint256_t&,
                                       &Transaction::GetNonce>>>
-    ordered_unique_comp_addr_nonce_key;
+    ordered_unique_comp_sender_nonce_key;
 
 namespace boost
 {
@@ -67,5 +67,5 @@ typedef boost::multi_index::multi_index_container<
     Transaction,
     boost::multi_index::indexed_by<ordered_non_unique_gas_key,
                                    hashed_unique_txnid_key,
-                                   ordered_unique_comp_addr_nonce_key>>
+                                   ordered_unique_comp_sender_nonce_key>>
     gas_txnid_comp_txns;
