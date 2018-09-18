dec0d768e73b6598263e7609f0b8d40b003a9a82
bb111189 <junhao@zilliqa.com>
2018-09-18 15:49:01 +0800

    Adjusted codebase that uses dsblockheader constructor to reflect DSPoWWinners

--- tests/Lookup/Test_LookupNodeForDSBlock.cpp
+++ tests/Lookup/Test_LookupNodeForDSBlock.cpp
@@ -66,9 +66,10 @@ BOOST_AUTO_TEST_CASE(testDSBlockStoring)
 
     std::pair<PrivKey, PubKey> pubKey1 = Schnorr::GetInstance().GenKeyPair();
 
-    DSBlock dsblock(
-        DSBlockHeader(50, 20, prevHash1, pubKey1.second, 0, 0, SWInfo()),
-        CoSignatures());
+    std::map<PubKey, Peer> powDSWinners;
+    DSBlock dsblock(DSBlockHeader(50, 20, prevHash1, pubKey1.second, 0, 0,
+                                  SWInfo(), powDSWinners),
+                    CoSignatures());
 
     curr_offset += dsblock.Serialize(dsblockmsg, curr_offset);
 
