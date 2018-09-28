c9a0f75f036237aedb687255f98bc9d7e8094ea3
Sandip <sandip@zilliqa.com>
2018-09-28 10:36:46 +0800

    Added libRumorSpreading ONLY

---
+++ src/libRumorSpreading/RumorHolder.h
@@ -0,0 +1,131 @@
+/**
+* Copyright (c) 2018 Zilliqa
+* This source code is being disclosed to you solely for the purpose of your participation in
+* testing Zilliqa. You may view, compile and run the code for that purpose and pursuant to
+* the protocols and algorithms that are programmed into, and intended by, the code. You may
+* not do anything else with the code without express permission from Zilliqa Research Pte. Ltd.,
+* including modifying or publishing the code (or any part of it), and developing or forming
+* another public or private blockchain network. This source code is provided ‘as is’ and no
+* warranties are given as to title or non-infringement, merchantability or fitness for purpose
+* and, to the extent permitted by law, all liability for your use of the code is disclaimed.
+* Some programs in this code are governed by the GNU General Public License v3.0 (available at
+* https://www.gnu.org/licenses/gpl-3.0.en.html) (‘GPLv3’). The programs that are governed by
+* GPLv3.0 are those programs that are located in the folders src/depends and tests/depends
+* and which include a reference to GPLv3 in their program files.
+**/
+
+#ifndef __RUMORHOLDER_H__
+#define __RUMORHOLDER_H__
+
+#include <functional>
+#include <map>
+#include <mutex>
+#include <unordered_set>
+
+#include "MemberID.h"
+#include "NetworkConfig.h"
+#include "RumorSpreadingInterface.h"
+#include "RumorStateMachine.h"
+
+namespace RRS
+{
+
+    // This is a thread-safe implementation of the 'RumorSpreadingInterface'.
+    class RumorHolder : public RumorSpreadingInterface
+    {
+    public:
+        // TYPES
+        typedef std::function<int()> NextMemberCb;
+
+        // ENUMS
+        enum class StatisticKey
+        {
+            NumPeers,
+            NumMessagesReceived,
+            Rounds,
+            NumPushMessages,
+            NumEmptyPushMessages,
+            NumPullMessages,
+            NumEmptyPullMessages,
+        };
+
+        static std::map<StatisticKey, std::string> s_enumKeyToString;
+
+    private:
+        // MEMBERS
+        const int m_id;
+        NetworkConfig m_networkConfig;
+        std::vector<int> m_peers;
+        std::unordered_set<int> m_peersInCurrentRound;
+        std::unordered_map<int, RumorStateMachine> m_rumors;
+        mutable std::mutex m_mutex;
+        NextMemberCb m_nextMemberCb;
+        std::unordered_set<int> m_nonPriorPeers;
+        std::map<StatisticKey, double> m_statistics;
+        int m_maxNeighborsPerRounds;
+
+        static const int MAX_RETRY = 3;
+
+        // METHODS
+        // Copy the member ids into a vector
+        void toVector(const std::unordered_set<int>& peers);
+
+        // Return a randomly selected member id
+        int chooseRandomMember();
+
+        // Add the specified 'value' to the previous statistic value
+        void increaseStatValue(StatisticKey key, double value);
+
+    public:
+        // CONSTRUCTORS
+        /// Create an instance which automatically figures out the network parameters.
+        RumorHolder(const std::unordered_set<int>& peers,
+                    int id = MemberID::next());
+        RumorHolder(const std::unordered_set<int>& peers,
+                    const NextMemberCb& cb, int id = MemberID::next());
+
+        /// Used for manually passed network parameters.
+        RumorHolder(const std::unordered_set<int>& peers,
+                    const NetworkConfig& networkConfig,
+                    int id = MemberID::next());
+        RumorHolder(const std::unordered_set<int>& peers,
+                    const NetworkConfig& networkConfig, const NextMemberCb& cb,
+                    int id = MemberID::next());
+        RumorHolder(const std::unordered_set<int>& peers, int maxRoundsInB,
+                    int maxRoundsInC, int maxTotalRounds,
+                    int maxNeighborsPerRound, int id);
+
+        RumorHolder(const RumorHolder& other);
+
+        RumorHolder(RumorHolder&& other) noexcept;
+
+        // METHODS
+        bool addRumor(int rumorId) override;
+
+        std::pair<int, std::vector<Message>>
+        receivedMessage(const Message& message, int fromPeer) override;
+
+        std::pair<std::vector<int>, std::vector<Message>>
+        advanceRound() override;
+
+        // CONST METHODS
+        int id() const;
+
+        const NetworkConfig& networkConfig() const;
+
+        const std::unordered_map<int, RumorStateMachine>& rumorsMap() const;
+
+        bool rumorExists(int rumorId) const;
+
+        bool isOld(int rumorId) const;
+
+        const std::map<StatisticKey, double>& statistics() const;
+
+        std::ostream& printStatistics(std::ostream& outStream) const;
+
+        bool operator==(const RumorHolder& other) const;
+    };
+
+} // project namespace
+
+#endif //RANDOMIZEDRUMORSPREADING_RumorHolder_H
---
+++ src/libRumorSpreading/RumorStateMachine.cpp
@@ -0,0 +1,187 @@
+/**
+* Copyright (c) 2018 Zilliqa
+* This source code is being disclosed to you solely for the purpose of your participation in
+* testing Zilliqa. You may view, compile and run the code for that purpose and pursuant to
+* the protocols and algorithms that are programmed into, and intended by, the code. You may
+* not do anything else with the code without express permission from Zilliqa Research Pte. Ltd.,
+* including modifying or publishing the code (or any part of it), and developing or forming
+* another public or private blockchain network. This source code is provided ‘as is’ and no
+* warranties are given as to title or non-infringement, merchantability or fitness for purpose
+* and, to the extent permitted by law, all liability for your use of the code is disclaimed.
+* Some programs in this code are governed by the GNU General Public License v3.0 (available at
+* https://www.gnu.org/licenses/gpl-3.0.en.html) (‘GPLv3’). The programs that are governed by
+* GPLv3.0 are those programs that are located in the folders src/depends and tests/depends
+* and which include a reference to GPLv3 in their program files.
+**/
+
+#include "RumorStateMachine.h"
+
+#define LITERAL(s) #s
+
+namespace RRS
+{
+
+    // STATIC MEMBERS
+    std::map<RumorStateMachine::State, std::string>
+        RumorStateMachine::s_enumKeyToString
+        = {{State::UNKNOWN, LITERAL(UNKNOWN)},
+           {State::NEW, LITERAL(NEW)},
+           {State::KNOWN, LITERAL(KNOWN)},
+           {State::OLD, LITERAL(OLD)},
+           {State::NUM_STATES, LITERAL(NUM_STATES)}};
+
+    // PRIVATE METHODS
+    void RumorStateMachine::advanceFromNew(
+        const std::unordered_set<int>& membersInRound)
+    {
+        m_roundsInB++;
+        if (m_age > m_networkConfigPtr->maxRoundsTotal())
+        {
+            advanceToOld();
+            return;
+        }
+
+        for (auto id : membersInRound)
+        {
+            if (m_memberRounds.count(id) <= 0)
+                m_memberRounds[id] = 0;
+        }
+
+        // Compare our age to the majority of rounds
+        int numLess = 0;
+        int numGreaterOrEqual = 0;
+        for (const auto entry : m_memberRounds)
+        {
+            int theirRound = entry.second;
+            if (theirRound < m_age)
+            {
+                numLess++;
+            }
+            else if (theirRound > m_networkConfigPtr->maxRoundsInB())
+            {
+                m_state = State::KNOWN;
+            }
+            else
+            {
+                numGreaterOrEqual++;
+            }
+        }
+
+        if (numGreaterOrEqual > numLess)
+        {
+            m_roundsInB++;
+        }
+
+        if (m_roundsInB > m_networkConfigPtr->maxRoundsInB())
+        {
+            m_state = State::KNOWN;
+        }
+        m_memberRounds.clear();
+    }
+
+    void RumorStateMachine::advanceFromKnown()
+    {
+        m_roundsInC++;
+        if (m_age > m_networkConfigPtr->maxRoundsTotal()
+            || m_roundsInC > m_networkConfigPtr->maxRoundsInC())
+        {
+            advanceToOld();
+        }
+    }
+
+    void RumorStateMachine::advanceToOld()
+    {
+        m_state = State::OLD;
+        m_memberRounds.clear();
+    }
+
+    // CONSTRUCTORS
+
+    RumorStateMachine::RumorStateMachine(const NetworkConfig* networkConfigPtr)
+        : m_state(State::NEW)
+        , m_networkConfigPtr(networkConfigPtr)
+        , m_age(0)
+        , m_roundsInB(0)
+        , m_roundsInC(0)
+        , m_memberRounds()
+    {
+    }
+
+    RumorStateMachine::RumorStateMachine(const NetworkConfig* networkConfigPtr,
+                                         int fromMember, int theirRound)
+        : m_state(State::NEW)
+        , m_networkConfigPtr(networkConfigPtr)
+        , m_age(0)
+        , m_roundsInB(0)
+        , m_roundsInC(0)
+        , m_memberRounds()
+    {
+        // Maximum number of rounds reached
+        if (theirRound > m_networkConfigPtr->maxRoundsTotal())
+        {
+            advanceToOld();
+            return;
+        }
+
+        // Stay in B-m state
+        m_memberRounds[fromMember] = theirRound;
+    }
+
+    void RumorStateMachine::rumorReceived(int memberId, int theirRound)
+    {
+        // Only care about other members when the rumor is NEW
+        if (m_state == State::NEW)
+        {
+            //why to throw exception .. we can receive the same rumor from same peer in same round. just update
+            /*if (m_memberRounds.count(memberId) > 0) {
+            throw std::logic_error("Received a message from the same member within a single round");
+        }*/
+            if (m_memberRounds[memberId] < theirRound)
+            {
+                m_memberRounds[memberId] = theirRound;
+            }
+        }
+    }
+
+    void RumorStateMachine::advanceRound(
+        const std::unordered_set<int>& peersInCurrentRound)
+    {
+        m_age++;
+        switch (m_state)
+        {
+        case State::NEW:
+            advanceFromNew(peersInCurrentRound);
+            return;
+        case State::KNOWN:
+            advanceFromKnown();
+            return;
+        case State::OLD:
+            m_age++;
+            return;
+        case State::UNKNOWN:
+        default:
+            throw std::logic_error("Unexpected state: "
+                                   + s_enumKeyToString[m_state]);
+        }
+    }
+
+    RumorStateMachine::State RumorStateMachine::state() const
+    {
+        return m_state;
+    }
+
+    int RumorStateMachine::age() const { return m_age; }
+
+    bool RumorStateMachine::isOld() const { return m_state == State::OLD; }
+
+    std::ostream& operator<<(std::ostream& os, const RumorStateMachine& machine)
+    {
+        os << "{ state: "
+           << RumorStateMachine::s_enumKeyToString[machine.m_state]
+           << ", currentRound: " << machine.m_age
+           << ", roundsInB: " << machine.m_roundsInB
+           << ", roundsInC: " << machine.m_roundsInC << "}";
+        return os;
+    }
+
+} // project namespace
