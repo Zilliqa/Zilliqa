1419a5750210b23e2ed01e5def2dcf475363bcaa
bb111189 <junhao@zilliqa.com>
2018-10-04 18:21:33 +0800

    use tie for m_PoWSWinners for == operator for DSBlockHeader

--- src/libData/BlockData/BlockHeader/DSBlockHeader.cpp
+++ src/libData/BlockData/BlockHeader/DSBlockHeader.cpp
@@ -199,16 +199,11 @@ const map<PubKey, Peer>& DSBlockHeader::GetDSPoWWinners() const
 
 bool DSBlockHeader::operator==(const DSBlockHeader& header) const
 {
-    if (m_PoWDSWinners != header.m_PoWDSWinners)
-    {
-        return false;
-    }
-
     return tie(m_dsDifficulty, m_difficulty, m_prevHash, m_leaderPubKey,
-               m_blockNum, m_timestamp, m_swInfo)
+               m_blockNum, m_timestamp, m_swInfo, m_PoWDSWinners)
         == tie(header.m_dsDifficulty, header.m_difficulty, header.m_prevHash,
                header.m_leaderPubKey, header.m_blockNum, header.m_timestamp,
-               header.m_swInfo);
+               header.m_swInfo, header.m_PoWDSWinners);
 }
 
 bool DSBlockHeader::operator<(const DSBlockHeader& header) const
