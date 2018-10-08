30222f474c9e973a70667d0a376d0fffd6d4e3b1
Sandip <sandip@zilliqa.com>
2018-10-08 18:49:47 +0800

    Changes to help benchmarking of gossip correctly

--- src/libRumorSpreading/RumorHolder.cpp
+++ src/libRumorSpreading/RumorHolder.cpp
@@ -285,6 +285,12 @@ std::pair<std::vector<int>, std::vector<Message>> RumorHolder::advanceRound() {
   std::vector<Message> pushMessages;
   for (auto& r : m_rumors) {
     RumorStateMachine& stateMach = r.second;
+
+    if (stateMach.isOld())
+    {
+      continue;
+    }
+
     stateMach.advanceRound(m_peersInCurrentRound);
     if (!stateMach.isOld()) {
       pushMessages.emplace_back(
