f2c756dd68ba965329b7a57c02fe18958ed2a4df
Gong Deli <gnnnnng@gmail.com>
2018-10-10 16:00:47 +0800

    do submodule update if soruce is not available

--- cmake/InstallMongo.cmake
+++ cmake/InstallMongo.cmake
@@ -11,6 +11,19 @@ message(STATUS "Building and installing mongo driver. This could take longer")
 
 set(INSTALL_MONGO_LOG ${CMAKE_BINARY_DIR}/install_mongo.log)
 
+# update submodule to the locked commit
+execute_process(
+    COMMAND git submodule update --init --recursive src/depends/mongo-c-driver
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    RESULT_VARIABLE INSTALL_MONGO_RET
+    OUTPUT_FILE ${INSTALL_MONGO_LOG}
+    ERROR_FILE ${INSTALL_MONGO_LOG}
+)
+
+if(NOT "${INSTALL_MONGO_RET}" STREQUAL "0")
+    message(FATAL_ERROR "Error when building and installing mongo, see more in log ${INSTALL_MONGO_LOG}")
+endif()
+
 # generate build directory
 execute_process(
     COMMAND ${CMAKE_COMMAND}
@@ -51,6 +64,19 @@ if(NOT "${INSTALL_MONGO_RET}" STREQUAL "0")
 endif()
 
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_BINARY_DIR}/lib/cmake/libmongoc-1.0")
+
+# update submodule to the locked commit
+execute_process(
+    COMMAND git submodule update --init --recursive src/depends/mongo-cxx-driver
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    RESULT_VARIABLE INSTALL_MONGO_RET
+    OUTPUT_FILE ${INSTALL_MONGO_LOG}
+    ERROR_FILE ${INSTALL_MONGO_LOG}
+)
+
+if(NOT "${INSTALL_MONGO_RET}" STREQUAL "0")
+    message(FATAL_ERROR "Error when building and installing mongo, see more in log ${INSTALL_MONGO_LOG}")
+endif()
 
 # generate build directory
 execute_process(
