c7f89272d421718fe2c91e84e407f44b0291cb8f
bb111189 <junhao@zilliqa.com>
2018-10-11 15:50:54 +0800

    Not to send vc block (before ds consensus) to lookup node

--- src/libDirectoryService/ViewChangePostProcessing.cpp
+++ src/libDirectoryService/ViewChangePostProcessing.cpp
@@ -229,8 +229,8 @@ void DirectoryService::ProcessViewChangeConsensusWhenDone() {
   // Broadcasting vcblock to lookup nodes iff view change do not occur before ds
   // block consensus. This is to be consistent with how normal node process the
   // vc block (before ds block).
-  if (viewChangeState == DSBLOCK_CONSENSUS ||
-      viewChangeState == DSBLOCK_CONSENSUS_PREP) {
+  if (viewChangeState != DSBLOCK_CONSENSUS &&
+      viewChangeState != DSBLOCK_CONSENSUS_PREP) {
     unsigned int nodeToSendToLookUpLo = m_mediator.GetShardSize(true) / 4;
     unsigned int nodeToSendToLookUpHi =
         nodeToSendToLookUpLo + TX_SHARING_CLUSTER_SIZE;
