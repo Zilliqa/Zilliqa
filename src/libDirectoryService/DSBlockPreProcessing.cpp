5ef4ea30dd9d88873765d72b3d23d7907ef21257
bb111189 <junhao@zilliqa.com>
2018-10-15 16:16:28 +0800

    Refactor viewchange integration to be enable/disable via ifdef

--- src/libDirectoryService/DSBlockPreProcessing.cpp
+++ src/libDirectoryService/DSBlockPreProcessing.cpp
@@ -628,14 +628,37 @@ bool DirectoryService::RunConsensusOnDSBlockWhenDSPrimary() {
   m_consensusBlockHash =
       m_mediator.m_dsBlockChain.GetLastBlock().GetBlockHash().asBytes();
 
-  // kill first ds leader (used for view change testing)
-  // Either do killing of ds leader or make ds leader do nothing.
-  /*if (m_consensusMyID == 0 && m_viewChangeCounter < 1)
+#ifdef VC_TEST_DS_SUSPEND_1
+  if (m_consensusMyID == 0 && m_viewChangeCounter < 1)
   {
-      LOG_GENERAL(INFO, "I am killing/suspending myself to test view change");
-      // throw exception();
+      LOG_GENERAL(INFO, "I am suspending myself to test viewchange");
       return false;
-  }*/
+  }
+#endif  // VC_TEST_DS_SUSPEND_1
+
+#ifdef VC_TEST_DS_SUsPEND_3
+  if (m_consensusMyID == 0 && m_viewChangeCounter < 3)
+  {
+      LOG_GENERAL(INFO, "I am suspending myself to test viewchange");
+      return false;
+  }
+#endif  // VC_TEST_DS_SUSPEND_3
+
+#ifdef VC_TEST_DS_TERMINATE_1
+  if (m_consensusMyID == 0 && m_viewChangeCounter < 1)
+  {
+      LOG_GENERAL(INFO, "I am killing myself to test viewchange");
+      throw exception();
+  }
+#endif  // VC_TEST_DS_TERMINATE_1
+
+#ifdef VC_TEST_DS_TERMINATE_3
+  if (m_consensusMyID == 0 && m_viewChangeCounter < 3)
+  {
+      LOG_GENERAL(INFO, "I am killing myself to test viewchange");
+      throw exception();
+  }
+#endif  // VC_TEST_DS_TERMINATE_3
 
   m_consensusObject.reset(new ConsensusLeader(
       consensusID, m_mediator.m_currentEpochNum, m_consensusBlockHash,
