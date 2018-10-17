74bc51a95b8f202c1fed7023e3c081d3894656c3
Sandip <sandip@zilliqa.com>
2018-10-17 14:57:38 +0800

    No. of txns are no more limited to MAXSUBMITTXNPERNODE*Shard_Size

--- src/common/Constants.cpp
+++ src/common/Constants.cpp
@@ -112,8 +112,6 @@ const unsigned int NUM_FINAL_BLOCK_PER_POW{
 const unsigned int NUM_DS_KEEP_TX_BODY{
     ReadFromConstantsFile("NUM_DS_KEEP_TX_BODY")};
 const uint32_t MAXMESSAGE{ReadFromConstantsFile("MAXMESSAGE")};
-const unsigned int MAXSUBMITTXNPERNODE{
-    ReadFromConstantsFile("MAXSUBMITTXNPERNODE")};
 const unsigned int MICROBLOCK_GAS_LIMIT{
     ReadFromConstantsFile("MICROBLOCK_GAS_LIMIT")};
 const unsigned int TX_SHARING_CLUSTER_SIZE{
--- src/common/Constants.h
+++ src/common/Constants.h
@@ -134,7 +134,6 @@ extern const unsigned int DS_MICROBLOCK_CONSENSUS_OBJECT_TIMEOUT;
 extern const unsigned int NUM_FINAL_BLOCK_PER_POW;
 extern const unsigned int NUM_DS_KEEP_TX_BODY;
 extern const uint32_t MAXMESSAGE;
-extern const unsigned int MAXSUBMITTXNPERNODE;
 extern const unsigned int MICROBLOCK_GAS_LIMIT;
 extern const unsigned int TX_SHARING_CLUSTER_SIZE;
 extern const unsigned int NEW_NODE_POW_DELAY;
