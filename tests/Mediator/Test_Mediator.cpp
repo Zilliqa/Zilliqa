fef634010ef4fde66ab862ae9368c771aba1942e
jendis <smutnja3@gmail.com>
2018-10-31 00:40:37 +0100

    first executable test scenario

--- tests/Mediator/Test_Mediator.cpp
+++ tests/Mediator/Test_Mediator.cpp
@@ -1,10 +1,47 @@
 /*
- * Test_Mediator.cpp
- *
- *  Created on: Oct 31, 2018
- *      Author: jenda
+ * Copyright (c) 2018 Zilliqa
+ * This source code is being disclosed to you solely for the purpose of your
+ * participation in testing Zilliqa. You may view, compile and run the code for
+ * that purpose and pursuant to the protocols and algorithms that are programmed
+ * into, and intended by, the code. You may not do anything else with the code
+ * without express permission from Zilliqa Research Pte. Ltd., including
+ * modifying or publishing the code (or any part of it), and developing or
+ * forming another public or private blockchain network. This source code is
+ * provided 'as is' and no warranties are given as to title or non-infringement,
+ * merchantability or fitness for purpose and, to the extent permitted by law,
+ * all liability for your use of the code is disclaimed. Some programs in this
+ * code are governed by the GNU General Public License v3.0 (available at
+ * https://www.gnu.org/licenses/gpl-3.0.en.html) ('GPLv3'). The programs that
+ * are governed by GPLv3.0 are those programs that are located in the folders
+ * src/depends and tests/depends and which include a reference to GPLv3 in their
+ * program files.
  */
 
+#include <limits>
+#include <random>
+#include "libMessage/Messenger.h"
+#include "libMediator/Mediator.h"
+#include "libUtils/Logger.h"
+#include "testLib/testLibFunctions.h"
 
+#define BOOST_TEST_MODULE message
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
 
+using namespace std;
+using namespace boost::multiprecision;
 
+BOOST_AUTO_TEST_SUITE(messenger_primitives_test)
+
+BOOST_AUTO_TEST_CASE(init) {
+  INIT_STDOUT_LOGGER();
+  rng.seed(std::random_device()());
+}
+
+BOOST_AUTO_TEST_CASE(test_InitMediator) {
+  KeyPair kp = generateKeyPair();
+  Peer p;
+  Mediator m(kp, p);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
