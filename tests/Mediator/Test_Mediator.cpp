521162261e8290dfdbd89c2178d52ef5581dde38
jendis <smutnja3@gmail.com>
2018-11-05 23:01:59 +0100

    added tests for libMediator

--- tests/Mediator/Test_Mediator.cpp
+++ tests/Mediator/Test_Mediator.cpp
@@ -19,12 +19,12 @@
 
 #include <limits>
 #include <random>
-#include "libMessage/Messenger.h"
-#include "libMediator/Mediator.h"
-#include "libUtils/Logger.h"
-#include "libTestUtils/TestUtils.h"
-#include "libDB/ArchiveDB.h"
 #include "libDB/Archival.h"
+#include "libDB/ArchiveDB.h"
+#include "libMediator/Mediator.h"
+#include "libMessage/Messenger.h"
+#include "libTestUtils/TestUtils.h"
+#include "libUtils/Logger.h"
 
 #define BOOST_TEST_MODULE message
 #define BOOST_TEST_DYN_LINK
@@ -39,13 +39,12 @@ Mediator* m;
 KeyPair kp;
 Peer p;
 
-
 BOOST_AUTO_TEST_CASE(init) {
   INIT_STDOUT_LOGGER();
   rng.seed(std::random_device()());
   kp = TestUtils::GenerateRandomKeyPair();
   p = Peer();
-  m = new Mediator (kp, p);
+  m = new Mediator(kp, p);
 }
 
 BOOST_AUTO_TEST_CASE(RegisterColleagues) {
@@ -69,47 +68,64 @@ BOOST_AUTO_TEST_CASE(UpdateTxBlockRand) {
   m->UpdateTxBlockRand(true);
 }
 
-BOOST_AUTO_TEST_CASE(HeartBeat) {
-  m->HeartBeatLaunch();
-}
+BOOST_AUTO_TEST_CASE(HeartBeat) { m->HeartBeatLaunch(); }
 
 BOOST_AUTO_TEST_CASE(VacuousEpoch) {
   uint64_t VACUOUS_EPOCH = NUM_FINAL_BLOCK_PER_POW - 1;
-  uint64_t ve = (NUM_FINAL_BLOCK_PER_POW * TestUtils::RandomIntInRng<uint64_t>(1,200000)) - 1;
-  BOOST_CHECK_MESSAGE(m->GetIsVacuousEpoch(ve), "Incorrect Vacuous epoch. POW period:" + std::to_string(NUM_FINAL_BLOCK_PER_POW) + ". Current block: " + std::to_string(ve));
+  uint64_t ve = (NUM_FINAL_BLOCK_PER_POW *
+                 TestUtils::RandomIntInRng<uint64_t>(1, 200000)) -
+                1;
+  BOOST_CHECK_MESSAGE(m->GetIsVacuousEpoch(ve),
+                      "Incorrect Vacuous epoch. POW period:" +
+                          std::to_string(NUM_FINAL_BLOCK_PER_POW) +
+                          ". Current block: " + std::to_string(ve));
 
   uint64_t i = 1;
-  for (; i < VACUOUS_EPOCH; i++){
+  for (; i < VACUOUS_EPOCH; i++) {
     m->IncreaseEpochNum();
-    BOOST_CHECK_MESSAGE(!m->GetIsVacuousEpoch(), "Incorrect Vacuous epoch. Final block number per POW " + std::to_string(NUM_FINAL_BLOCK_PER_POW) + ". Current block: " + std::to_string(i));
+    BOOST_CHECK_MESSAGE(!m->GetIsVacuousEpoch(),
+                        "Incorrect Vacuous epoch. Final block number per POW " +
+                            std::to_string(NUM_FINAL_BLOCK_PER_POW) +
+                            ". Current block: " + std::to_string(i));
   }
   m->IncreaseEpochNum();
-  BOOST_CHECK_MESSAGE(m->GetIsVacuousEpoch(), "Missed Vacuous epoch. Final block number per POW " + std::to_string(NUM_FINAL_BLOCK_PER_POW) + ". Current block: " + std::to_string(i));
+  BOOST_CHECK_MESSAGE(m->GetIsVacuousEpoch(),
+                      "Missed Vacuous epoch. Final block number per POW " +
+                          std::to_string(NUM_FINAL_BLOCK_PER_POW) +
+                          ". Current block: " + std::to_string(i));
 }
 
 BOOST_AUTO_TEST_CASE(GetNodeMode) {
-  uint32_t size = TestUtils::RandomIntInRng<uint32_t>(2,100);
-  for (uint32_t i = 1; i <= size; i++){
-    m->m_DSCommittee->push_front(std::make_pair(TestUtils::GenerateRandomPubKey(), TestUtils::GenerateRandomPeer(0,true)));
+  uint32_t size = TestUtils::RandomIntInRng<uint32_t>(2, 100);
+  for (uint32_t i = 1; i <= size; i++) {
+    m->m_DSCommittee->push_front(
+        std::make_pair(TestUtils::GenerateRandomPubKey(),
+                       TestUtils::GenerateRandomPeer(0, true)));
   }
 
-  Peer p_unknown = TestUtils::GenerateRandomPeer(0,false);
+  Peer p_unknown = TestUtils::GenerateRandomPeer(0, false);
 
   string EXPECTED_MODE = "SHRD";
   string mode = m->GetNodeMode(p_unknown);
-  BOOST_CHECK_MESSAGE(mode == EXPECTED_MODE, "Wrong mode. Expected " + EXPECTED_MODE + ". Result: " + mode);
+  BOOST_CHECK_MESSAGE(
+      mode == EXPECTED_MODE,
+      "Wrong mode. Expected " + EXPECTED_MODE + ". Result: " + mode);
 
   EXPECTED_MODE = "DSBU";
   size_t dsc_size = m->m_DSCommittee->size();
   size_t pair_i = TestUtils::RandomIntInRng<size_t>(1, dsc_size - 1);
   (*m->m_DSCommittee)[pair_i].second = p_unknown;
   mode = m->GetNodeMode(p_unknown);
-  BOOST_CHECK_MESSAGE(mode == EXPECTED_MODE, "Wrong mode. Expected " + EXPECTED_MODE + ". Result: " + mode);
+  BOOST_CHECK_MESSAGE(
+      mode == EXPECTED_MODE,
+      "Wrong mode. Expected " + EXPECTED_MODE + ". Result: " + mode);
 
   EXPECTED_MODE = "DSLD";
   (*m->m_DSCommittee)[0].second = p_unknown;
   mode = m->GetNodeMode(p_unknown);
-  BOOST_CHECK_MESSAGE(mode == EXPECTED_MODE, "Wrong mode. Expected " + EXPECTED_MODE + ". Result: " + mode);
+  BOOST_CHECK_MESSAGE(
+      mode == EXPECTED_MODE,
+      "Wrong mode. Expected " + EXPECTED_MODE + ". Result: " + mode);
 }
 
 BOOST_AUTO_TEST_CASE(GetShardSize) {
@@ -120,10 +136,10 @@ BOOST_AUTO_TEST_CASE(GetShardSize) {
   uint32_t EXPECTED_SHARDSIZE = 651;
   uint32_t shardsize = m->GetShardSize(true);
 
-  BOOST_CHECK_MESSAGE(std::to_string(shardsize) == std::to_string(EXPECTED_SHARDSIZE), "Wrong mode. Expected " + std::to_string(EXPECTED_SHARDSIZE) + ". Result: " + std::to_string(shardsize));
+  BOOST_CHECK_MESSAGE(
+      std::to_string(shardsize) == std::to_string(EXPECTED_SHARDSIZE),
+      "Wrong mode. Expected " + std::to_string(EXPECTED_SHARDSIZE) +
+          ". Result: " + std::to_string(shardsize));
 }
-
-
-
 
 BOOST_AUTO_TEST_SUITE_END()
