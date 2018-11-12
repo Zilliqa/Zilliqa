165befc6316230e1a20c66d0ddcc40b2e7a77c0f
kaikawaliu <liuhaichuan@Hotmail.sg>
2018-11-12 12:07:36 +0800

    More constants for gas pricer

--- src/common/Constants.h
+++ src/common/Constants.h
@@ -136,7 +136,6 @@ extern const unsigned int CONSENSUS_OBJECT_TIMEOUT;
 extern const unsigned int FETCHING_MISSING_DATA_TIMEOUT;
 extern const unsigned int DS_MICROBLOCK_CONSENSUS_OBJECT_TIMEOUT;
 extern const unsigned int NUM_FINAL_BLOCK_PER_POW;
-extern const unsigned int NUM_DS_KEEP_TX_BODY;
 extern const uint32_t MAXMESSAGE;
 extern const unsigned int TX_SHARING_CLUSTER_SIZE;
 extern const unsigned int NEW_NODE_POW_DELAY;
@@ -193,7 +192,9 @@ extern const unsigned int UNFILLED_RATIO_HIGH;
 extern const unsigned int GAS_PRICE_PRECISION;
 extern const unsigned int GAS_PRICE_DROP_RATIO;
 extern const unsigned int GAS_PRICE_RAISE_RATIO;
-extern const boost::multiprecision::uint256_t DEFAULT_MIN_GAS_PRICE;
+extern const unsigned int GAS_PRICE_TOLERANCE;
+extern const unsigned int MEAN_GAS_PRICE_DS_NUM;
+extern const boost::multiprecision::uint256_t PRECISION_MIN_VALUE;
 
 extern const bool TEST_NET_MODE;
 extern const bool EXCLUDE_PRIV_IP;
