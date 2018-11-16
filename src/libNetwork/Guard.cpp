07e4ec52cead48fcad36c79083b49aecc2707839
bb111189 <junhao@zilliqa.com>
2018-11-16 14:43:10 +0800

    Refactor whitelist into guard mode

---
+++ src/libNetwork/Guard.cpp
@@ -0,0 +1,201 @@
+/*
+ * Copyright (c) 2018 Zilliqa
+ * This source code is being disclosed to you solely for the purpose of your
+ * participation in testing Zilliqa. You may view, compile and run the code for
+ * that purpose and pursuant to the protocols and algorithms that are programmed
+ * into, and intended by, the code. You may not do anything else with the code
+ * without express permission from Zilliqa Research Pte. Ltd., including
+ * modifying or publishing the code (or any part of it), and developing or
+ * forming another public or private blockchain network. This source code is
+ * provided 'as is' and no warranties are given as to title or non-infringement,
+ * merchantability or fitness for purpose and, to the extent permitted by law,
+ * all liability for your use of the code is disclaimed. Some programs in this
+ * code are governed by the GNU General Public License v3.0 (available at
+ * https://www.gnu.org/licenses/gpl-3.0.en.html) ('GPLv3'). The programs that
+ * are governed by GPLv3.0 are those programs that are located in the folders
+ * src/depends and tests/depends and which include a reference to GPLv3 in their
+ * program files.
+ */
+
+#include "Guard.h"
+
+#include <arpa/inet.h>
+#include <boost/foreach.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <iostream>
+#include <string>
+
+#include "Peer.h"
+#include "libUtils/DataConversion.h"
+#include "libUtils/Logger.h"
+
+using namespace std;
+using namespace boost::multiprecision;
+
+Guard::Guard() {}
+
+Guard::~Guard() {}
+
+Guard& Guard::GetInstance() {
+  static Guard whitelistInfo;
+  return whitelistInfo;
+}
+
+void Guard::UpdateDSGuardlist() {
+  if (!GUARD_MODE) {
+    LOG_GENERAL(WARNING, "Not in Guard mode. DS guard is not available.");
+    return;
+  }
+
+  ifstream config("ds_whitelist.xml");
+
+  if (config.fail()) {
+    LOG_GENERAL(WARNING, "No whitelist xml present");
+    return;
+  }
+
+  using boost::property_tree::ptree;
+  ptree pt;
+  read_xml(config, pt);
+
+  BOOST_FOREACH (ptree::value_type const& v, pt.get_child("nodes")) {
+    if (v.first == "peer") {
+      PubKey key(DataConversion::HexStrToUint8Vec(v.second.get<string>("pubk")),
+                 0);
+      AddToDSGuardlist(key);
+    }
+  }
+
+  LOG_GENERAL(INFO, "Total number of entries in DS guard list:  "
+                        << m_DSGuardList.size());
+}
+
+void Guard::UpdateShardGuardlist() {
+  if (!GUARD_MODE) {
+    LOG_GENERAL(WARNING, "Not in guard mode. Guard list is not available.");
+    return;
+  }
+
+  ifstream config("shard_whitelist.xml");
+
+  if (config.fail()) {
+    LOG_GENERAL(WARNING, "No shard_whitelist xml present");
+    return;
+  }
+
+  using boost::property_tree::ptree;
+  ptree pt;
+  read_xml(config, pt);
+
+  lock_guard<mutex> g(m_mutexShardGuardList);
+  m_ShardGuardList.clear();
+
+  for (auto& addr : pt.get_child("address")) {
+    PubKey key(DataConversion::HexStrToUint8Vec(addr.second.data()), 0);
+    m_ShardGuardList.emplace_back(key);
+    // LOG_GENERAL(INFO, "Added " << key);
+  }
+
+  LOG_GENERAL(INFO, "Total number of entries in shard whitelist:  "
+                        << m_ShardGuardList.size());
+}
+
+void Guard::AddToDSGuardlist(const PubKey& dsGuardPubKey) {
+  if (!GUARD_MODE) {
+    // LOG_GENERAL(WARNING, "Not in Guard mode. Guard list is not available.");
+    return;
+  }
+
+  lock_guard<mutex> g(m_mutexDSGuardList);
+  m_DSGuardList.emplace_back(dsGuardPubKey);
+  // LOG_GENERAL(INFO, "Added " << dsGuardPubKey);
+}
+
+bool Guard::IsNodeInDSGuardList(const PubKey& nodePubKey) {
+  lock_guard<mutex> g(m_mutexDSGuardList);
+  if (std::find(m_DSGuardList.begin(), m_DSGuardList.end(), nodePubKey) ==
+      m_DSGuardList.end()) {
+    LOG_GENERAL(WARNING, "Node not inside DS guard list " << nodePubKey);
+    return false;
+  }
+  return true;
+}
+
+bool Guard::IsPubkeyInShardGuardList(const PubKey& nodePubKey) {
+  lock_guard<mutex> g(m_mutexShardGuardList);
+
+  if (std::find(m_ShardGuardList.begin(), m_ShardGuardList.end(), nodePubKey) ==
+      m_ShardGuardList.end()) {
+    LOG_GENERAL(WARNING, "Pubk Not inside shard guard list " << nodePubKey);
+    return false;
+  }
+  return true;
+}
+
+bool Guard::IsValidIP(const uint128_t& ip_addr) {
+  struct sockaddr_in serv_addr;
+  serv_addr.sin_addr.s_addr = ip_addr.convert_to<unsigned long>();
+  uint32_t ip_addr_c = ntohl(serv_addr.sin_addr.s_addr);
+  if (ip_addr <= 0 || ip_addr >= (uint32_t)-1) {
+    LOG_GENERAL(WARNING,
+                "Invalid IPv4 address " << inet_ntoa(serv_addr.sin_addr));
+    return false;
+  }
+
+  if (!EXCLUDE_PRIV_IP) {
+    // No filtering enable. Hence, IP (other than 0.0.0.0 and 255.255.255.255)
+    // is allowed.
+    return true;
+  }
+
+  lock_guard<mutex> g(m_mutexIPExclusion);
+  for (const auto& ip_pair : m_IPExclusionRange) {
+    if (ip_pair.first <= ip_addr_c && ip_pair.second >= ip_addr_c) {
+      LOG_GENERAL(WARNING,
+                  "In Exclusion List: " << inet_ntoa(serv_addr.sin_addr));
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void Guard::AddToExclusionList(const string& ft, const string& sd) {
+  struct sockaddr_in serv_addr1, serv_addr2;
+  try {
+    inet_aton(ft.c_str(), &serv_addr1.sin_addr);
+    inet_aton(sd.c_str(), &serv_addr2.sin_addr);
+  } catch (exception& e) {
+    LOG_GENERAL(WARNING, "Error " << e.what());
+    return;
+  }
+
+  AddToExclusionList(serv_addr1.sin_addr.s_addr, serv_addr2.sin_addr.s_addr);
+}
+
+void Guard::AddToExclusionList(const uint128_t& ft, const uint128_t& sd) {
+  if (ft > (uint32_t)-1 || sd > (uint32_t)-1) {
+    LOG_GENERAL(WARNING, "Wrong parameters for IPv4");
+    return;
+  }
+  uint32_t ft_c = ntohl(ft.convert_to<uint32_t>());
+  uint32_t sd_c = ntohl(sd.convert_to<uint32_t>());
+  lock_guard<mutex> g(m_mutexIPExclusion);
+
+  if (ft_c > sd_c) {
+    m_IPExclusionRange.emplace_back(sd_c, ft_c);
+  } else {
+    m_IPExclusionRange.emplace_back(ft_c, sd_c);
+  }
+}
+
+void Guard::Init() {
+  UpdateDSGuardlist();
+  if (EXCLUDE_PRIV_IP) {
+    LOG_GENERAL(INFO, "Adding Priv IPs to Exclusion List");
+    AddToExclusionList("172.16.0.0", "172.31.255.255");
+    AddToExclusionList("192.168.0.0", "192.168.255.255");
+    AddToExclusionList("10.0.0.0", "10.255.255.255");
+  }
+}
---
+++ src/libNetwork/Guard.h
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2018 Zilliqa
+ * This source code is being disclosed to you solely for the purpose of your
+ * participation in testing Zilliqa. You may view, compile and run the code for
+ * that purpose and pursuant to the protocols and algorithms that are programmed
+ * into, and intended by, the code. You may not do anything else with the code
+ * without express permission from Zilliqa Research Pte. Ltd., including
+ * modifying or publishing the code (or any part of it), and developing or
+ * forming another public or private blockchain network. This source code is
+ * provided 'as is' and no warranties are given as to title or non-infringement,
+ * merchantability or fitness for purpose and, to the extent permitted by law,
+ * all liability for your use of the code is disclaimed. Some programs in this
+ * code are governed by the GNU General Public License v3.0 (available at
+ * https://www.gnu.org/licenses/gpl-3.0.en.html) ('GPLv3'). The programs that
+ * are governed by GPLv3.0 are those programs that are located in the folders
+ * src/depends and tests/depends and which include a reference to GPLv3 in their
+ * program files.
+ */
+
+#ifndef __GUARD_H__
+#define __GUARD_H__
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#include <boost/multiprecision/cpp_int.hpp>
+#pragma GCC diagnostic pop
+#include <mutex>
+#include <unordered_map>
+#include <vector>
+
+#include "Peer.h"
+#include "libCrypto/Schnorr.h"
+
+class Guard {
+  Guard();
+  ~Guard();
+
+  // Singleton should not implement these
+  Guard(Guard const&) = delete;
+  void operator=(Guard const&) = delete;
+
+  // DS guardlist
+  std::mutex m_mutexDSGuardList;
+  std::vector<PubKey> m_DSGuardList;
+
+  // Shard guardlist
+  std::mutex m_mutexShardGuardList;
+  std::vector<PubKey> m_ShardGuardList;
+
+  // IPFilter
+  std::mutex m_mutexIPExclusion;
+  std::vector<std::pair<boost::multiprecision::uint128_t,
+                        boost::multiprecision::uint128_t>>
+      m_IPExclusionRange;
+
+ public:
+  /// Returns the singleton Guard instance.
+  static Guard& GetInstance();
+  void UpdateDSGuardlist();
+  void UpdateShardGuardlist();
+
+  void AddToDSGuardlist(const PubKey& dsGuardPubKey);
+  bool IsNodeInDSGuardList(const PubKey& nodePubKey);
+  bool IsPubkeyInShardGuardList(const PubKey& nodePubKey);
+
+  // To check if IP is a valid v4 IP and not belongs to exclusion list
+  bool IsValidIP(const boost::multiprecision::uint128_t& ip_addr);
+
+  // To add limits to the exclusion list
+  void AddToExclusionList(const boost::multiprecision::uint128_t& ft,
+                          const boost::multiprecision::uint128_t& sd);
+  void AddToExclusionList(const std::string& ft, const std::string& sd);
+  // Intialize
+  void Init();
+};
+
+#endif  // __GUARD_H__
