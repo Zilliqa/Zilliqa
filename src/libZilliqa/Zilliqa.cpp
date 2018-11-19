6458f6971f54c47d3b29865f876d1966858b59de
bb111189 <junhao@zilliqa.com>
2018-11-19 11:30:03 +0800

    init guard upon process launch

--- src/libZilliqa/Zilliqa.cpp
+++ src/libZilliqa/Zilliqa.cpp
@@ -109,6 +109,10 @@ Zilliqa::Zilliqa(const std::pair<PrivKey, PubKey>& key, const Peer& peer,
 
 {
   LOG_MARKER();
+  
+  // Setting the guard upon process launch
+  Guard::GetInstance().Init();
+
   // Launch the thread that reads messages from the queue
   auto funcCheckMsgQueue = [this]() mutable -> void {
     pair<vector<unsigned char>, Peer>* message = NULL;
@@ -166,7 +170,6 @@ Zilliqa::Zilliqa(const std::pair<PrivKey, PubKey>& key, const Peer& peer,
     switch (syncType) {
       case SyncType::NO_SYNC:
         LOG_GENERAL(INFO, "No Sync Needed");
-        Guard::GetInstance().Init();
         break;
       case SyncType::NEW_SYNC:
         LOG_GENERAL(INFO, "Sync as a new node");
