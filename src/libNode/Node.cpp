fd8b571fa4bede84ea98ef3d68adacf315be58d7
Clark Yang <clark@zilliqa.com>
2018-11-19 18:50:44 +0800

    Fix clang-format issue and parameter naming

--- src/libNode/Node.cpp
+++ src/libNode/Node.cpp
@@ -1032,8 +1032,9 @@ bool Node::ProcessTxnPacketFromLookupCore(const vector<unsigned char>& message,
 
   {
     lock_guard<mutex> g(m_mutexCreatedTransactions);
-    LOG_GENERAL(INFO, "TxnPool size before processing: " << m_createdTxns.size());
-    
+    LOG_GENERAL(INFO,
+                "TxnPool size before processing: " << m_createdTxns.size());
+
     for (const auto& txn : checkedTxns) {
       m_createdTxns.insert(txn);
     }
