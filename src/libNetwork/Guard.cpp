fb4b23847fce59aec0a4eea139a28f1045d58994
bb111189 <junhao@zilliqa.com>
2018-11-19 12:16:58 +0800

    Add new api to get size of ds/shard guard

--- src/libNetwork/Guard.cpp
+++ src/libNetwork/Guard.cpp
@@ -146,6 +146,15 @@ bool Guard::IsPubkeyInShardGuardList(const PubKey& nodePubKey) {
   return true;
 }
 
+  unsigned int Guard::GetNumOfDSGuard(){
+    lock_guard<mutex> g(m_mutexDSGuardList);
+    return m_DSGuardList.size();
+  }
+  unsigned int Guard::GetNumOfShardGuard(){
+    lock_guard<mutex> g(m_mutexShardGuardList);
+    return m_ShardGuardList.size();
+  }
+
 bool Guard::IsValidIP(const uint128_t& ip_addr) {
   struct sockaddr_in serv_addr;
   serv_addr.sin_addr.s_addr = ip_addr.convert_to<unsigned long>();
