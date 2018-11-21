afae5543468a94aa155eb914b96bacff47f02e1c
Kaustubh <iit2014147@iiita.ac.in>
2018-11-21 09:36:34 +0530

    Change epoch num

--- src/libDirectoryService/DSBlockPreProcessing.cpp
+++ src/libDirectoryService/DSBlockPreProcessing.cpp
@@ -1027,7 +1027,7 @@ bool DirectoryService::RunConsensusOnDSBlockWhenDSBackup() {
   }
 
 #ifdef VC_TEST_VC_PRECHECK_1
-  if (m_consensusMyID == 3 && m_mediator.m_currentEpochNum >= 3) {
+  if (m_consensusMyID == 3 && m_mediator.m_currentEpochNum == 2) {
     LOG_EPOCH(
         WARNING, to_string(m_mediator.m_currentEpochNum).c_str(),
         "I am suspending myself to test viewchange (VC_TEST_VC_PRECHECK_1)");
