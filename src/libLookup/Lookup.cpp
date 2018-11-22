3cea5f56f6def26208b68559831d31336f2e6d63
Kaustubh <iit2014147@iiita.ac.in>
2018-11-22 14:12:54 +0530

    Stop travis from crying

--- src/libLookup/Lookup.cpp
+++ src/libLookup/Lookup.cpp
@@ -129,7 +129,6 @@ Transaction CreateValidTestingTransaction(PrivKey& fromPrivKey,
 bool Lookup::GenTxnToSend(size_t num_txn,
                           map<uint32_t, vector<Transaction>>& mp,
                           uint32_t numShards) {
-
   LOG_MARKER();
   vector<Transaction> txns;
 
