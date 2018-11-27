bebfbea917deea5c73ebee1244381a41639a75dd
Clark Yang <clark@zilliqa.com>
2018-11-27 13:53:12 +0800

    Refactor safemath

--- src/libUtils/SafeMath.h
+++ src/libUtils/SafeMath.h
@@ -17,206 +17,28 @@
  * program files.
  */
 
-#ifndef __SafeMath_H__
-#define __SafeMath_H__
-
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#include <boost/multiprecision/cpp_int.hpp>
-#pragma GCC diagnostic pop
-#include "Logger.h"
+#ifndef __SAFEMATH_H__
+#define __SAFEMATH_H__
 
 template <class T>
 class SafeMath {
  public:
-  static bool mul(const T& a, const T& b, T& result) {
-    if (a == 0 || b == 0) {
-      result = 0;
-      return true;
-    }
+  static bool mul(const T& a, const T& b, T& result);
+  static bool power(const T& base, const T& exponent, T& result);
+  static T power(const T& base, const T& exponent, bool isCritical = false);
+  static bool div(const T& a, const T& b, T& result);
+  static bool sub(const T& a, const T& b, T& result);
+  static bool add(const T& a, const T& b, T& result);
 
-    if (typeid(a) == typeid(int8_t) || typeid(a) == typeid(int16_t) ||
-        typeid(a) == typeid(int32_t) || typeid(a) == typeid(int64_t) ||
-        typeid(a) == typeid(boost::multiprecision::int128_t) ||
-        typeid(a) == typeid(boost::multiprecision::int256_t) ||
-        typeid(a) == typeid(boost::multiprecision::int512_t) ||
-        typeid(a) == typeid(boost::multiprecision::int1024_t)) {
-      if ((a == std::numeric_limits<T>::min() && b == (T)-1) ||
-          (a == (T)-1 && b == std::numeric_limits<T>::min())) {
-        LOG_GENERAL(WARNING, "Multiplication Overflow!");
-        return false;
-      }
-    }
-
-    T c = a * b;
-    if (c / a != b) {
-      LOG_GENERAL(WARNING, "Multiplication Underflow/Overflow!");
-      return false;
-    }
-    result = c;
-    return true;
-  }
-
-  static bool power(const T& base, const T& exponent, T& result) {
-    if (exponent == 0) {
-      result = 1;
-      return true;
-    }
-
-    if (exponent < 0) {
-      LOG_GENERAL(WARNING, "Doesn't support pow with negative index");
-      return false;
-    }
-
-    T temp = base, count = exponent - 1;
-
-    while (count > 0) {
-      if (!SafeMath::mul(temp, base, temp)) {
-        LOG_GENERAL(WARNING, "SafeMath::pow failed");
-        return false;
-      }
-      --count;
-    }
-
-    result = temp;
-    return true;
-  }
-
-  // if isCritical is true then will call LOG FATAL,
-  // Now only used for declare constant variable in Constants.cpp
-  static T power(const T& base, const T& exponent, bool isCritical = false) {
-    if (exponent == 0) {
-      return 1;
-    }
-
-    if (exponent < 0) {
-      LOG_GENERAL(WARNING, "Doesn't support pow with negative index");
-      return 0;
-    }
-
-    T ret = base, count = exponent - 1;
-
-    while (count > 0) {
-      if (!SafeMath::mul(ret, base, ret)) {
-        LOG_GENERAL(isCritical ? FATAL : WARNING,
-                    "SafeMath::pow failed ret: " << ret << " base " << base);
-        return ret;
-      }
-      --count;
-    }
-
-    return ret;
-  }
-
-  static bool div(const T& a, const T& b, T& result) {
-    if (b == 0) {
-      LOG_GENERAL(WARNING, "Denominator cannot be zero!");
-      return false;
-    }
-
-    if (typeid(a) == typeid(int8_t) || typeid(a) == typeid(int16_t) ||
-        typeid(a) == typeid(int32_t) || typeid(a) == typeid(int64_t) ||
-        typeid(a) == typeid(boost::multiprecision::int128_t) ||
-        typeid(a) == typeid(boost::multiprecision::int256_t) ||
-        typeid(a) == typeid(boost::multiprecision::int512_t) ||
-        typeid(a) == typeid(boost::multiprecision::int1024_t)) {
-      if (a == std::numeric_limits<T>::min() && b == (T)-1) {
-        LOG_GENERAL(WARNING, "Division Overflow!");
-        return false;
-      }
-    }
-
-    T c = a / b;
-    if (a != b * c + a % b) {
-      return false;
-    }
-
-    result = c;
-    return true;
-  }
-
-  static bool sub(const T& a, const T& b, T& result) {
-    if (typeid(a) == typeid(uint8_t) || typeid(a) == typeid(uint16_t) ||
-        typeid(a) == typeid(uint32_t) || typeid(a) == typeid(uint64_t) ||
-        typeid(a) == typeid(boost::multiprecision::uint128_t) ||
-        typeid(a) == typeid(boost::multiprecision::uint256_t) ||
-        typeid(a) == typeid(boost::multiprecision::uint512_t) ||
-        typeid(a) == typeid(boost::multiprecision::uint1024_t)) {
-      if (b > a) {
-        LOG_GENERAL(WARNING,
-                    "For unsigned subtraction, minuend should be greater than "
-                    "subtrahend!");
-        return false;
-      }
-
-      result = a - b;
-      return true;
-    }
-
-    if (typeid(a) == typeid(int8_t) || typeid(a) == typeid(int16_t) ||
-        typeid(a) == typeid(int32_t) || typeid(a) == typeid(int64_t) ||
-        typeid(a) == typeid(boost::multiprecision::int128_t) ||
-        typeid(a) == typeid(boost::multiprecision::int256_t) ||
-        typeid(a) == typeid(boost::multiprecision::int512_t) ||
-        typeid(a) == typeid(boost::multiprecision::int1024_t)) {
-      if (a > 0 && b > std::numeric_limits<T>::min() - a) {
-        LOG_GENERAL(WARNING, "Subtraction Overflow!");
-        return false;
-      }
-
-      if (a < 0 && b < std::numeric_limits<T>::max() - a) {
-        LOG_GENERAL(WARNING, "Subtraction Underflow!");
-        return false;
-      }
-
-      result = a - b;
-      return true;
-    }
-
-    LOG_GENERAL(WARNING, "Data type " << typeid(a).name() << " not supported!");
-    return false;
-  }
-
-  static bool add(const T& a, const T& b, T& result) {
-    if (typeid(a) == typeid(uint8_t) || typeid(a) == typeid(uint16_t) ||
-        typeid(a) == typeid(uint32_t) || typeid(a) == typeid(uint64_t) ||
-        typeid(a) == typeid(boost::multiprecision::uint128_t) ||
-        typeid(a) == typeid(boost::multiprecision::uint256_t) ||
-        typeid(a) == typeid(boost::multiprecision::uint512_t) ||
-        typeid(a) == typeid(boost::multiprecision::uint1024_t)) {
-      result = a + b;
-
-      if (result < a) {
-        LOG_GENERAL(WARNING, "Addition Overflow!");
-        return false;
-      }
-
-      return true;
-    }
-
-    if (typeid(a) == typeid(int8_t) || typeid(a) == typeid(int16_t) ||
-        typeid(a) == typeid(int32_t) || typeid(a) == typeid(int64_t) ||
-        typeid(a) == typeid(boost::multiprecision::int128_t) ||
-        typeid(a) == typeid(boost::multiprecision::int256_t) ||
-        typeid(a) == typeid(boost::multiprecision::int512_t) ||
-        typeid(a) == typeid(boost::multiprecision::int1024_t)) {
-      if (a > 0 && b > std::numeric_limits<T>::max() - a) {
-        LOG_GENERAL(WARNING, "Addition Overflow!");
-        return false;
-      }
-
-      if (a < 0 && b < std::numeric_limits<T>::min() - a) {
-        LOG_GENERAL(WARNING, "Addition Underflow!");
-        return false;
-      }
-
-      result = a + b;
-      return true;
-    }
-
-    LOG_GENERAL(WARNING, "Data type " << typeid(a).name() << " not supported!");
-    return false;
-  }
+ private:
+  static bool IsSignedInt(const T& a);
+  static bool IsUnsignedInt(const T& a);
+  static bool add_signint(const T& a, const T& b, T& result);
+  static bool add_unsignint(const T& a, const T& b, T& result);
+  static bool sub_signint(const T& a, const T& b, T& result);
+  static bool sub_unsignint(const T& a, const T& b, T& result);
 };
 
-#endif  //__SafeMath_H__
+#include "SafeMath.tpp"
+
+#endif  //__SAFEMATH_H__
