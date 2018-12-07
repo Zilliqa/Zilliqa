53b759c4ea2cb982fbbf6a4b1a4709b0dd56e13f
xiao shengguang <shengguangxiao@gmail.com>
2018-12-07 15:26:47 +0800

    Add state log for micro block consensus and broadcast time

--- tests/Zilliqa/profile_time.py
+++ tests/Zilliqa/profile_time.py
@@ -30,13 +30,14 @@ except ImportError:
 STATE_LOG_FILE = 'state-00001-log.txt'
 KEYWORD_DSCON = '[DSCON]'
 KEYWORD_DSBLK = '[DSBLK]'
-KEYWORD_MBCON = '[MICON]'
+KEYWORD_MICON = '[MICON]'
+KEYWORD_MIBLK = '[MIBLK]'
 KEYWORD_FBCON = '[FBCON]'
 KEYWORD_FLBLK = '[FLBLK]'
 KEYWORD_BEGIN = 'BGIN'
 KEYWORD_DONE = 'DONE'
 KEYWORD_SEND = 'SENDING'
-KEYWORD_RECV = 'RECEIVED'
+KEYWORD_RECV = 'RECVD'
 
 OUTPUT_LINE_FORMAT = "%s\t%6d\t%s\t%s\t%d\n"
 
@@ -50,11 +51,14 @@ class Consensus:
 
 DSConsensusStartTime = SortedDict()
 DSConsensusEndTime = SortedDict()
+POWCountOfEpoch = SortedDict()
 
 DSBlockSendTime = SortedDict()
 DSBlockRecvTime = SortedDict()
 
-MBConsensusDict = SortedDict()
+MIConsensusDict = SortedDict()
+MIBlockSendTime = SortedDict()
+MIBlockRecvTime = SortedDict()
 
 FBConsensusDict = SortedDict()
 FLBlockSendTime = SortedDict()
@@ -75,17 +79,20 @@ def find_files(directory, pattern):
 				yield filename
 
 def get_time(line):
-	m = re.search(r'[\d]+:[\d]+:[\d]+:[ ]*[\d]+', line)
-	if m != None:
-		return m.group(0)
-	else:
-		return ""
+	# The format is [ 18-12-07T09:47:21.817 ], what need to get is 09:47:21.817
+	return line[11:24]
 
 def get_block_number(line):
-	m = re.search(r'[[\d]+]', line)
+	m = re.search(r'\[[0-9]+\]', line)
 	blockNumber = m.group(0)
 	blockNumber = blockNumber[1:len(blockNumber)-1]
 	return int(blockNumber)
+
+def get_pow_count(line):
+	m = re.search(r'POWS = [\d]+', line)
+	strPoW = m.group(0)
+	powCount = strPoW[len('POWS = ') : len(strPoW)]
+	return int(powCount)
 
 def scan_file(fileName):
 	file = open(fileName, "r+")
@@ -95,7 +102,8 @@ def scan_file(fileName):
 		if line.find(KEYWORD_DSCON) != -1:
 			blockNumber = get_block_number(line)
 			if line.find(KEYWORD_BEGIN) != -1:
-				DSConsensusStartTime[blockNumber] = get_time(line)				
+				DSConsensusStartTime[blockNumber] = get_time(line)
+				POWCountOfEpoch[blockNumber] = get_pow_count(line)
 			elif line.find(KEYWORD_DONE) != -1:
 				DSConsensusEndTime[blockNumber] = get_time(line)
 		elif line.find(KEYWORD_DSBLK) != -1:
@@ -112,7 +120,7 @@ def scan_file(fileName):
 				# Need to get the last one to receive
 				if ((not DSBlockRecvTime.has_key(blockNumber)) or recvTime > convert_time_string(DSBlockRecvTime[blockNumber])):
 					DSBlockRecvTime[blockNumber] = strRecvTime
-		elif line.find(KEYWORD_MBCON) != -1:
+		elif line.find(KEYWORD_MICON) != -1:
 			blockNumber = get_block_number(line)
 			if line.find(KEYWORD_BEGIN) != -1:
 				mbConsensusStartTime = get_time(line)
@@ -122,7 +130,21 @@ def scan_file(fileName):
 				mbConsensus.startTime = mbConsensusStartTime
 				mbConsensus.endTime = get_time(line)
 				mbConsensus.timeSpan = convert_time_string(mbConsensus.endTime) - convert_time_string(mbConsensus.startTime)
-				MBConsensusDict.setdefault(blockNumber, []).append(mbConsensus)
+				MIConsensusDict.setdefault(blockNumber, []).append(mbConsensus)
+		elif line.find(KEYWORD_MIBLK) != -1:
+			blockNumber = get_block_number(line)
+			if line.find(KEYWORD_SEND) != -1:
+				strSendTime = get_time(line)
+				sendTime = convert_time_string(strSendTime)
+				# Need to get the earliest one to send out
+				if ((not MIBlockSendTime.has_key(blockNumber)) or sendTime < convert_time_string(MIBlockSendTime[blockNumber])):
+					MIBlockSendTime[blockNumber] = get_time(line)
+			elif line.find(KEYWORD_RECV) != -1:
+				strRecvTime = get_time(line)
+				recvTime = convert_time_string(strRecvTime)
+				# Need to get the last one to receive
+				if ((not MIBlockRecvTime.has_key(blockNumber)) or recvTime > convert_time_string(MIBlockRecvTime[blockNumber])):
+					MIBlockRecvTime[blockNumber] = strRecvTime
 		elif line.find(KEYWORD_FBCON) != -1:
 			blockNumber = get_block_number(line)
 			if line.find(KEYWORD_BEGIN) != -1:
@@ -151,7 +173,8 @@ def scan_file(fileName):
 	file.close()
 
 def convert_time_string(strTime):
-	a,b,c,d = strTime.split(':')
+	a,b,cd = strTime.split(':')
+	c,d = cd.split('.')
 	return int(a)*3600000 + int(b) * 60000 + int(c) * 1000 + int(d)
 
 def printResult(outputFile):	
@@ -162,8 +185,11 @@ def printResult(outputFile):
 	DSBlockSendTimeValues = DSBlockSendTime.values()
 	DSBlockRecvTimeValues = DSBlockRecvTime.values()
 
-	MBConsensusDictKeys = MBConsensusDict.keys()
-	MBConsensusDictValues = MBConsensusDict.values()
+	MBConsensusDictKeys = MIConsensusDict.keys()
+	MBConsensusDictValues = MIConsensusDict.values()
+
+	MIBlockSendTimeValues = MIBlockSendTime.values()
+	MIBlockRecvTimeValues = MIBlockRecvTime.values()
 
 	FBConsensusDictKeys = FBConsensusDict.keys()
 	FBConsensusDictValues = FBConsensusDict.values()
@@ -173,9 +199,13 @@ def printResult(outputFile):
 
 	totalFBBlockNumber = len(FBConsensusDictKeys)
 	totalDSBlockNumber = len(DSConsensusEndTimeValues)
+
+	# Write the data column titles
+	outputFile.write("Operation   \t Epoch\t Start Time\t     End Time  \tTime Span(us)\n")
+
 	dsIndex = 0
 	fbIndex = 0
-	mbIndex = 0
+	mbIndex = 0	
 	while fbIndex < totalFBBlockNumber:
 		if DSConsensusStartTimeKeys[dsIndex] == FBConsensusDictKeys[fbIndex]:
 			# Print out PoW time, it is consider from end of last final block to the beginning of new DS block
@@ -184,25 +214,26 @@ def printResult(outputFile):
 				powFinishTime = DSConsensusStartTimeValues[dsIndex]
 				powTimeSpan = convert_time_string(powFinishTime) - convert_time_string(powStartTime)
 				dsConStartTime = convert_time_string(DSConsensusEndTimeValues[dsIndex])
-				#outputFile.write("POW         \t" + str(DSConsensusStartTimeKeys[dsIndex]) + "\t" + powStartTime + "\t" + powFinishTime + "\t" + str(powTimeSpan) + "\n")
-				outputFile.write(OUTPUT_LINE_FORMAT % ('POW         ', DSConsensusStartTimeKeys[dsIndex], powStartTime, powFinishTime, powTimeSpan))
+				blockNumber = DSConsensusStartTimeKeys[dsIndex]
+				powCount = POWCountOfEpoch[blockNumber]
+				strPoW = 'POW (%6d)' % powCount
+				outputFile.write(OUTPUT_LINE_FORMAT % (strPoW, blockNumber, powStartTime, powFinishTime, powTimeSpan))
 
 			# Print out DS consensus time
 			dsTimeSpan = convert_time_string(DSConsensusEndTimeValues[dsIndex]) - convert_time_string(DSConsensusStartTimeValues[dsIndex])
-			#outputFile.write("DS Consensus\t" + str(DSConsensusStartTimeKeys[dsIndex]) + "\t" + DSConsensusStartTimeValues[dsIndex] + "\t" + DSConsensusEndTimeValues[dsIndex] + "\t" + str(dsTimeSpan) + "\n")
 			outputFile.write(OUTPUT_LINE_FORMAT % ('DS Consensus',DSConsensusStartTimeKeys[dsIndex], DSConsensusStartTimeValues[dsIndex], DSConsensusEndTimeValues[dsIndex], dsTimeSpan))
 
 			# Print out DS block broadcast time
 			dsBdcastSpan = convert_time_string(DSBlockRecvTimeValues[dsIndex]) - convert_time_string(DSBlockSendTimeValues[dsIndex])
-			#outputFile.write("DS Bdcast   \t" + str(DSConsensusStartTimeKeys[dsIndex]) + "\t" + DSBlockSendTimeValues[dsIndex] + "\t" + DSBlockRecvTimeValues[dsIndex] + "\t" + str(dsBdcastSpan) + "\n")
 			outputFile.write(OUTPUT_LINE_FORMAT % ('DSBlk Bdcast', DSConsensusStartTimeKeys[dsIndex], DSBlockSendTimeValues[dsIndex], DSBlockRecvTimeValues[dsIndex], dsBdcastSpan))
 
 		if mbIndex < len(MBConsensusDictKeys):
 			if (MBConsensusDictKeys[mbIndex] == FBConsensusDictKeys[fbIndex]):
 				for mbConsensus in MBConsensusDictValues[mbIndex]:
-					#mbTimeSpan = convert_time_string(mbConsensus.endTime) - convert_time_string(mbConsensus.startTime)
-					#outputFile.write("MB Consensus\t" + str(mbConsensus.blockNumber) + "\t" + mbConsensus.startTime + "\t" + mbConsensus.endTime + "\t" + str(mbTimeSpan) + "\n")
-					outputFile.write(OUTPUT_LINE_FORMAT % ('MB Consensus', mbConsensus.blockNumber, mbConsensus.startTime, mbConsensus.endTime, mbConsensus.timeSpan))
+					outputFile.write(OUTPUT_LINE_FORMAT % ('MI Consensus', mbConsensus.blockNumber, mbConsensus.startTime, mbConsensus.endTime, mbConsensus.timeSpan))
+
+				miBdcastSpan = convert_time_string(MIBlockRecvTimeValues[mbIndex]) - convert_time_string(MIBlockSendTimeValues[mbIndex])
+				outputFile.write(OUTPUT_LINE_FORMAT % ('MIBlk Bdcast', MBConsensusDictKeys[fbIndex], MIBlockSendTimeValues[fbIndex], MIBlockRecvTimeValues[fbIndex], miBdcastSpan))
 				mbIndex += 1
 			else:
 				print("Warning: no complete micro block consensus found for block " + str(FBConsensusDictKeys[fbIndex]))
