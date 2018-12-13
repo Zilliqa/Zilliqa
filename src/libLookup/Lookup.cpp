9efdf0781c2ebbdbd2750647838bca935462ef47
Sandip <sandip@zilliqa.com>
2018-12-13 12:57:34 +0800

    Added delay before starting sync of new lookup

--- src/libLookup/Lookup.cpp
+++ src/libLookup/Lookup.cpp
@@ -88,6 +88,10 @@ void Lookup::InitSync() {
     uint64_t dsBlockNum = 0;
     uint64_t txBlockNum = 0;
 
+    // Hack to allow seed server to be restarted so as to get my newlookup ip
+    // and register me with multiplier.
+    this_thread::sleep_for(chrono::seconds(NEW_LOOKUP_SYNC_DELAY));
+
     // Initialize all blockchains and blocklinkchain
     InitAsNewJoiner();
 
@@ -109,8 +113,6 @@ void Lookup::InitSync() {
 
       this_thread::sleep_for(chrono::seconds(NEW_NODE_SYNC_INTERVAL));
     }
-    // Register myself with multiplier.
-    // TBD
   };
   DetachedFunction(1, func);
 }
