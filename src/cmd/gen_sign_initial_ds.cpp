437fafeea48103366f4af909c7a12424210cd32b
jendis <smutnja3@gmail.com>
2018-12-26 13:09:40 +0100

    Add usage of boost/program_options in gen_sign_initial_ds

--- src/cmd/gen_sign_initial_ds.cpp
+++ src/cmd/gen_sign_initial_ds.cpp
@@ -19,11 +19,19 @@
 #include <string>
 #include "libUtils/UpgradeManager.h"
 
+#include "boost/program_options.hpp"
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
 
+namespace po = boost::program_options;
+
 using boost::property_tree::ptree;
 using namespace std;
+
+#define SUCCESS 0
+#define ERROR_IN_COMMAND_LINE -1
+#define ERROR_UNHANDLED_EXCEPTION -2
+#define ERROR_UNEXPECTED -3
 
 namespace {
 
@@ -44,78 +52,119 @@ struct PTree {
 }  // namespace
 
 int main(int argc, char** argv) {
-  if (argc != 3) {
-    cout << "Input format "
-         << " "
-         << "[privKeyFile]"
-         << " "
-         << "[pubKeyFile]";
-    return -1;
-  }
+  try {
+    string line;
+    vector<PrivKey> privKeys;
+    vector<PubKey> pubKeys;
+    string pubKey_string;
+    string pubk_fn;
+    string privk_fn;
 
-  bytes message;
-  vector<PubKey> dsComm;
+    po::options_description desc("Options");
 
-  if (!UpgradeManager::GetInstance().LoadInitialDS(dsComm)) {
-    cout << "unable to load ";
-    return -1;
-  }
-  unsigned int curr_offset = 0;
-  for (auto& dsKey : dsComm) {
-    dsKey.Serialize(message, curr_offset);
-    curr_offset += PUB_KEY_SIZE;
-  }
+    desc.add_options()("help,h", "Print help messages")(
+        "privk,i", po::value<string>(&privk_fn)->required(), "Filename containing private keys each per line")(
+        "pubk,u", po::value<string>(&pubk_fn)->required(), "Filename containing public keys each per line");
 
-  string line;
-  vector<PrivKey> privKeys;
-  {
-    fstream privFile(argv[1], ios::in);
+    po::variables_map vm;
+    try {
+      po::store(po::parse_command_line(argc, argv, desc), vm);
 
-    while (getline(privFile, line)) {
-      privKeys.emplace_back(DataConversion::HexStrToUint8Vec(line), 0);
+      /** --help option
+       */
+      if (vm.count("help")) {
+        SWInfo::LogBrandBugReport();
+        cout << desc << endl;
+        return SUCCESS;
+      }
+      po::notify(vm);
+    } catch (boost::program_options::required_option& e) {
+      SWInfo::LogBrandBugReport();
+      cerr << "ERROR: " << e.what() << endl << endl;
+      cout << desc;
+      return ERROR_IN_COMMAND_LINE;
+    } catch (boost::program_options::error& e) {
+      SWInfo::LogBrandBugReport();
+      cerr << "ERROR: " << e.what() << endl << endl;
+      return ERROR_IN_COMMAND_LINE;
     }
-  }
 
-  vector<PubKey> pubKeys;
-  string pubKey_string;
-  {
-    fstream pubFile(argv[2], ios::in);
+    //--------------------------
 
-    while (getline(pubFile, line)) {
-      pubKey_string = line;
-      pubKeys.emplace_back(DataConversion::HexStrToUint8Vec(line), 0);
+    vector<unsigned char> message;
+    vector<PubKey> dsComm;
+
+    if (!UpgradeManager::GetInstance().LoadInitialDS(dsComm)) {
+      cout << "Unable to load DS";
+      return ERROR_UNEXPECTED;
     }
-  }
+    unsigned int curr_offset = 0;
+    for (auto& dsKey : dsComm) {
+      dsKey.Serialize(message, curr_offset);
+      curr_offset += PUB_KEY_SIZE;
+    }
 
-  if (privKeys.size() != pubKeys.size()) {
-    if (pubKeys.size() != 1) {
-      cout << "Only one key allowed";
+    try {
+      {
+        fstream privFile(privk_fn, ios::in);
+        while (getline(privFile, line)) {
+          privKeys.emplace_back(DataConversion::HexStrToUint8Vec(line), 0);
+        }
+      }
+    }
+    catch (exception& e) {
+      cerr << "Problem occured when reading private keys on line: " << privKeys.size() + 1 << endl;
+      return ERROR_IN_COMMAND_LINE;
+    }
+
+    try {
+      {
+        fstream pubFile(pubk_fn, ios::in);
+        while (getline(pubFile, line)) {
+          pubKey_string = line;
+          pubKeys.emplace_back(DataConversion::HexStrToUint8Vec(line), 0);
+        }
+      }
+    }
+    catch (exception& e) {
+      cerr << "Problem occured when reading public keys on line: "  << pubKeys.size() + 1 << endl;
+      return ERROR_IN_COMMAND_LINE;
+    }
+
+    if (privKeys.size() != pubKeys.size()) {
+      if (pubKeys.size() != 1) {
+        cerr << "Only one key allowed";
+        return -1;
+      }
+      cerr << "Private key number must equal to public key number!";
       return -1;
     }
-    cout << "Private key number must equal to public key number!";
-    return -1;
+
+    string sig_str;
+
+    for (unsigned int i = 0; i < privKeys.size(); ++i) {
+      Signature sig;
+      Schnorr::GetInstance().Sign(message, privKeys.at(i), pubKeys.at(i), sig);
+      vector<unsigned char> result;
+      sig.Serialize(result, 0);
+      sig_str = DataConversion::Uint8VecToHexStr(result);
+    }
+
+    auto pt = PTree::GetInstance();
+    if (!sig_str.empty()) {
+      pt.push_back(ptree::value_type(signatureProp, ptree(sig_str.c_str())));
+    }
+    if (!pubKey_string.empty()) {
+      pt.push_back(
+          ptree::value_type(publicKeyProp, ptree(pubKey_string.c_str())));
+    }
+
+    write_xml(dsNodeFile.c_str(), pt);
+
+  } catch (exception& e) {
+    cerr << "Unhandled Exception reached the top of main: " << e.what()
+              << ", application will now exit" << endl;
+    return ERROR_UNHANDLED_EXCEPTION;
   }
-
-  string sig_str;
-
-  for (unsigned int i = 0; i < privKeys.size(); ++i) {
-    Signature sig;
-    Schnorr::GetInstance().Sign(message, privKeys.at(i), pubKeys.at(i), sig);
-    bytes result;
-    sig.Serialize(result, 0);
-    sig_str = DataConversion::Uint8VecToHexStr(result);
-  }
-
-  auto pt = PTree::GetInstance();
-  if (!sig_str.empty()) {
-    pt.push_back(ptree::value_type(signatureProp, ptree(sig_str.c_str())));
-  }
-  if (!pubKey_string.empty()) {
-    pt.push_back(
-        ptree::value_type(publicKeyProp, ptree(pubKey_string.c_str())));
-  }
-
-  write_xml(dsNodeFile.c_str(), pt);
-
-  return 0;
+  return SUCCESS;
 }
