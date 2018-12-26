c59fbad593b2fc8cd33c2463a0d00036e6eee603
jendis <smutnja3@gmail.com>
2018-12-26 13:08:28 +0100

    remove regex.h inclusion from IPConverter

--- src/libUtils/IPConverter.h
+++ src/libUtils/IPConverter.h
@@ -26,7 +26,6 @@
 #include <arpa/inet.h>
 #include <boost/asio.hpp>
 #include <string>
-#include <regex>
 #include "libUtils/SWInfo.h"
 
 /// Utility class for converter from ip address string to numerical
