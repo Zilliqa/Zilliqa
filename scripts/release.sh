e36564929695cbc6827ffa42ef37ec39a53fda8b
Clark Yang <clark@zilliqa.com>
2018-12-26 17:51:20 +0800

    Refactor release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -35,19 +35,26 @@ scillaPath=""    # Scilla will NOT be released if leaving this field empty
 
 # Environment variables
 releaseDir="release"
-versionFile="../VERSION"
+versionFile="VERSION"
 dsNodeFile="dsnodes.xml"
-majorLine=2
-minorLine=4
-fixLine=6
-DSLine=8
-commitLine=12
-shaLine=14
-sigLine=16
+zilliqaDSLine=2
+scillaDSLine=4
+zilliqaMajorLine=6
+zilliqaMinorLine=8
+zilliqaFixLine=10
+scillaMajorLine=14
+scillaMinorLine=16
+scillaFixLine=18
+zilliqaCommitLine=20
+zilliqaShaLine=22
+zilliqaSigLine=24
+scillaCommitLine=26
+scillaShaLine=28
+scillaSigLine=30
 
 # Validate input argument
 if [ "$#" -ne 0 ]; then
-    echo "Usage: source ./scripts/release.sh"
+    echo "Usage: source scripts/release.sh"
     return 1
 fi
 
@@ -83,58 +90,55 @@ fi
 
 if [ -d "${scillaPath}" ]; then
     echo -e "\n\n\033[0;32m*INFO* Scilla will be released.\033[0m\n"
+    scillaPath="$(realpath ${scillaPath})"
 else
-    echo -e "\n\n\033[0;32m*INFO* Scilla Path : ${scillaPath} not existed, we will NOT release Scilla.\033[0m\n"
+    echo -e "\n\n\033[0;32m*INFO* Scilla Path : ${scillaPath} not existed, Scilla will NOT be released.\033[0m\n"
+    scillaPath=""
 fi
 
 # Read information from files
 constantFile="$(realpath ${constantFile})"
 constantLookupFile="$(realpath ${constantLookupFile})"
 constantArchivalFile="$(realpath ${constantArchivalFile})"
+versionFile="$(realpath ${versionFile})"
 accountName="$(grep -oPm1 "(?<=<UPGRADE_HOST_ACCOUNT>)[^<]+" ${constantFile})"
 repoName="$(grep -oPm1 "(?<=<UPGRADE_HOST_REPO>)[^<]+" ${constantFile})"
-major="$(sed -n ${majorLine}p ${versionFile})"
-minor="$(sed -n ${minorLine}p ${versionFile})"
-fix="$(sed -n ${fixLine}p ${versionFile})"
-DS="$(sed -n ${DSLine}p ${versionFile})"
+major="$(sed -n ${zilliqaMajorLine}p ${versionFile})"
+minor="$(sed -n ${zilliqaMinorLine}p ${versionFile})"
+fix="$(sed -n ${zilliqaFixLine}p ${versionFile})"
+DS="$(sed -n ${zilliqaDSLine}p ${versionFile})"
 commit="$(git describe --always)"
-
-# Write new version information into version file
-echo -e "Writing new software version into ${versionFile}..."
-sed -i "${commitLine}s/.*/${commit}/" ${versionFile}
 newVer=${major}.${minor}.${fix}.${DS}.${commit}
 export ZIL_VER=${newVer}
 export ZIL_PACK_NAME=${packageName}
-echo -e "New software version: ${newVer} is written into ${versionFile} successfully.\n"
 
 # Use cpack to making deb file
 echo -e "Make deb package..."
 rm -rf ${releaseDir}
 cmake -H. -B${releaseDir} -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/usr/local/
 cmake --build ${releaseDir} --j4
-cd ${releaseDir}; make package; cp ../${versionFile} .; debFile="$(ls *.deb)"; cd -
+cd ${releaseDir}; make package; cp ${versionFile} .; debFile="$(ls *.deb)"; cd -
 echo -e "Deb packages are generated successfully.\n"
 
-# Make SHA-256 & multi-signature
-echo -e "Making SHA-256 & multi-signature..."
+# Write new version information into version file and make SHA-256 & multi-signature
 privKeyFile="$(realpath ${privKeyFile})"
 pubKeyFile="$(realpath ${pubKeyFile})"
 cd ${releaseDir}
+sed -i "${zilliqaCommitLine}s/.*/${commit}/" $(basename ${versionFile})
+echo -e "Making SHA-256 & multi-signature..."
 sha="$(sha256sum ${debFile}|cut -d ' ' -f1|tr 'a-z' 'A-Z')"
-sed -i "${shaLine}s/.*/${sha}/" ${versionFile}
+sed -i "${zilliqaShaLine}s/.*/${sha}/" $(basename ${versionFile})
 signature="$(./bin/signmultisig ${sha} ${privKeyFile} ${pubKeyFile})"
-sed -i "${sigLine}s/.*/${signature}/" ${versionFile}
+sed -i "${zilliqaSigLine}s/.*/${signature}/" $(basename ${versionFile})
 cd -
-echo -e "SHA-256 & multi-signature are written into ${versionFile} successfully.\n"
+echo -e "SHA-256 & multi-signature are written into $(basename ${versionFile}) successfully.\n"
 
-#Sign the ds nodes and update the xml
+#Update the xml
 cd ${releaseDir}
-echo -e "Updating ds xml file"
+echo -e "Updating xml file"
 cp ../constants_local.xml ./constants.xml
-cp ../${dsNodeFile} ./${dsNodeFile}
 ret="$(./bin/gensigninitialds   ${privKeyFile} ${pubKeyFile})"
-cp ./${dsNodeFile} ../${dsNodeFile}
-cd - 
+cd -
 
 # Upload package onto GitHub
 echo -e "Creating new release and uploading package onto GitHub..."
@@ -163,7 +167,7 @@ curl -v -s \
   -H "Authorization: token ${GitHubToken}" \
   -H "Content-Type:application/octet-stream" \
   --data-binary @${pubKeyFile} \
-  "https://uploads.github.com/repos/${accountName}/${repoName}/releases/${releaseId}/assets?name=$(basename {pubKeyFile})"
+  "https://uploads.github.com/repos/${accountName}/${repoName}/releases/${releaseId}/assets?name=$(basename ${pubKeyFile})"
 curl -v -s \
   -H "Authorization: token ${GitHubToken}" \
   -H "Content-Type:application/vnd.debian.binary-package" \
@@ -172,13 +176,8 @@ curl -v -s \
 curl -v -s \
   -H "Authorization: token ${GitHubToken}" \
   -H "Content-Type:application/octet-stream" \
-  --data-binary @${releaseDir}/${versionFile} \
-  "https://uploads.github.com/repos/${accountName}/${repoName}/releases/${releaseId}/assets?name=${versionFile}"
-curl -v -s  \
-  -H "Authorization: token ${GitHubToken}" \
-  -H "Content-Type:application/octet-stream"  \
-  --data-binary @"${dsNodeFile}" \
-  "https://uploads.github.com/repos/${accountName}/${repoName}/releases/${releaseId}/assets?name=${dsNodeFile}"
+  --data-binary @${releaseDir}/$(basename ${versionFile}) \
+  "https://uploads.github.com/repos/${accountName}/${repoName}/releases/${releaseId}/assets?name=$(basename ${versionFile})"
 curl -v -s  \
   -H "Authorization: token ${GitHubToken}" \
   -H "Content-Type:application/octet-stream"  \
@@ -195,4 +194,4 @@ curl -v -s  \
   --data-binary @"${constantArchivalFile}" \
   "https://uploads.github.com/repos/${accountName}/${repoName}/releases/${releaseId}/assets?name=${constantArchivalFile##*/}_archival"
 rm ${releaseLog}
-echo -e "\nA new draft release with package is created on Github sucessfully, please proceed to publishing the draft release on Github webpage.\n"
+echo -e "\n\n\033[0;32mA new draft release with package is created on Github successfully, please proceed to publishing the draft release on Github web page.\033[0m\n"
