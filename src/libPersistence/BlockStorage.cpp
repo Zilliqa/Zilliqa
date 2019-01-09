25e06b9b6fcf9aead4efbc22c047748033c0f128
kaustubh <kaustubh@zilliqa.com>
2019-01-09 15:03:03 +0800

    merge for testing

--- src/libPersistence/BlockStorage.cpp
+++ src/libPersistence/BlockStorage.cpp
@@ -123,7 +123,6 @@ bool BlockStorage::GetTxnFromHistoricalDB(const dev::h256& key,
                                           TxBodySharedPtr& body) {
   std::string bodyString;
 
-
   bodyString = m_txnHistoricalDB->Lookup(key);
 
   if (bodyString.empty()) {
