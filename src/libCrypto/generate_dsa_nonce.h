ced899baf0673d84dbcdc4ae3824ee88baba3aa9
Gong Deli <gnnnnng@gmail.com>
2019-01-10 13:57:15 +0800

    implement BN_generate_dsa_nonce

---
+++ src/libCrypto/generate_dsa_nonce.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2019 Zilliqa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Copyright 1995-2018 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+#ifndef _GENERATE_DSA_NONCE_H_
+#define _GENERATE_DSA_NONCE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int BN_generate_dsa_nonce(BIGNUM *out, const BIGNUM *range, const BIGNUM *priv,
+                          const unsigned char *message, size_t message_len,
+                          BN_CTX *ctx);
+#ifdef __cplusplus
+}
+#endif
+#endif
