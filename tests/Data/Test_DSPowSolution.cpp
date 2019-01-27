04003e8cb822945658c18ce30e087c127c4ea492
jendis <smutnja3@gmail.com>
2019-01-27 17:22:14 +0100

    Extends Test_DSPowSolution

--- tests/Data/Test_DSPowSolution.cpp
+++ tests/Data/Test_DSPowSolution.cpp
@@ -24,6 +24,7 @@
 #include "libUtils/DataConversion.h"
 #include "libUtils/Logger.h"
 #include "libTestUtils/TestUtils.h"
+#include "libCrypto/Schnorr.h"
 
 #define BOOST_TEST_MODULE dspowsolutiontest
 #define BOOST_TEST_DYN_LINK
@@ -34,11 +35,35 @@ using namespace std;
 
 BOOST_AUTO_TEST_SUITE(dspowsolutiontest)
 
-BOOST_AUTO_TEST_CASE(testContractInvoking) {
+BOOST_AUTO_TEST_CASE(testDSPowSolutionClass) {
   INIT_STDOUT_LOGGER();
   LOG_MARKER();
 
+  uint64_t blockNumberInput = TestUtils::DistUint64();
+  uint8_t difficultyLevelInput = TestUtils::DistUint8();
+  Peer submitterPeerInput = Peer();
 
+  PairOfKey keypair = TestUtils::GenerateRandomKeyPair();
+  bytes message = TestUtils::GenerateRandomCharVector(TestUtils::Dist1to99());
+  Signature signatureInput = TestUtils::GetSignature(message, keypair);
+  PubKey submitterKeyInput = keypair.second;
+
+  uint64_t nonceInput = TestUtils::DistUint64();
+  string resultingHashInput = TestUtils::GenerateRandomString(64);
+  string mixHashInput = TestUtils::GenerateRandomString(64);
+  uint32_t lookupIdInput = TestUtils::DistUint32();
+  uint128_t gasPriceInput = TestUtils::DistUint128();
+
+
+
+  DSPowSolution dsps(blockNumberInput,
+                difficultyLevelInput,
+                submitterPeerInput,submitterKeyInput,
+                nonceInput,
+                resultingHashInput,
+                mixHashInput, lookupIdInput,
+                gasPriceInput,
+                signatureInput);
 
 }
 
