4796b01e6df000f013de238704a99e7dd562aa3b
kaustubh <kaustubh@zilliqa.com>
2019-01-28 13:58:18 +0530

    Set luckyAddr

--- src/libDirectoryService/Coinbase.cpp
+++ src/libDirectoryService/Coinbase.cpp
@@ -421,9 +421,10 @@ void DirectoryService::InitCoinbase() {
       base_reward, base_reward_each, lookupReward, reward_each_lookup,
       nodeReward,  reward_each,      balance_left, PubKey(PrivKey()),
       Address()};
-  StoreCoinbaseInDiagnosticDB(entry);
+
   if (nonGuard.empty()) {
     LOG_GENERAL(WARNING, "No non-guard found, skip LuckyDraw");
+    StoreCoinbaseInDiagnosticDB(entry);
     return;
   }
 
@@ -445,6 +446,8 @@ void DirectoryService::InitCoinbase() {
                           << "][" << m_mediator.m_currentEpochNum << "]["
                           << balance_left << "] lucky draw");
   }
+  entry.luckyDrawWinnerAddr = luckyAddr;
+  StoreCoinbaseInDiagnosticDB(entry);
 }
 
 void DirectoryService::StoreCoinbaseInDiagnosticDB(
