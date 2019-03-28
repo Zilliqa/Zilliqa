bd97c212fd78b9b8e092f6728184e3b6dd7e285c
kaikawaliu <liuhaichuan@Hotmail.sg>
2019-03-28 15:24:27 +0800

    Add mutexes and new LatestEpochStatesUpdated

--- src/libPersistence/BlockStorage.h
+++ src/libPersistence/BlockStorage.h
@@ -253,11 +253,17 @@ class BlockStorage : public Singleton<BlockStorage> {
   /// Save state root
   bool PutStateRoot(const bytes& data);
 
+  /// Save latest epoch when states were moved to disk
+  bool PutLatestEpochStatesUpdated(const uint64_t& epochNum);
+
   /// Retrieve Last Transactions Trie Root Hash
   bool GetMetadata(MetaType type, bytes& data);
 
   // Retrieve the state root
   bool GetStateRoot(bytes& data);
+
+  /// Save latest epoch when states were moved to disk
+  bool GetLatestEpochStatesUpdated(uint64_t& epochNum);
 
   /// Save DS committee
   bool PutDSCommittee(const std::shared_ptr<DequeOfNode>& dsCommittee,
@@ -340,21 +346,24 @@ class BlockStorage : public Singleton<BlockStorage> {
   bool RefreshAll();
 
  private:
-  std::mutex m_mutexMetadata;
-  std::mutex m_mutexDsBlockchain;
-  std::mutex m_mutexTxBlockchain;
-  std::mutex m_mutexMicroBlock;
-  std::mutex m_mutexDsCommittee;
-  std::mutex m_mutexVCBlock;
-  std::mutex m_mutexFallbackBlock;
-  std::mutex m_mutexBlockLink;
-  std::mutex m_mutexShardStructure;
-  std::mutex m_mutexStateDelta;
-  std::mutex m_mutexTempState;
-  std::mutex m_mutexTxBody;
-  std::mutex m_mutexTxBodyTmp;
   std::mutex m_mutexDiagnostic;
-  std::mutex m_mutexStateRoot;
+
+  mutable std::shared_timed_mutex m_mutexMetadata;
+  mutable std::shared_timed_mutex m_mutexDsBlockchain;
+  mutable std::shared_timed_mutex m_mutexTxBlockchain;
+  mutable std::shared_timed_mutex m_mutexMicroBlock;
+  mutable std::shared_timed_mutex m_mutexDsCommittee;
+  mutable std::shared_timed_mutex m_mutexVCBlock;
+  mutable std::shared_timed_mutex m_mutexFallbackBlock;
+  mutable std::shared_timed_mutex m_mutexBlockLink;
+  mutable std::shared_timed_mutex m_mutexShardStructure;
+  mutable std::shared_timed_mutex m_mutexStateDelta;
+  mutable std::shared_timed_mutex m_mutexTempState;
+  mutable std::shared_timed_mutex m_mutexTxBody;
+  mutable std::shared_timed_mutex m_mutexTxBodyTmp;
+  mutable std::shared_timed_mutex m_mutexStateRoot;
+  mutable std::shared_timed_mutex m_mutexTxnHistorical;
+  mutable std::shared_timed_mutex m_mutexMBHistorical;
 
   unsigned int m_diagnosticDBNodesCounter;
   unsigned int m_diagnosticDBCoinbaseCounter;
