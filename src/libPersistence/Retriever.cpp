d1cc5d7717a43e8c9f4c961f5bc151379b5895be
Clark Yang <ckyang0225@gmail.com>
2019-03-28 15:29:23 +0800

    Revert "Replace experimental filesystem with boost since macos dont support"
    This reverts commit 3294a2d50723e9102707ec75a9d9904cd8587f96.

--- src/libPersistence/Retriever.cpp
+++ src/libPersistence/Retriever.cpp
@@ -22,11 +22,18 @@
 #include <exception>
 #include <vector>
 
+#include <boost/filesystem.hpp>
+#include <experimental/filesystem>
+
 #include "libData/AccountData/AccountStore.h"
 #include "libData/AccountData/Transaction.h"
 #include "libPersistence/BlockStorage.h"
 #include "libUtils/DataConversion.h"
-#include "libUtils/FileSystem.h"
+
+using namespace boost::filesystem;
+namespace filesys = boost::filesystem;
+
+namespace stdfs = std::experimental::filesystem;
 
 Retriever::Retriever(Mediator& mediator) : m_mediator(mediator) {}
 
@@ -78,9 +85,11 @@ bool Retriever::RetrieveTxBlocks(bool trimIncompletedBlocks) {
     // Check if StateDeltaFromS3/StateDelta_{i} exists and copy over to the
     // local persistence/stateDelta
     std::string source = "StateDeltaFromS3/stateDelta_" + std::to_string(i);
-    if (boost::filesystem::exists(source)) {
+    if (stdfs::exists(source)) {
       try {
-        recursive_copy_dir(source, target);
+        stdfs::copy(source, target,
+                    stdfs::copy_options::overwrite_existing |
+                        stdfs::copy_options::recursive);
       } catch (std::exception& e) {
         LOG_GENERAL(FATAL, "Failed to copy over stateDelta for TxBlk:" << i);
       }
@@ -92,8 +101,8 @@ bool Retriever::RetrieveTxBlocks(bool trimIncompletedBlocks) {
 
         // generate state now for NUM_FINAL_BLOCK_PER_POW statedeltas
         for (unsigned int j = firstStateDeltaIndex; j <= i; j++) {
-          if (!bfs::exists("StateDeltaFromS3/stateDelta_" +
-                           std::to_string(j))) {
+          if (!stdfs::exists("StateDeltaFromS3/stateDelta_" +
+                             std::to_string(j))) {
             continue;
           }
           bytes stateDelta;
@@ -112,8 +121,6 @@ bool Retriever::RetrieveTxBlocks(bool trimIncompletedBlocks) {
         }
         // commit the state to disk
         AccountStore::GetInstance().MoveUpdatesToDisk();
-        // clear the stateDelta db
-        BlockStorage::GetBlockStorage().ResetDB(BlockStorage::STATE_DELTA);
         firstStateDeltaIndex = i + 1;
       }
     } else  // we rely on next statedelta that covers this missing one
