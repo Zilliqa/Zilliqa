4db59b44bc8b16d95a56b6f49f98c5b776c62775
Sandip <sandip@zilliqa.com>
2019-04-26 11:36:19 +0800

    Fix StateDelta check

--- src/libMessage/Messenger.cpp
+++ src/libMessage/Messenger.cpp
@@ -2541,12 +2541,6 @@ bool Messenger::GetAccountStoreDelta(const bytes& src,
                                      const unsigned int offset,
                                      AccountStore& accountStore,
                                      const bool revertible, bool temp) {
-  if (offset >= src.size()) {
-    LOG_GENERAL(WARNING, "Invalid data and offset, data size "
-                             << src.size() << ", offset " << offset);
-    return false;
-  }
-
   ProtoAccountStore result;
   result.ParseFromArray(src.data() + offset, src.size() - offset);
 
@@ -2602,12 +2596,6 @@ bool Messenger::GetAccountStoreDelta(const bytes& src,
                                      const unsigned int offset,
                                      AccountStoreTemp& accountStoreTemp,
                                      bool temp) {
-  if (offset >= src.size()) {
-    LOG_GENERAL(WARNING, "Invalid data and offset, data size "
-                             << src.size() << ", offset " << offset);
-    return false;
-  }
-
   ProtoAccountStore result;
   result.ParseFromArray(src.data() + offset, src.size() - offset);
 
