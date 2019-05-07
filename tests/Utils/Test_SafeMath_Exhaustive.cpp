4433af474f330dd097ce16e5958e0b24d6693f64
kaustubh <kaustubh@zilliqa.com>
2019-05-08 00:14:27 +0530

    Include Pow

--- tests/Utils/Test_SafeMath_Exhaustive.cpp
+++ tests/Utils/Test_SafeMath_Exhaustive.cpp
@@ -18,6 +18,7 @@
 #include "libUtils/Logger.h"
 #include "libUtils/SafeMath.h"
 
+#include <boost/phoenix/phoenix.hpp>
 #include <functional>
 
 #define BOOST_TEST_MODULE safemath_exhaustive
@@ -25,13 +26,30 @@
 #include <boost/test/unit_test.hpp>
 
 using namespace std;
+namespace mp = boost::multiprecision;
+
+using typeToTest = uint8_t;
+using typeToVerify = uint64_t;
+
+using typeToTestSigned = make_signed<typeToTest>::type;
+using typeToVerifySigned = make_signed<typeToVerify>::type;
+
+template <class Integer>
+Integer powerTest(const Integer& base, const Integer& exponent) {
+  return static_cast<Integer>(powl(base, exponent));
+}
+
+enum class OperatorType { DIV = 0, EXP, OTHER };
 
 // WiderInteger should have greater space than integer
 // Also both should be unsigned or both should be signed
 template <class Integer, class WiderInteger>
 void test_function_exhaustive(
-    function<bool(const Integer&, const Integer&, Integer&)> safemath_operator,
-    function<WiderInteger(WiderInteger&, WiderInteger&)> generic_operator) {
+    const function<bool(const Integer&, const Integer&, Integer&)>
+        safemath_operator,
+    const function<WiderInteger(const WiderInteger&, const WiderInteger&)>
+        generic_operator,
+    const OperatorType op = OperatorType::OTHER) {
   constexpr WiderInteger maxValue = numeric_limits<Integer>::max();
   constexpr WiderInteger minValue = numeric_limits<Integer>::min();
   LOG_GENERAL(INFO, "Min: " << minValue << " Max: " << maxValue);
@@ -39,19 +57,21 @@ void test_function_exhaustive(
     for (WiderInteger j = minValue; j <= maxValue; ++j) {
       Integer res{};
       WiderInteger actual_result{};
-      if (generic_operator == divides<WiderInteger>() && j == 0) {
+      if ((op == OperatorType::DIV && j == 0) ||
+          (op == OperatorType::EXP && j < 0)) {
         continue;
       }
       actual_result = generic_operator(i, j);
-      if (!safemath_operator(i, j, res)) {
-        BOOST_CHECK_MESSAGE(res != actual_result, "False positive in SafeMath "
-                                                      << " " << res << " "
-                                                      << actual_result);
-        LOG_GENERAL(INFO,
-                    "Correct Result: " << actual_result <<" operators: "<<i<<" "<<j;);
-      } else {
-        BOOST_CHECK_MESSAGE(res == actual_result,
-                            "SafeMath wrong " << res << " " << actual_result);
+      const bool& success = safemath_operator(i, j, res);
+      BOOST_CHECK_MESSAGE(!(success && res != actual_result),
+                          "SafeMath wrong " << res << " " << actual_result);
+      if (!success) {
+        // In case of failure, log the values
+        if (actual_result <= maxValue && actual_result >= minValue) {
+          LOG_GENERAL(INFO, "Result calculated otherwise: "
+                                << actual_result << " operators: " << i << " "
+                                << j);
+        }
       }
     }
   }
@@ -61,51 +81,64 @@ BOOST_AUTO_TEST_SUITE(safemath_exhaustive)
 
 BOOST_AUTO_TEST_CASE(test_uint8_addition) {
   INIT_STDOUT_LOGGER();
-  test_function_exhaustive<uint8_t, uint32_t>(SafeMath<uint8_t>::add,
-                                              plus<uint32_t>());
+  test_function_exhaustive<typeToTest, typeToVerify>(SafeMath<typeToTest>::add,
+                                                     plus<typeToVerify>());
 }
 
 BOOST_AUTO_TEST_CASE(test_uint8_subtraction) {
   INIT_STDOUT_LOGGER();
-  test_function_exhaustive<uint8_t, uint32_t>(SafeMath<uint8_t>::sub,
-                                              minus<uint32_t>());
+  test_function_exhaustive<typeToTest, typeToVerify>(SafeMath<typeToTest>::sub,
+                                                     minus<typeToVerify>());
 }
 
 BOOST_AUTO_TEST_CASE(test_int8_addition) {
   INIT_STDOUT_LOGGER();
-  test_function_exhaustive<int8_t, int32_t>(SafeMath<int8_t>::add,
-                                            plus<int32_t>());
+  test_function_exhaustive<typeToTestSigned, typeToVerifySigned>(
+      SafeMath<typeToTestSigned>::add, plus<typeToVerifySigned>());
 }
 
 BOOST_AUTO_TEST_CASE(test_int8_subtraction) {
   INIT_STDOUT_LOGGER();
-  test_function_exhaustive<int8_t, int32_t>(SafeMath<int8_t>::sub,
-                                            minus<int32_t>());
+  test_function_exhaustive<typeToTestSigned, typeToVerifySigned>(
+      SafeMath<typeToTestSigned>::sub, minus<typeToVerifySigned>());
 }
 
 BOOST_AUTO_TEST_CASE(test_uint8_mul) {
   INIT_STDOUT_LOGGER();
-  test_function_exhaustive<uint8_t, uint32_t>(SafeMath<uint8_t>::mul,
-                                              multiplies<uint32_t>());
+  test_function_exhaustive<typeToTest, typeToVerify>(
+      SafeMath<typeToTest>::mul, multiplies<typeToVerify>());
 }
 
 BOOST_AUTO_TEST_CASE(test_int8_mul) {
   INIT_STDOUT_LOGGER();
-  test_function_exhaustive<int8_t, int32_t>(SafeMath<int8_t>::mul,
-                                            multiplies<int32_t>());
+  test_function_exhaustive<typeToTestSigned, typeToVerifySigned>(
+      SafeMath<typeToTestSigned>::mul, multiplies<typeToVerifySigned>());
 }
 
 BOOST_AUTO_TEST_CASE(test_uint8_div) {
   INIT_STDOUT_LOGGER();
-  test_function_exhaustive<uint8_t, uint32_t>(SafeMath<uint8_t>::div,
-                                              divides<uint32_t>());
+  test_function_exhaustive<typeToTest, typeToVerify>(
+      SafeMath<typeToTest>::div, divides<typeToVerify>(), OperatorType::DIV);
 }
 
 BOOST_AUTO_TEST_CASE(test_int8_div) {
   INIT_STDOUT_LOGGER();
-  test_function_exhaustive<int8_t, int32_t>(SafeMath<int8_t>::div,
-                                            divides<int32_t>());
+  test_function_exhaustive<typeToTestSigned, typeToVerifySigned>(
+      SafeMath<typeToTestSigned>::div, divides<typeToVerifySigned>(),
+      OperatorType::DIV);
 }
 
+BOOST_AUTO_TEST_CASE(test_uint8_pow) {
+  INIT_STDOUT_LOGGER();
+  test_function_exhaustive<typeToTest, typeToVerify>(
+      SafeMath<typeToTest>::power, powerTest<typeToVerify>, OperatorType::EXP);
+}
+
+BOOST_AUTO_TEST_CASE(test_int8_pow) {
+  INIT_STDOUT_LOGGER();
+  test_function_exhaustive<typeToTestSigned, typeToVerifySigned>(
+      SafeMath<typeToTestSigned>::power, powerTest<typeToVerifySigned>,
+      OperatorType::EXP);
+}
 
 BOOST_AUTO_TEST_SUITE_END();
