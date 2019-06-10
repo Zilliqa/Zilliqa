45c5bf43e2a5e7aae856b76af1047c40f034668f
kaustubh <kaustubh@zilliqa.com>
2019-06-10 11:52:32 +0530

    non captilization

--- src/libServer/LookupServer.cpp
+++ src/libServer/LookupServer.cpp
@@ -1060,7 +1060,7 @@ Json::Value LookupServer::DSBlockListing(unsigned int page) {
   uint maxPages = (currBlockNum / PAGE_SIZE) + 1;
 
   if (currBlockNum == INIT_BLOCK_NUMBER) {
-    throw JsonRpcException(RPC_IN_WARMUP, "No DS Blocks");
+    throw JsonRpcException(RPC_IN_WARMUP, "No DS blocks");
   }
 
   _json["maxPages"] = maxPages;
