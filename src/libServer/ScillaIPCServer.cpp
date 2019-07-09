afc58d8e87535436b872ca6f7c8aed9ac25a692c
Advay Pal <advaypal@hotmail.com>
2019-07-09 16:25:29 +0800

    Add contract address and connect to BC methods

--- src/libServer/ScillaIPCServer.cpp
+++ src/libServer/ScillaIPCServer.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 // #include "BCSimulator.h"
 #include "ScillaIPCServer.h"
-
-#include <stdio.h>
+#include "libPersistence/ContractStorage2.h"
+#include "libUtils/DataConversion.h"
 
 using namespace jsonrpc;
 using namespace std;
 
-ScillaIPCServer::ScillaIPCServer(UnixDomainSocketServer &server) : 
+ScillaIPCServer::ScillaIPCServer(UnixDomainSocketServer &server, const dev::h160& address) : 
                                  AbstractServer<ScillaIPCServer>(server, JSONRPC_SERVER_V2) {
 
   this->bindAndAddMethod(Procedure("fetchStateValue", PARAMS_BY_NAME, JSON_BOOLEAN, 
@@ -17,16 +17,29 @@ ScillaIPCServer::ScillaIPCServer(UnixDomainSocketServer &server) :
   this->bindAndAddMethod(Procedure("updateStateValue", jsonrpc::PARAMS_BY_NAME, 
                                    "query", JSON_STRING, "value", JSON_STRING, NULL), 
                                    &ScillaIPCServer::updateStateValueI);
+  this->contractAddress = address;
 
 }
 
-void ScillaIPCServer::updateStateValue(const string &query, const string &value) { 
-  cout << "HELLO!!";
+void setContractAddress(const dev::h160& address) {
+  this->contractAddress = address;
+}
+
+dev::h160 getContractAddress() {
+  return this->contractAddress;
+}
+
+void ScillaIPCServer::updateStateValue(const string &query, const string &value) {
+  return ContractStorage2::UpdateStateValue(this->contractAddress, DataConversion::StringToCharArray(query),
+                                            0, DataConversion:: StringToCharArray(value), 0);
+  // cout << "HELLO!!";
   // bc.updateStateValue(StringToCharArray(name), StringToCharArray(value));
 }
 
 bool ScillaIPCServer::fetchStateValue(const string &query, const string &value) {
-  return true;
+  return ContractStorage2::FetchStateValue(this->contractAddress, DataConversion::StringToCharArray(query),
+                                           0, DataConversion:: StringToCharArray(value), 0);
+  // return true;
   // return bc.fetchStateValue(StringToCharArray(name), StringToCharArray(value));
 }
 
--- src/libServer/ScillaIPCServer.h
+++ src/libServer/ScillaIPCServer.h
@@ -1,19 +1,21 @@
 #include <jsonrpccpp/server.h>
 #include <jsonrpccpp/server/connectors/unixdomainsocketserver.h>
 class ScillaIPCServer : jsonrpc::AbstractServer<ScillaIPCServer> {
-private:
-  // BCSimulator bc;
-public:
-  ScillaIPCServer(jsonrpc::UnixDomainSocketServer &server);
+  private:
+    dev::h160& contractAddress;
+  public:
+    ScillaIPCServer(jsonrpc::UnixDomainSocketServer &server, const dev::h160& address);
 
-  inline void fetchStateValueI(const Json::Value &request, Json::Value &response) {
-    response = this->fetchStateValue(request["query"].asString(), request["value"].asString());
-  }
+    inline void fetchStateValueI(const Json::Value &request, Json::Value &response) {
+      response = this->fetchStateValue(request["query"].asString(), request["value"].asString());
+    }
 
-  inline void updateStateValueI(const Json::Value &request, Json::Value &response) {
-    this->updateStateValue(request["query"].asString(), request["value"].asString());
-  }
-  bool fetchStateValue(const std::string& query, const std::string& value);
-  void updateStateValue(const std::string& query, const std::string& value);
-  int main();
-};
+    inline void updateStateValueI(const Json::Value &request, Json::Value &response) {
+      this->updateStateValue(request["query"].asString(), request["value"].asString());
+    }
+    bool fetchStateValue(const std::string& query, const std::string& value);
+    void updateStateValue(const std::string& query, const std::string& value);
+    void setContractAddress(const dev::h160& address);
+    dev::h160 getContractAddress();
+    int main();
+  };
