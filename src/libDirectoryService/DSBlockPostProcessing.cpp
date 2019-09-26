37d7eb7308f8e142253bd2f20f946867914c6cc9
Sandip <sandip@zilliqa.com>
2019-09-26 17:01:34 +0800

    fixed clang formatting

--- src/libDirectoryService/DSBlockPostProcessing.cpp
+++ src/libDirectoryService/DSBlockPostProcessing.cpp
@@ -437,10 +437,10 @@ void DirectoryService::StartFirstTxEpoch() {
           INFO,
           "No other shards. So no other microblocks expected to be received");
       m_stopRecvNewMBSubmission = true;
-      
+
       auto func1 = [this]() mutable -> void {
-          m_mediator.m_node->CommitTxnPacketBuffer();
-        };
+        m_mediator.m_node->CommitTxnPacketBuffer();
+      };
       DetachedFunction(1, func1);
 
       RunConsensusOnFinalBlock();
