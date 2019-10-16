bdd7786016cb03f942f637dca59cac25de8f52e7
kaikawaliu <liuhaichuan@Hotmail.sg>
2019-10-16 18:35:51 +0800

    Add ENABLE_WEBSOCKET constant

--- src/common/Constants.cpp
+++ src/common/Constants.cpp
@@ -308,6 +308,8 @@ const unsigned int NUM_SHARD_PEER_TO_REVEAL{
     ReadConstantNumeric("NUM_SHARD_PEER_TO_REVEAL", "node.jsonrpc.")};
 const std::string SCILLA_IPC_SOCKET_PATH{
     ReadConstantString("SCILLA_IPC_SOCKET_PATH", "node.jsonrpc.")};
+const bool ENABLE_WEBSOCKET{
+    ReadConstantString("ENABLE_WEBSOCKET", "node.jsonrpc.") == "true"};
 const unsigned int WEBSOCKET_PORT{
     ReadConstantNumeric("WEBSOCKET_PORT", "node.jsonrpc.")};
 
