77f275bb8a1a5baefd96141841c62f194453390c
Antonio Nunez <ansnunez@gmail.com>
2019-11-28 13:46:05 +0800

    To v5.2.0-alpha.0

--- src/libUtils/SWInfo.h
+++ src/libUtils/SWInfo.h
@@ -22,7 +22,7 @@
 #include <iostream>
 #include "common/Serializable.h"
 
-const std::string VERSION_TAG = "v5.1.0-alpha.0";
+const std::string VERSION_TAG = "v5.2.0-alpha.0";
 const std::string ZILLIQA_BRAND = "Copyright (C) Zilliqa. Version " +
                                   VERSION_TAG + ".  <https://www.zilliqa.com/>";
 
