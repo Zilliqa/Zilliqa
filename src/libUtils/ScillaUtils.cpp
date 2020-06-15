9667b8c70a3918eb1c1a352bfb028f17ab8cf028
kaikawaliu <haichuan@zilliqa.com>
2020-06-15 16:11:58 +0800

    address comments

--- src/libUtils/ScillaUtils.cpp
+++ src/libUtils/ScillaUtils.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Zilliqa
+ * Copyright (C) 2020 Zilliqa
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
