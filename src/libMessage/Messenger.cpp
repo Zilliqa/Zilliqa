sidutta/Zilliqa-1

handling shard node ip change

src/libMessage/Messenger.cpp
@@ -8996,6 +8996,97 @@ bool Messenger::GetVCNodeSetDSTxBlockFromSeed(const bytes& src,
   return true;
 }
 
+bool Messenger::SetNodeNewShardGuardNetworkInfo(
+    bytes& dst, const unsigned int offset, const uint64_t dsEpochNumber,
+    const Peer& shardGuardNewNetworkInfo, const uint64_t timestamp,
+    const PairOfKey& shardGuardkey) {
+  LOG_MARKER();
+  NodeSetShardGuardNetworkInfoUpdate result;
+
+  result.mutable_data()->set_dsepochnumber(dsEpochNumber);
+  SerializableToProtobufByteArray(
+      shardGuardkey.second, *result.mutable_data()->mutable_shardguardpubkey());
+  PeerToProtobuf(shardGuardNewNetworkInfo,
+                 *result.mutable_data()->mutable_shardguardnewnetworkinfo());
+  result.mutable_data()->set_timestamp(timestamp);
+
+  if (!result.data().IsInitialized()) {
+    LOG_GENERAL(
+        WARNING,
+        "NodeLookupSetShardGuardNetworkInfoUpdate.Data initialization failed");
+    return false;
+  }
+
+  bytes tmp(result.data().ByteSize());
+  result.data().SerializeToArray(tmp.data(), tmp.size());
+
+  Signature signature;
+  if (!Schnorr::Sign(tmp, shardGuardkey.first, shardGuardkey.second,
+                     signature)) {
+    LOG_GENERAL(WARNING, "Failed to sign shard guard identity update");
+    return false;
+  }
+
+  SerializableToProtobufByteArray(signature, *result.mutable_signature());
+
+  if (!result.IsInitialized()) {
+    LOG_GENERAL(
+        WARNING,
+        "NodeLookupSetShardGuardNetworkInfoUpdate initialization failed");
+    return false;
+  }
+
+  return SerializeToArray(result, dst, offset);
+}
+
+bool Messenger::GetNodeNewShardGuardNetworkInfo(const bytes& src,
+                                                const unsigned int offset,
+                                                uint64_t& dsEpochNumber,
+                                                Peer& shardGuardNewNetworkInfo,
+                                                uint64_t& timestamp,
+                                                PubKey& shardGuardPubkey) {
+  LOG_MARKER();
+
+  if (offset >= src.size()) {
+    LOG_GENERAL(WARNING, "Invalid data and offset, data size "
+                             << src.size() << ", offset " << offset);
+    return false;
+  }
+
+  NodeSetShardGuardNetworkInfoUpdate result;
+  result.ParseFromArray(src.data() + offset, src.size() - offset);
+
+  if (!result.IsInitialized() || !result.data().IsInitialized()) {
+    LOG_GENERAL(
+        WARNING,
+        "NodeLookupSetShardGuardNetworkInfoUpdate initialization failed");
+    return false;
+  }
+
+  // First deserialize the fields needed just for signature check
+  PROTOBUFBYTEARRAYTOSERIALIZABLE(result.data().shardguardpubkey(),
+                                  shardGuardPubkey);
+  Signature signature;
+  PROTOBUFBYTEARRAYTOSERIALIZABLE(result.signature(), signature);
+
+  // Check signature
+  bytes tmp(result.data().ByteSize());
+  result.data().SerializeToArray(tmp.data(), tmp.size());
+  if (!Schnorr::Verify(tmp, 0, tmp.size(), signature, shardGuardPubkey)) {
+    LOG_GENERAL(WARNING,
+                "NodeLookupSetShardGuardNetworkInfoUpdate signature wrong");
+    return false;
+  }
+
+  // Deserialize the remaining fields
+  dsEpochNumber = result.data().dsepochnumber();
+  ProtobufToPeer(result.data().shardguardnewnetworkinfo(),
+                 shardGuardNewNetworkInfo);
+  timestamp = result.data().timestamp();
+
+  return true;
+}
+
 bool Messenger::SetDSLookupNewDSGuardNetworkInfo(
     bytes& dst, const unsigned int offset, const uint64_t dsEpochNumber,
     const Peer& dsGuardNewNetworkInfo, const uint64_t timestamp,

Zilliqa/Zilliqa

src/libMessage/Messenger.cpp
@@ -8996,6 +8996,94 @@ bool Messenger::GetVCNodeSetDSTxBlockFromSeed(const bytes& src,
   return true;
 }
 
+bool Messenger::SetNodeNewShardGuardNetworkInfo(
+    bytes& dst, const unsigned int offset, const uint64_t dsEpochNumber,
+    const Peer& shardGuardNewNetworkInfo, const uint64_t timestamp,
+    const PairOfKey& shardGuardkey) {
+  LOG_MARKER();
+  NodeSetShardGuardNetworkInfoUpdate result;
+
+  result.mutable_data()->set_dsepochnumber(dsEpochNumber);
+  SerializableToProtobufByteArray(
+      shardGuardkey.second, *result.mutable_data()->mutable_shardguardpubkey());
+  PeerToProtobuf(shardGuardNewNetworkInfo,
+                 *result.mutable_data()->mutable_shardguardnewnetworkinfo());
+  result.mutable_data()->set_timestamp(timestamp);
+
+  if (!result.data().IsInitialized()) {
+    LOG_GENERAL(
+        WARNING,
+        "NodeSetShardGuardNetworkInfoUpdate.Data initialization failed");
+    return false;
+  }
+
+  bytes tmp(result.data().ByteSize());
+  result.data().SerializeToArray(tmp.data(), tmp.size());
+
+  Signature signature;
+  if (!Schnorr::Sign(tmp, shardGuardkey.first, shardGuardkey.second,
+                     signature)) {
+    LOG_GENERAL(WARNING, "Failed to sign shard guard identity update");
+    return false;
+  }
+
+  SerializableToProtobufByteArray(signature, *result.mutable_signature());
+
+  if (!result.IsInitialized()) {
+    LOG_GENERAL(WARNING,
+                "NodeSetShardGuardNetworkInfoUpdate initialization failed");
+    return false;
+  }
+
+  return SerializeToArray(result, dst, offset);
+}
+
+bool Messenger::GetNodeNewShardGuardNetworkInfo(const bytes& src,
+                                                const unsigned int offset,
+                                                uint64_t& dsEpochNumber,
+                                                Peer& shardGuardNewNetworkInfo,
+                                                uint64_t& timestamp,
+                                                PubKey& shardGuardPubkey) {
+  LOG_MARKER();
+
+  if (offset >= src.size()) {
+    LOG_GENERAL(WARNING, "Invalid data and offset, data size "
+                             << src.size() << ", offset " << offset);
+    return false;
+  }
+
+  NodeSetShardGuardNetworkInfoUpdate result;
+  result.ParseFromArray(src.data() + offset, src.size() - offset);
+
+  if (!result.IsInitialized() || !result.data().IsInitialized()) {
+    LOG_GENERAL(WARNING,
+                "NodeSetShardGuardNetworkInfoUpdate initialization failed");
+    return false;
+  }
+
+  // First deserialize the fields needed just for signature check
+  PROTOBUFBYTEARRAYTOSERIALIZABLE(result.data().shardguardpubkey(),
+                                  shardGuardPubkey);
+  Signature signature;
+  PROTOBUFBYTEARRAYTOSERIALIZABLE(result.signature(), signature);
+
+  // Check signature
+  bytes tmp(result.data().ByteSize());
+  result.data().SerializeToArray(tmp.data(), tmp.size());
+  if (!Schnorr::Verify(tmp, 0, tmp.size(), signature, shardGuardPubkey)) {
+    LOG_GENERAL(WARNING, "NodeSetShardGuardNetworkInfoUpdate signature wrong");
+    return false;
+  }
+
+  // Deserialize the remaining fields
+  dsEpochNumber = result.data().dsepochnumber();
+  ProtobufToPeer(result.data().shardguardnewnetworkinfo(),
+                 shardGuardNewNetworkInfo);
+  timestamp = result.data().timestamp();
+
+  return true;
+}
+
 bool Messenger::SetDSLookupNewDSGuardNetworkInfo(
     bytes& dst, const unsigned int offset, const uint64_t dsEpochNumber,
     const Peer& dsGuardNewNetworkInfo, const uint64_t timestamp,