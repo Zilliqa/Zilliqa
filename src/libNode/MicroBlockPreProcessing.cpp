5ce43da65d341b9d1d7082b207fd2379a956be56
kaikawaliu <haichuan@zilliqa.com>
2020-08-27 15:28:43 +0800

    Make TxnFees back to void coinbase Address

--- src/libNode/MicroBlockPreProcessing.cpp
+++ src/libNode/MicroBlockPreProcessing.cpp
@@ -69,10 +69,7 @@ bool Node::ComposeMicroBlock(const uint64_t& microblock_gas_limit) {
   uint128_t rewards = 0;
   if (m_mediator.GetIsVacuousEpoch() &&
       m_mediator.m_ds->m_mode != DirectoryService::IDLE) {
-    if (!SafeMath<uint128_t>::add(m_mediator.m_ds->m_totalTxnFees,
-                                  COINBASE_REWARD_PER_DS, rewards)) {
-      LOG_GENERAL(WARNING, "rewards addition unsafe!");
-    }
+    rewards = COINBASE_REWARD_PER_DS;
   } else {
     rewards = m_txnFees;
   }
@@ -1339,12 +1336,10 @@ bool Node::CheckMicroBlockHashes(bytes& errorMsg) {
   // Check Rewards
   if (m_mediator.GetIsVacuousEpoch() &&
       m_mediator.m_ds->m_mode != DirectoryService::IDLE) {
-    // Check COINBASE_REWARD_PER_DS + totalTxnFees
-    uint128_t rewards = 0;
-    if (!SafeMath<uint128_t>::add(m_mediator.m_ds->m_totalTxnFees,
-                                  COINBASE_REWARD_PER_DS, rewards)) {
-      LOG_GENERAL(WARNING, "total_reward addition unsafe!");
-    }
+    // Check COINBASE_REWARD_PER_DS
+
+    uint128_t rewards = COINBASE_REWARD_PER_DS;
+
     if (rewards != m_microblock->GetHeader().GetRewards()) {
       LOG_CHECK_FAIL("Total rewards", m_microblock->GetHeader().GetRewards(),
                      rewards);
