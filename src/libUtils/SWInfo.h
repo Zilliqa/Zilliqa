93f2371c98dea6a23cafe4c26ccb636cead0635b
Antonio Nunez <ansnunez@gmail.com>
2020-11-19 17:37:10 +0800

    To v7.0.0-beta.4

--- src/libUtils/SWInfo.h
+++ src/libUtils/SWInfo.h
@@ -22,7 +22,7 @@
 #include <iostream>
 #include "common/Serializable.h"
 
-const std::string VERSION_TAG = "v7.0.0-beta.3";
+const std::string VERSION_TAG = "v7.0.0-beta.4";
 const std::string ZILLIQA_BRAND = "Copyright (C) Zilliqa. Version " +
                                   VERSION_TAG + ".  <https://www.zilliqa.com/>";
 
