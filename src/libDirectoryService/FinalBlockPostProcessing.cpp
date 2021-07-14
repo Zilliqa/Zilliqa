8ad4d46bcac41754aec61e7bc4f8ebc3e4d14c90
chetanphirke <chetan@zilliqa.com>
2021-07-14 16:46:40 +0800

    Clean STL cache every tx epoch for normal nodes
    Clean STL cache every tx epoch for dsnodes

--- src/libDirectoryService/FinalBlockPostProcessing.cpp
+++ src/libDirectoryService/FinalBlockPostProcessing.cpp
@@ -228,8 +228,6 @@ void DirectoryService::ProcessFinalBlockConsensusWhenDone() {
     };
     DetachedFunction(1, writeStateToDisk);
 
-    // Clear STL memory cache
-    DetachedFunction(1, CommonUtils::ReleaseSTLMemoryCache);
   } else {
     // Coinbase
     SaveCoinbase(m_finalBlock->GetB1(), m_finalBlock->GetB2(),
@@ -244,6 +242,9 @@ void DirectoryService::ProcessFinalBlockConsensusWhenDone() {
       return;
     }
   }
+
+  // Clear STL memory cache
+  DetachedFunction(1, CommonUtils::ReleaseSTLMemoryCache);
 
   m_mediator.UpdateDSBlockRand();
   m_mediator.UpdateTxBlockRand();
