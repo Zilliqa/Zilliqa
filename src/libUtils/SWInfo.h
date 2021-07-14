4ebed77acae455ff6553e396d35f583c3ec4cf03
Sandip Bhoir <sandip@zilliqa.com>
2021-07-14 16:52:18 +0800

    To v8.0.6

--- src/libUtils/SWInfo.h
+++ src/libUtils/SWInfo.h
@@ -22,7 +22,7 @@
 #include <iostream>
 #include "common/Serializable.h"
 
-const std::string VERSION_TAG = "v8.0.5";
+const std::string VERSION_TAG = "v8.0.6";
 const std::string ZILLIQA_BRAND = "Copyright (C) Zilliqa. Version " +
                                   VERSION_TAG + ".  <https://www.zilliqa.com/>";
 
