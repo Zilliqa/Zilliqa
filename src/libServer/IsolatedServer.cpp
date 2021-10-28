a3b88bdfc1b30156511e5a71376aa3d0e7afde0e
Alvin <22679743+Kooriii@users.noreply.github.com>
2021-10-28 16:19:24 +0800

    Add priority mutex for GetSmartContractState API call
    Add reference in constructor
    
    Change to track time to acquire lock
    
    Add min and max performance timings
    
    Fix min to not be 0
    
    Test for crash
    
    Try new perfomance logging
    
    Fix PerfMarker crashing process
    
    Test writers priority
    
    Change exclusive locks for writers
    
    Prioritize GetSmartContractState calls
    
    Shift code line
    
    Prioritize DeserializeDelta and add wait condition for readers on same mutex
    
    Check on LOOKUP_NODE_MODE only if not revertible
    
    Remove mechanism in revertible path

--- src/libServer/IsolatedServer.cpp
+++ src/libServer/IsolatedServer.cpp
@@ -259,6 +259,9 @@ Json::Value IsolatedServer::CreateTransaction(const Json::Value& _json) {
     {
       shared_lock<shared_timed_mutex> lock(
           AccountStore::GetInstance().GetPrimaryMutex());
+      AccountStore::GetInstance().GetPrimaryWriteAccessCond().wait(lock, [] {
+        return AccountStore::GetInstance().GetPrimaryWriteAccess();
+      });
 
       const Account* sender = AccountStore::GetInstance().GetAccount(fromAddr);
 
@@ -303,6 +306,10 @@ Json::Value IsolatedServer::CreateTransaction(const Json::Value& _json) {
         {
           shared_lock<shared_timed_mutex> lock(
               AccountStore::GetInstance().GetPrimaryMutex());
+          AccountStore::GetInstance().GetPrimaryWriteAccessCond().wait(
+              lock, [] {
+                return AccountStore::GetInstance().GetPrimaryWriteAccess();
+              });
 
           const Account* account =
               AccountStore::GetInstance().GetAccount(tx.GetToAddr());
