a878554aab266bd714139acfc36baab7cf7e26c0
Alvin <22679743+Kooriii@users.noreply.github.com>
2021-12-29 16:36:56 +0800

    Fix condition for bypass

--- src/libLookup/Lookup.cpp
+++ src/libLookup/Lookup.cpp
@@ -2095,7 +2095,7 @@ void Lookup::RetrieveTxBlocks(vector<TxBlock>& txBlocks, uint64_t& lowBlockNum,
     highBlockNum = GetFetchRangeUpperBound();
   }
 
-  if (lowBlockNum < highBlockNum) {
+  if (lowBlockNum > highBlockNum) {
     lowBlockNum = highBlockNum;
   }
 
