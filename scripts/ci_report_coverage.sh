81b42b7def3f58b23de9eff66b4c0bfc2836bbb0
Joel Lim <56012934+joel-zilliqa@users.noreply.github.com>
2022-02-07 18:07:04 +0800

    rip forgot it was a script and not travis commands

--- scripts/ci_report_coverage.sh
+++ scripts/ci_report_coverage.sh
@@ -21,12 +21,12 @@ set -e
 
 # Create lcov report
 # capture coverage info
-- lcov --directory . --capture --output-file coverage.info
+lcov --directory . --capture --output-file coverage.info
 # filter out system and extra files.
 # To also not include test code in coverage add them with full path to the patterns: '*/tests/*'
-- lcov --remove coverage.info '/usr/*' "${HOME}"'/.cache/*' --output-file coverage.info
+lcov --remove coverage.info '/usr/*' "${HOME}"'/.cache/*' --output-file coverage.info
 # output coverage data for debugging (optional)
-- lcov --list coverage.info
+lcov --list coverage.info
 # Uploading to CodeCov
 # '-f' specifies file(s) to use and disables manual coverage gathering and file search which has already been done above
-- bash <(curl -s https://codecov.io/bash) -f coverage.info || echo "Codecov did not collect coverage reports"
+bash <(curl -s https://codecov.io/bash) -f coverage.info || echo "Codecov did not collect coverage reports"
