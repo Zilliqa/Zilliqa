41b5a874f2149e45c07151449cbf4cc057af2547
Antonio Nunez <ansnunez@gmail.com>
2022-02-08 12:28:42 +0800

    Fixed lcov report uploading to CodeCov

--- scripts/ci_report_coverage.sh
+++ scripts/ci_report_coverage.sh
@@ -19,5 +19,14 @@
 
 set -e
 
-# assume that it is run from project root directory
-bash <(curl -s https://codecov.io/bash) -g '/usr/**' -x gcov-5 -X gcovout || echo "Codecov did not collect coverage reports"
+# Create lcov report
+# capture coverage info
+lcov --directory . --capture --output-file coverage.info
+# filter out system and extra files.
+# To also not include test code in coverage add them with full path to the patterns: '*/tests/*'
+lcov --remove coverage.info '/usr/*' "${HOME}"'/.cache/*' --output-file coverage.info
+# output coverage data for debugging (optional)
+lcov --list coverage.info
+# Uploading to CodeCov
+# '-f' specifies file(s) to use and disables manual coverage gathering and file search which has already been done above
+bash <(curl -s https://codecov.io/bash) -f coverage.info || echo "Codecov did not collect coverage reports"
