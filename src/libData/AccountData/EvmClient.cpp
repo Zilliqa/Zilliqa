1e6ace442d8efa2a82fcca0069ad4479a7a50b34
stephen <stephen@zilliqa.com>
2022-05-17 20:05:42 +0100

    First set of real changes + new files

--- src/libData/AccountData/EvmClient.cpp
+++ src/libData/AccountData/EvmClient.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2020 Zilliqa
+* Copyright (C) 2022 Zilliqa
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -14,6 +14,7 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+
 #include "EvmClient.h"
 
 
--- src/libUtils/RunnerDetails.h
+++ src/libUtils/RunnerDetails.h
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2019 Zilliqa
+* Copyright (C) 2022 Zilliqa
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -14,7 +14,6 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-
 #ifndef ZILLIQA_RUNNERDETAILS_H
 #define ZILLIQA_RUNNERDETAILS_H
 
