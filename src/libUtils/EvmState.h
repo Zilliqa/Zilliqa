24aeed82384dc20d2955bb089c976308ddba57e8
stephen <stephen@zilliqa.com>
2022-05-31 21:38:12 +0100

    include gaurd, mot detected locally :-(

--- src/libUtils/EvmState.h
+++ src/libUtils/EvmState.h
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 #ifndef ZILLIQA_SRC_LIBUTILS_EVMSTATE_H_
 #define ZILLIQA_SRC_LIBUTILS_EVMSTATE_H_
 
