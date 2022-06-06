a0a20df61ae96e39d7704973a21310aec1a612a7
Steve White <stephen@zilliqa.com>
2022-06-06 14:39:15 +0100

    Updated copyright year

--- src/libUtils/EvmUtils.h
+++ src/libUtils/EvmUtils.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 202 Zilliqa
+ * Copyright (C) 2020 Zilliqa
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
