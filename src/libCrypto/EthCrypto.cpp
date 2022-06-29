0ce449f80c21e5d3100e8780e8d0e3d23d84cf8b
n-hutton <nathan@zilliqa.com>
2022-06-29 10:07:09 +0100

    PR feedback

--- src/libCrypto/EthCrypto.cpp
+++ src/libCrypto/EthCrypto.cpp
@@ -38,15 +38,19 @@ constexpr uint8_t prelude[] = {25,  69,  116, 104, 101, 114, 101, 117, 109,
                                32,  83,  105, 103, 110, 101, 100, 32,  77,
                                101, 115, 115, 97,  103, 101, 58,  10,  48};
 
+auto bnFree = [](BIGNUM* b) { BN_free(b); };
+auto ecFree = [](EC_GROUP* b) { EC_GROUP_free(b); };
+auto epFree = [](EC_POINT* b) { EC_POINT_free(b); };
+auto esFree = [](ECDSA_SIG* b) { ECDSA_SIG_free(b); };
+auto ekFree = [](EC_KEY* b) { EC_KEY_free(b); };
+
 // https://stackoverflow.com/questions/57385412/
 void SetOpensslSignature(const std::string& sSignatureInHex, ECDSA_SIG* pSign) {
   // Openssl uses raw pointers and macros for freeing but can force it into
   // smart pointers methodology
-  std::unique_ptr<BIGNUM, std::function<void(BIGNUM*)>> rr(
-      NULL, [](BIGNUM* b) { BN_free(b); });
+  std::unique_ptr<BIGNUM, decltype(bnFree)> rr(NULL, bnFree);
   BIGNUM* r_ptr = rr.get();
-  std::unique_ptr<BIGNUM, std::function<void(BIGNUM*)>> ss(
-      NULL, [](BIGNUM* b) { BN_free(b); });
+  std::unique_ptr<BIGNUM, decltype(bnFree)> ss(NULL, bnFree);
   BIGNUM* s_ptr = ss.get();
 
   std::string sSignatureR =
@@ -64,8 +68,7 @@ void SetOpensslSignature(const std::string& sSignatureInHex, ECDSA_SIG* pSign) {
 // should be hex \0 terminated string with no compression bit on it
 bool SetOpensslPublicKey(const char* sPubKeyString, EC_KEY* pKey) {
   // X co-ordinate
-  std::unique_ptr<BIGNUM, std::function<void(BIGNUM*)>> gx(
-      NULL, [](BIGNUM* b) { BN_free(b); });
+  std::unique_ptr<BIGNUM, decltype(bnFree)> gx(NULL, bnFree);
 
   BIGNUM* gx_ptr = gx.get();
 
@@ -100,12 +103,11 @@ bool SetOpensslPublicKey(const char* sPubKeyString, EC_KEY* pKey) {
 
   // Create a new curve group
   // Refactor: probably can have static/constexpr curve, need to check
-  std::unique_ptr<EC_GROUP, std::function<void(EC_GROUP*)>> curve_group(
-      EC_GROUP_new_by_curve_name(NID_secp256k1),
-      [](EC_GROUP* g) { EC_GROUP_free(g); });
+  std::unique_ptr<EC_GROUP, decltype(ecFree)> curve_group(
+      EC_GROUP_new_by_curve_name(NID_secp256k1), ecFree);
 
-  std::unique_ptr<EC_POINT, std::function<void(EC_POINT*)>> point(
-      EC_POINT_new(curve_group.get()), [](EC_POINT* g) { EC_POINT_free(g); });
+  std::unique_ptr<EC_POINT, decltype(epFree)> point(
+      EC_POINT_new(curve_group.get()), epFree);
 
   // This performs the decompression at the same time as setting the pubKey
   EC_POINT_set_compressed_coordinates_GFp(curve_group.get(), point.get(),
@@ -123,16 +125,16 @@ bool SetOpensslPublicKey(const char* sPubKeyString, EC_KEY* pKey) {
   }
 }
 
-bool VerifyEcdsaSecp256k1(const std::string&, const std::string& sSignature,
+bool VerifyEcdsaSecp256k1(const std::string& /*sRandomNumber*/,
+                          const std::string& sSignature,
                           const std::string& sDevicePubKeyInHex) {
-  std::unique_ptr<ECDSA_SIG, std::function<void(ECDSA_SIG*)>> zSignature(
-      ECDSA_SIG_new(), [](ECDSA_SIG* b) { ECDSA_SIG_free(b); });
+  std::unique_ptr<ECDSA_SIG, decltype(esFree)> zSignature(ECDSA_SIG_new(),
+                                                          esFree);
 
   SetOpensslSignature(sSignature, zSignature.get());
 
-  std::unique_ptr<EC_KEY, std::function<void(EC_KEY*)>> zPublicKey(
-      EC_KEY_new_by_curve_name(NID_secp256k1),
-      [](EC_KEY* b) { EC_KEY_free(b); });
+  std::unique_ptr<EC_KEY, decltype(ekFree)> zPublicKey(
+      EC_KEY_new_by_curve_name(NID_secp256k1), ekFree);
 
   if (!SetOpensslPublicKey(sDevicePubKeyInHex.c_str(), zPublicKey.get())) {
     LOG_GENERAL(WARNING, "Failed to get the public key from the hex input");
@@ -150,9 +152,8 @@ bool VerifyEcdsaSecp256k1(const std::string&, const std::string& sSignature,
 // per the 'Standards for Efficient Cryptography' specification
 std::string ToUncompressedPubKey(std::string const& pubKey) {
   // Create public key pointer
-  std::unique_ptr<EC_KEY, std::function<void(EC_KEY*)>> zPublicKey(
-      EC_KEY_new_by_curve_name(NID_secp256k1),
-      [](EC_KEY* b) { EC_KEY_free(b); });
+  std::unique_ptr<EC_KEY, decltype(ekFree)> zPublicKey(
+      EC_KEY_new_by_curve_name(NID_secp256k1), ekFree);
 
   // The +2 removes '0x' at the beginning of the string
   if (!SetOpensslPublicKey(pubKey.c_str() + 2, zPublicKey.get())) {
