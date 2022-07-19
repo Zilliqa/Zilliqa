0f1786347e7f4e60ed02d8bfe3ea54062d4619bc
n-hutton <nathan.hutton@fetch.ai>
2022-07-19 18:39:26 +0100

    v1 test

--- tests/PythonEthApi/test_api.py
+++ tests/PythonEthApi/test_api.py
@@ -1,5 +1,3 @@
-#
-#
 #  Copyright (C) 2019 Zilliqa
 #
 #  This program is free software: you can redistribute it and/or modify
@@ -18,23 +16,30 @@
 import sys
 import argparse
 import requests
+import os
 
 FILE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-TEST_RETURN_STATUS = 0
 
-def test_chainid(url: string):
+def test_chainid(url: str) -> bool:
     try:
-        response = requests.get(url + "/chain_id")
+        response = requests.post(url, json={"id": "1", "jsonrpc": "2.0", "method": "eth_chainId"})
+
+        if response.status_code != 200:
+            raise Exception(f"Bad status code {response.status_code} - {response.text}")
+
+        if "0x" not in response.json()["result"]:
+            raise Exception(f"Bad json or response {response.status_code} - {response.text}")
+
     except Exception as e:
-        print("Failed test test_chainid with error {e}")
-        TEST_RETURN_STATUS = 1
+        print(f"Failed test test_chainid with error: '{e}'")
+        return False
 
+    return True
 
-print(f"File path is {FILE_PATH}")
 
 def parse_commandline():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--api', type=str, help='API to test against')
+    parser.add_argument('--api', type=str, required=True, help='API to test against')
     return parser.parse_args()
 
 
@@ -43,9 +48,15 @@ def main():
 
     print(f"args are {args.api}")
 
-    test_chainid(api)
+    if args.api[-1] != '/':
+        args.api[-1].append('/')
 
-    sys.exit(TEST_RETURN_STATUS)
+    ret = test_chainid(args.api)
+    ret |= test_chainid(args.api)
+
+    if not ret:
+        print(f"Test failed")
+        sys.exit(1)
 
 
 if __name__ == '__main__':
