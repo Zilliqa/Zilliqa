2ae2ede3e850913a6ef908c704e445fe63e4f665
Steve White <stephen@zilliqa.com>
2022-07-25 15:31:29 +0100

    Just a reference master build

--- src/cmd/main.cpp
+++ src/cmd/main.cpp
@@ -176,6 +176,7 @@ int main(int argc, const char* argv[]) {
     INIT_EPOCHINFO_LOGGER("epochinfo", logpath.c_str());
 
     LOG_GENERAL(INFO, ZILLIQA_BRAND);
+    LOG_GENERAL(INFO, "Master TEst Build");
 
     if (SyncType::NEW_SYNC == syncType && CHAIN_ID == MAINNET_CHAIN_ID) {
       SWInfo::IsLatestVersion();
