152ed81b71a5439313e26fd3550d6e07293b5f45
Steve White <stephen@zilliqa.com>
2022-07-26 08:30:45 +0100

    Temporarily enable EVM so that tests pass.

--- src/libData/AccountData/AccountStore.cpp
+++ src/libData/AccountData/AccountStore.cpp
@@ -36,8 +36,9 @@ using namespace Contract;
 
 AccountStore::AccountStore() {
   m_accountStoreTemp = make_unique<AccountStoreTemp>(*this);
+  bool  ipcScillaInit = false;
 
-  if ((ENABLE_SC && ENABLE_EVM) || ISOLATED_SERVER) {
+  if (ENABLE_SC && (!LOOKUP_NODE_MODE || ISOLATED_SERVER)) {
     /// Scilla IPC Server
     /// clear path
     boost::filesystem::remove_all(SCILLA_IPC_SOCKET_PATH);
@@ -47,9 +48,10 @@ AccountStore::AccountStore() {
         SCILLA_SERVER_LOOP_WAIT_MICROSECONDS);
     m_scillaIPCServer =
         make_shared<ScillaIPCServer>(*m_scillaIPCServerConnector);
-    if (!LOOKUP_NODE_MODE || ISOLATED_SERVER) {
-      ScillaClient::GetInstance().Init();
-    }
+
+    ScillaClient::GetInstance().Init();
+    ipcScillaInit = true;
+
 
     if (m_scillaIPCServer == nullptr) {
       LOG_GENERAL(WARNING, "m_scillaIPCServer NULL");
@@ -65,6 +67,9 @@ AccountStore::AccountStore() {
   // EVM required to run on Lookup nodes too for view calls
   if (ENABLE_EVM) {
     EvmClient::GetInstance().Init();
+    if (not ipcScillaInit){
+      ScillaClient::GetInstance().Init();
+    }
   }
 }
 
