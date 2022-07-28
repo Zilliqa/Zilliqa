dd8a0368bc61e57f0c318802fe4fff2047775456
n-hutton <nathan@zilliqa.com>
2022-07-28 17:48:22 +0100

    style...

--- tests/Crypto/Test_EthCrypto.cpp
+++ tests/Crypto/Test_EthCrypto.cpp
@@ -40,17 +40,24 @@ BOOST_AUTO_TEST_SUITE(ethCryptotest)
  */
 BOOST_AUTO_TEST_CASE(TestEthTXParse) {
   // Parse a RLP Tx to extract the fields
-  std::string const rlp = "f86e01850d9e63a68c82520894673e5ef1ae0a2ef7d0714a96a734ffcd1d8a381f872386f26fc1000080830102bda0ef23fef2ffa3538b2c8204278ad0427491b5359c346c50a9236b9b554c45749ea02da3eba55c891dde91e73a312fd3748936fb7af8fb34c2f0fed8a9877f227e1d";
+  std::string const rlp =
+      "f86e01850d9e63a68c82520894673e5ef1ae0a2ef7d0714a96a734ffcd1d8a381f872386"
+      "f26fc1000080830102bda0ef23fef2ffa3538b2c8204278ad0427491b5359c346c50a923"
+      "6b9b554c45749ea02da3eba55c891dde91e73a312fd3748936fb7af8fb34c2f0fed8a987"
+      "7f227e1d";
 
   auto const result = parseRawTxFields(rlp);
 
-  BOOST_CHECK_EQUAL(DataConversion::Uint8VecToHexStrRet(result.toAddr), "673E5EF1AE0A2EF7D0714A96A734FFCD1D8A381F");
+  BOOST_CHECK_EQUAL(DataConversion::Uint8VecToHexStrRet(result.toAddr),
+                    "673E5EF1AE0A2EF7D0714A96A734FFCD1D8A381F");
   BOOST_CHECK_EQUAL(result.amount, uint128_t{"10000000000000000"});
   BOOST_CHECK_EQUAL(result.gasPrice, uint128_t{"58491905676"});
   BOOST_CHECK_EQUAL(result.code.size(), 0);
   BOOST_CHECK_EQUAL(result.data.size(), 0);
-  BOOST_CHECK_EQUAL(DataConversion::Uint8VecToHexStrRet(result.signature), "EF23FEF2FFA3538B2C8204278AD0427491B5359C346C50A9236B9B554C45749E2DA3EBA55C891DDE91E73A312FD3748936FB7AF8FB34C2F0FED8A9877F227E1D");
-
+  BOOST_CHECK_EQUAL(
+      DataConversion::Uint8VecToHexStrRet(result.signature),
+      "EF23FEF2FFA3538B2C8204278AD0427491B5359C346C50A9236B9B554C45749E2DA3EBA5"
+      "5C891DDE91E73A312FD3748936FB7AF8FB34C2F0FED8A9877F227E1D");
 }
 
 /**
@@ -59,15 +66,21 @@ BOOST_AUTO_TEST_CASE(TestEthTXParse) {
  * \details As above
  */
 BOOST_AUTO_TEST_CASE(TestRecoverECDSASig) {
-
   // Example RLP raw transaction, sent from metamask with chain ID 33101
   // Note: must use that chain id for this RLP example
-  // private key: a8b68f4800bc7513fca14a752324e41b2fa0a7c06e80603aac9e5961e757d906
-  // eth addr: 0x6cCAa29b6cD36C8238E8Fa137311de6153b0b4e7
-  // seed phrase: art rubber roof off fetch bulb board foot payment engage pyramid tiger
+  // private key:
+  // a8b68f4800bc7513fca14a752324e41b2fa0a7c06e80603aac9e5961e757d906 eth addr:
+  // 0x6cCAa29b6cD36C8238E8Fa137311de6153b0b4e7 seed phrase: art rubber roof off
+  // fetch bulb board foot payment engage pyramid tiger
 
-  std::string const rlp = "f86e01850d9e63a68c82520894673e5ef1ae0a2ef7d0714a96a734ffcd1d8a381f872386f26fc1000080830102bda0ef23fef2ffa3538b2c8204278ad0427491b5359c346c50a9236b9b554c45749ea02da3eba55c891dde91e73a312fd3748936fb7af8fb34c2f0fed8a9877f227e1d";
-  std::string const pubKey = "041419977507436A81DD0AC7BEB6C7C0DECCBF1A1A1A5E595F647892628A0F65BC9D19CBF0712F881B529D39E7F75D543DC3E646880A0957F6E6DF5C1B5D0EB278";
+  std::string const rlp =
+      "f86e01850d9e63a68c82520894673e5ef1ae0a2ef7d0714a96a734ffcd1d8a381f872386"
+      "f26fc1000080830102bda0ef23fef2ffa3538b2c8204278ad0427491b5359c346c50a923"
+      "6b9b554c45749ea02da3eba55c891dde91e73a312fd3748936fb7af8fb34c2f0fed8a987"
+      "7f227e1d";
+  std::string const pubKey =
+      "041419977507436A81DD0AC7BEB6C7C0DECCBF1A1A1A5E595F647892628A0F65BC9D19CB"
+      "F0712F881B529D39E7F75D543DC3E646880A0957F6E6DF5C1B5D0EB278";
 
   auto const result = RecoverECDSAPubSig(rlp, 33101);
   auto const restultStr = DataConversion::Uint8VecToHexStrRet(result);
