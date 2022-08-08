33907ea5911c21ae66452df0ebfbc7220a0bcaab
n-hutton <nathan.hutton@fetch.ai>
2022-08-08 13:34:31 +0100

    bump once more

--- scripts/integration_test.sh
+++ scripts/integration_test.sh
@@ -32,7 +32,7 @@ echo "Starting isolated server"
 sleep 15
 
 echo "Starting python test"
-sudo apt-get install python3-pip
+sudo apt-get install python3-pip python3-setuptools
 python3 -m pip install -r ./tests/PythonEthApi/requirements.txt
 python3 ./tests/PythonEthApi/test_api.py --api http://localhost:5555 > out.txt
 
