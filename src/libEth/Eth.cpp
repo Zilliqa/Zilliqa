6f372403a40880d607bafb4e16a6a7e96ad09e4d
n-hutton <nathan.cameron.hutton@gmail.com>
2022-08-25 07:03:33 +0100

    [master] Fixes after testing with web3 (#2905)

--- src/libEth/Eth.cpp
+++ src/libEth/Eth.cpp
@@ -16,6 +16,7 @@
  */
 
 #include "Eth.h"
+#include "common/Constants.h"
 #include "depends/common/RLP.h"
 #include "jsonrpccpp/server.h"
 #include "libUtils/DataConversion.h"
@@ -50,8 +51,8 @@ Json::Value populateReceiptHelper(std::string const &txnhash, bool success,
   ret["root"] =
       "0x0000000000000000000000000000000000000000000000000000000000001010";
   ret["status"] = success ? "0x1" : "0x0";
-  ret["to"] = to;                   // todo: fill
-  ret["transactionIndex"] = "0x0";  // todo: fill
+  ret["to"] = to;
+  ret["transactionIndex"] = "0x0";
 
   return ret;
 }
@@ -74,7 +75,7 @@ EthFields parseRawTxFields(std::string const &message) {
   int i = 0;
   // todo: checks on size of rlp stream etc.
 
-  ret.version = 65538;
+  ret.version = DataConversion::Pack(CHAIN_ID, 2);
 
   // RLP TX contains: nonce, gasPrice, gasLimit, to, value, data, v,r,s
   for (auto it = rlpStream1.begin(); it != rlpStream1.end();) {
