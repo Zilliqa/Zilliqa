f629c8d5beae42275586ef8c33edc135485dca14
n-hutton <nathan.cameron.hutton@gmail.com>
2022-08-25 16:20:27 +0100

    More 0x fixes against web3 (#2909)

--- src/libServer/JSONConversion.cpp
+++ src/libServer/JSONConversion.cpp
@@ -631,11 +631,14 @@ const Json::Value JSONConversion::convertTxtoEthJson(
     const TransactionWithReceipt& txn) {
   Json::Value retJson;
   retJson["from"] = "0x" + txn.GetTransaction().GetSenderAddr().hex();
-  retJson["gas"] = (boost::format("0x%x") % txn.GetTransactionReceipt().GetCumGas()).str();
+  retJson["gas"] =
+      (boost::format("0x%x") % txn.GetTransactionReceipt().GetCumGas()).str();
   // ethers also expectes gasLimit and ChainId
-  retJson["gasLimit"] = std::to_string(txn.GetTransactionReceipt().GetCumGas());
-  retJson["chainId"] = CHAIN_ID;
-  retJson["gasPrice"] = "0x" + txn.GetTransaction().GetGasPrice().str();
+  retJson["gasLimit"] =
+      (boost::format("0x%x") % txn.GetTransactionReceipt().GetCumGas()).str();
+  retJson["chainId"] = (boost::format("0x%x") % ETH_CHAINID_INT).str();
+  retJson["gasPrice"] =
+      (boost::format("0x%x") % txn.GetTransaction().GetGasPrice()).str();
   retJson["hash"] = "0x" + txn.GetTransaction().GetTranID().hex();
 
   // Concatenated Code and CallData form input entry in response json
@@ -659,9 +662,11 @@ const Json::Value JSONConversion::convertTxtoEthJson(
   retJson["input"] = inputField;
   // ethers also expects 'data' field
   retJson["data"] = inputField;
-  retJson["nonce"] = (boost::format("0x%x") % txn.GetTransaction().GetNonce()).str();
+  retJson["nonce"] =
+      (boost::format("0x%x") % txn.GetTransaction().GetNonce()).str();
   retJson["to"] = "0x" + txn.GetTransaction().GetToAddr().hex();
-  retJson["value"] = txn.GetTransaction().GetAmount().str();
+  retJson["value"] =
+      (boost::format("0x%x") % txn.GetTransaction().GetAmount()).str();
   return retJson;
 }
 
