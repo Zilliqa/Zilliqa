dfe5615fd71fd785fe7aa873dd1e0a7fcaaef596
artem <artgor4github@gmail.com>
2022-08-26 16:16:17 +0300

    ...

--- cmake/InstallSchnorr.cmake
+++ cmake/InstallSchnorr.cmake
@@ -9,74 +9,79 @@ message(STATUS "Building and installing Schnorr")
 
 # download, check and untar
 execute_process(
-    COMMAND git submodule update --init --recursive --remote src/depends/Schnorr
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    RESULT_VARIABLE SCHNORR_INSTALL_RET
-    OUTPUT_FILE ${SCHNORR_INSTALL_LOG}
-    ERROR_FILE ${SCHNORR_INSTALL_LOG}
+        COMMAND git submodule update --init --recursive --remote src/depends/Schnorr
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        RESULT_VARIABLE SCHNORR_INSTALL_RET
+        OUTPUT_FILE ${SCHNORR_INSTALL_LOG}
+        ERROR_FILE ${SCHNORR_INSTALL_LOG}
 )
 
-if(NOT "${SCHNORR_INSTALL_RET}" STREQUAL "0")
+if (NOT "${SCHNORR_INSTALL_RET}" STREQUAL "0")
     message(FATAL_ERROR "Error when building and installing Schnorr (1), see more in log ${SCHNORR_INSTALL_LOG}")
-endif()
+endif ()
+
+execute_process(
+        COMMAND sed -i 's/Boost_INCLUDE_DIR\\}/Boost_INCLUDE_DIRS\\}/g' ${SCHNORR_SOURCE_DIR}/CMakeLists.txt
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+)
 
 # generate build directory
 execute_process(
-    COMMAND ${CMAKE_COMMAND}
+        COMMAND ${CMAKE_COMMAND}
         -H${SCHNORR_SOURCE_DIR}
         -B${SCHNORR_BINARY_DIR}
         -DCMAKE_INSTALL_PREFIX=${SCHNORR_INSTALL_DIR}
         -DCMAKE_BUILD_TYPE:STRING=Release
         -Wno-dev
-    RESULT_VARIABLE SCHNORR_INSTALL_RET
-    OUTPUT_FILE ${SCHNORR_INSTALL_LOG}
-    ERROR_FILE ${SCHNORR_INSTALL_LOG}
+        RESULT_VARIABLE SCHNORR_INSTALL_RET
+        OUTPUT_FILE ${SCHNORR_INSTALL_LOG}
+        ERROR_FILE ${SCHNORR_INSTALL_LOG}
 )
 
-if(NOT "${SCHNORR_INSTALL_RET}" STREQUAL "0")
+if (NOT "${SCHNORR_INSTALL_RET}" STREQUAL "0")
     execute_process(
             COMMAND tail -n 100 ${SCHNORR_INSTALL_LOG}
             WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     )
     message(FATAL_ERROR "Error when building and installing Schnorr (2), see more in log ${SCHNORR_INSTALL_LOG}")
-endif()
+endif ()
 
 # build and install proto
 execute_process(
-    COMMAND ${CMAKE_COMMAND} --build ${SCHNORR_BINARY_DIR} -- -j${N}
-    RESULT_VARIABLE SCHNORR_INSTALL_RET
-    OUTPUT_FILE ${SCHNORR_INSTALL_LOG}
-    ERROR_FILE ${SCHNORR_INSTALL_LOG}
+        COMMAND ${CMAKE_COMMAND} --build ${SCHNORR_BINARY_DIR} -- -j${N}
+        RESULT_VARIABLE SCHNORR_INSTALL_RET
+        OUTPUT_FILE ${SCHNORR_INSTALL_LOG}
+        ERROR_FILE ${SCHNORR_INSTALL_LOG}
 )
 
-if(NOT "${SCHNORR_INSTALL_RET}" STREQUAL "0")
+if (NOT "${SCHNORR_INSTALL_RET}" STREQUAL "0")
     execute_process(
             COMMAND tail -n 200 ${SCHNORR_INSTALL_LOG}
             WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     )
     message(FATAL_ERROR "Error when building and installing Schnorr (3), see more in log ${SCHNORR_INSTALL_LOG}")
-endif()
+endif ()
 
 execute_process(
-    COMMAND ${CMAKE_COMMAND} --build ${SCHNORR_BINARY_DIR} --target install
-    RESULT_VARIABLE SCHNORR_INSTALL_RET
-    OUTPUT_FILE ${SCHNORR_INSTALL_LOG}
-    ERROR_FILE ${SCHNORR_INSTALL_LOG}
+        COMMAND ${CMAKE_COMMAND} --build ${SCHNORR_BINARY_DIR} --target install
+        RESULT_VARIABLE SCHNORR_INSTALL_RET
+        OUTPUT_FILE ${SCHNORR_INSTALL_LOG}
+        ERROR_FILE ${SCHNORR_INSTALL_LOG}
 )
 
-if(NOT "${SCHNORR_INSTALL_RET}" STREQUAL "0")
+if (NOT "${SCHNORR_INSTALL_RET}" STREQUAL "0")
     execute_process(
             COMMAND tail -n 200 ${SCHNORR_INSTALL_LOG}
             WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     )
     message(FATAL_ERROR "Error when building and installing Schnorr (4), see more in log ${SCHNORR_INSTALL_LOG}")
-endif()
+endif ()
 
 list(APPEND CMAKE_PREFIX_PATH ${SCHNORR_INSTALL_DIR})
 link_directories(${SCHNORR_INSTALL_DIR}/lib)
 
 find_path(
-    SCHNORR_INCLUDE_DIR
-    NAMES include/Schnorr.h
-    DOC "Schnorr include directory"
+        SCHNORR_INCLUDE_DIR
+        NAMES include/Schnorr.h
+        DOC "Schnorr include directory"
 )
