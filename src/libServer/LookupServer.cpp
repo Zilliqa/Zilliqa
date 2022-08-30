76574986b913fc1a8878aabbfba53b87edeaa0d6
Michel <107615499+moboware@users.noreply.github.com>
2022-08-30 10:20:42 +0200

    [master] Zil 4695 eth balance subunit must be in wei 10 18 (#2918)
    * ZIL-4695 GetEthBalance returns Eth-balance wei
    
    * ZIL-4695 return balance string in 0x format
    
    * ZIL-4695 added ZIL_SCALING_FACTOR in the   configuration file
    
    * ZIL-4695 ue zil scale constant
    
    * ZIL-4695 add missing space in command line
    
    * ZIL-4695 extra documentation
    
    * ZIL-4695 add zil scaling factor to constants.xml
    
    * ZIL-4695 remove debug logs

--- src/libServer/LookupServer.cpp
+++ src/libServer/LookupServer.cpp
@@ -1185,6 +1185,18 @@ Json::Value LookupServer::GetEthBlockCommon(const TxBlock& txBlock,
                                                  includeFullTransactions);
 }
 
+Json::Value LookupServer::GetEthBalance(const std::string& address) {
+  const auto balanceStr = this->GetBalance(address, true)["balance"].asString();
+
+  const uint256_t ethBalance =
+      std::strtoll(balanceStr.c_str(), nullptr, 16) * EVM_ZIL_SCALING_FACTOR;
+
+  std::stringstream strm;
+  strm << "0x" << std::hex << ethBalance;
+
+  return strm.str();
+}
+
 Json::Value LookupServer::GetEthBlockTransactionCountByHash(
     const std::string& inputHash) {
   try {
@@ -1508,7 +1520,7 @@ Json::Value LookupServer::GetTxBlockByNum(const string& blockNum,
     throw JsonRpcException(RPC_INVALID_PARAMS, "String not numeric");
   } catch (invalid_argument& e) {
     LOG_GENERAL(INFO, "[Error]" << e.what() << " Input: " << blockNum);
-    throw JsonRpcException(RPC_INVALID_PARAMS, "Invalid arugment");
+    throw JsonRpcException(RPC_INVALID_PARAMS, "Invalid argument");
   } catch (out_of_range& e) {
     LOG_GENERAL(INFO, "[Error]" << e.what() << " Input: " << blockNum);
     throw JsonRpcException(RPC_INVALID_PARAMS, "Out of range");
@@ -1560,7 +1572,7 @@ Json::Value LookupServer::GetLatestTxBlock() {
   return JSONConversion::convertTxBlocktoJson(Latest);
 }
 
-Json::Value LookupServer::GetBalance(const string& address) {
+Json::Value LookupServer::GetBalanceAndNonce(const string& address) {
   if (!LOOKUP_NODE_MODE) {
     throw JsonRpcException(RPC_INVALID_REQUEST, "Sent to a non-lookup");
   }
@@ -1682,7 +1694,7 @@ string LookupServer::GetEthCallImpl(const Json::Value& _json,
 // Get balance, but return the result as hex rather than decimal string
 Json::Value LookupServer::GetBalance(const string& address, bool noThrow) {
   try {
-    auto ret = this->GetBalance(address);
+    auto ret = this->GetBalanceAndNonce(address);
 
     // Will fit into 128 since that is the native zil balance
     // size
