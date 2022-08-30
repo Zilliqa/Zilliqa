f46becfa8c9a4994643318585475b963d0b3ae62
Valeriy Zamaraiev <valeriy@zilliqa.com>
2022-08-30 12:19:22 +0200

    clang-format-fix

--- tests/EvmLookupServer/Test_EvmLookupServer.cpp
+++ tests/EvmLookupServer/Test_EvmLookupServer.cpp
@@ -396,7 +396,8 @@ BOOST_AUTO_TEST_CASE(test_eth_coinbase) {
 
   LOG_GENERAL(DEBUG, response.asString());
 
-  BOOST_CHECK_EQUAL(response.asString(), "0x0000000000000000000000000000000000000000");
+  BOOST_CHECK_EQUAL(response.asString(),
+                    "0x0000000000000000000000000000000000000000");
 }
 
 BOOST_AUTO_TEST_CASE(test_net_version) {
