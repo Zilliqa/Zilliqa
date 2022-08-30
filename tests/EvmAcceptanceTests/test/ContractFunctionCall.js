d1cf451d58e49c47c864df9098b5b54b027e3f3f
Saeed Dadkhah <saeed@zilliqa.com>
2022-08-30 14:50:52 +0430

    Add contract function call tests

---
+++ tests/EvmAcceptanceTests/test/ContractFunctionCall.js
@@ -0,0 +1,138 @@
+const { expect } = require("chai");
+const { ZilliqaHelper } = require('../helper/ZilliqaHelper');
+
+describe("Contract Function Call with web3.js", function () {
+    describe("When caller is an externally owned account", function () {
+        describe("When it's a public view function", function () {
+            it("should return correct value when return type is uint256")
+            it("should return correct value when return type is address")
+            it("should return correct value when return type is string")
+            it("should return correct value when return type is enum")
+        });
+        describe("When it's a external view function", function () {
+        });
+        describe("When it's a private view function", function () {
+        });
+        describe("When it's a internal view function", function () {
+        });
+
+        describe("When it's a public pure function", function () {
+        });
+        describe("When it's a external pure function", function () {
+        });
+        describe("When it's a private pure function", function () {
+        });
+        describe("When it's a internal pure function", function () {
+        });
+
+        describe("When it's a public payable function", function () {
+        });
+        describe("When it's a external payable function", function () {
+        });
+        describe("When it's a private payable function", function () {
+        });
+        describe("When it's a internal payable function", function () {
+        });
+    })
+
+    describe("When caller is a contract account", function () {
+        describe("When it's a public view function", function () {
+            it("should return correct value when return type is uint256")
+            it("should return correct value when return type is address")
+            it("should return correct value when return type is string")
+            it("should return correct value when return type is enum")
+        });
+        describe("When it's a external view function", function () {
+        });
+        describe("When it's a private view function", function () {
+        });
+        describe("When it's a internal view function", function () {
+        });
+
+        describe("When it's a public pure function", function () {
+        });
+        describe("When it's a external pure function", function () {
+        });
+        describe("When it's a private pure function", function () {
+        });
+        describe("When it's a internal pure function", function () {
+        });
+
+        describe("When it's a public payable function", function () {
+        });
+        describe("When it's a external payable function", function () {
+        });
+        describe("When it's a private payable function", function () {
+        });
+        describe("When it's a internal payable function", function () {
+        });
+    })
+})
+
+describe("Contract Function Call with ethers.js", function () {
+    describe("When caller is an externally owned account", function () {
+        describe("When it's a public view function", function () {
+            it("should return correct value when return type is uint256")
+            it("should return correct value when return type is address")
+            it("should return correct value when return type is string")
+            it("should return correct value when return type is enum")
+        });
+        describe("When it's a external view function", function () {
+        });
+        describe("When it's a private view function", function () {
+        });
+        describe("When it's a internal view function", function () {
+        });
+
+        describe("When it's a public pure function", function () {
+        });
+        describe("When it's a external pure function", function () {
+        });
+        describe("When it's a private pure function", function () {
+        });
+        describe("When it's a internal pure function", function () {
+        });
+
+        describe("When it's a public payable function", function () {
+        });
+        describe("When it's a external payable function", function () {
+        });
+        describe("When it's a private payable function", function () {
+        });
+        describe("When it's a internal payable function", function () {
+        });
+    })
+
+    describe("When caller is a contract account", function () {
+        describe("When it's a public view function", function () {
+            it("should return correct value when return type is uint256")
+            it("should return correct value when return type is address")
+            it("should return correct value when return type is string")
+            it("should return correct value when return type is enum")
+        });
+        describe("When it's a external view function", function () {
+        });
+        describe("When it's a private view function", function () {
+        });
+        describe("When it's a internal view function", function () {
+        });
+
+        describe("When it's a public pure function", function () {
+        });
+        describe("When it's a external pure function", function () {
+        });
+        describe("When it's a private pure function", function () {
+        });
+        describe("When it's a internal pure function", function () {
+        });
+
+        describe("When it's a public payable function", function () {
+        });
+        describe("When it's a external payable function", function () {
+        });
+        describe("When it's a private payable function", function () {
+        });
+        describe("When it's a internal payable function", function () {
+        });
+    })
+})
