25dc81b802a55fb226a0ef564745614ac9b8fdbc
Saeed Dadkhah <saeed@zilliqa.com>
2022-08-31 16:07:18 +0430

    Add ganache network

--- tests/EvmAcceptanceTests/hardhat.config.js
+++ tests/EvmAcceptanceTests/hardhat.config.js
@@ -4,8 +4,16 @@ require("@nomiclabs/hardhat-web3");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
-  defaultNetwork: "isolated_server",
+  defaultNetwork: "ganache",
   networks: {
+    ganache: {
+      url: "http://127.0.0.1:7545",
+      chainId: 1337,
+      accounts: [
+        "c95690aed4461afd835b17492ff889af72267a8bdf7d781e305576cd8f7eb182",
+        "05751249685e856287c2b2b9346e70a70e1d750bc69a35cef740f409ad0264ad"
+      ]
+    },
     zilliqa: {
       url: "https://master-mod-api.dev.z7a.xyz/",
       accounts: ["d96e9eb5b782a80ea153c937fa83e5948485fbfc8b7e7c069d7b914dbc350aba"],
