595d21d873c86da1c73428dfdea44998c51f4a87
Valeriy Zamaraiev <valeriy@zilliqa.com>
2022-09-01 10:13:04 +0200

    Remove dependency on pyzil. Normalize amounts in tests

--- tests/evm_ds_test/run_test_loop.sh
+++ tests/evm_ds_test/run_test_loop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (C) 2019 Zilliqa
+# Copyright (C) 2022 Zilliqa
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- tests/evm_ds_test/tests/conftest.py
+++ tests/evm_ds_test/tests/conftest.py
@@ -19,6 +19,7 @@ import os
 import numpy
 import random as rand
 
+
 @pytest.fixture(scope="session", autouse=True)
 def random():
     seed = int(os.environ.get("TESTSEED", "0"))
