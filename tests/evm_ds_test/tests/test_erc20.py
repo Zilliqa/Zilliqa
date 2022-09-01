39b14181edaf5d15f73ea1ed4561c027f52f0196
Valeriy Zamaraiev <valeriy@zilliqa.com>
2022-09-01 11:46:41 +0200

    [master] Remove dependency on pyzil. Normalize amounts in tests (#2932)
    * Remove dependency on pyzil. Normalize amounts in tests
    
    * remove code for real
    
    * get_transaction_count > get_nonce

--- tests/evm_ds_test/tests/test_erc20.py
+++ tests/evm_ds_test/tests/test_erc20.py
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import time
 import pytest
 from eth_utils import decode_hex, to_checksum_address as eth_to_checksum_address
 
