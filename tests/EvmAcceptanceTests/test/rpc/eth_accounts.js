adc274f402980ff46dea3c91bdad65dff0d3395d
Saeed Dadkhah <saeed@zilliqa.com>
2022-09-02 13:47:37 +0430

    port accounts

---
+++ tests/EvmAcceptanceTests/test/rpc/eth_accounts.js
@@ -0,0 +1,22 @@
+var assert = require('chai').assert,
+    Utils = require('../../helper/Utils')
+
+// METHOD
+var method = 'eth_accounts';
+
+// TEST
+var asyncTest = async function(done){
+    await Utils.callEthMethod(method, 1, [], function(result, status) {
+        assert.equal(status, 200, 'has status code');
+        assert.property(result, 'result', (result.error) ? result.error.message : 'error');
+        assert.isArray(result.result, 'is array');
+        // we should have at least one account here (coinbase)
+        assert.isTrue(Utils.isAddress(result.result[0]));
+    });
+};
+
+describe(method, function(){
+    it('should return an array with accounts', async function () {
+        await asyncTest();
+    });
+});
