ab034b28492e48e5bbd3d55d6335ac493cf51567
Saeed Dadkhah <saeed@zilliqa.com>
2022-09-02 14:42:50 +0430

    port get balance

--- tests/EvmAcceptanceTests/test/rpc/eth_getBalance.js
+++ tests/EvmAcceptanceTests/test/rpc/eth_getBalance.js
@@ -1,24 +1,46 @@
-const { ZilliqaHelper } = require('../../helper/ZilliqaHelper');
-assert = require('chai').assert;
+const { assert } = require('chai')
+const Utils = require('../../helper/Utils')
 
-const METHOD = 'eth_getBalance';
+// METHOD
+var method = 'eth_getBalance';
 
-describe("Calling " + METHOD, function () {
-    const helper = new ZilliqaHelper()
 
-    it("should return the balance as specified in the ethereum protocol", async function() {
-        const expectedResult = 0
-        await helper.callEthMethod(METHOD, 1, [
-            "0xF0C05464f12cB2a011d21dE851108a090C95c755", "latest"
-        ],(result, status) => {
+// TEST
+var asyncTest = async function(params, expectedResult){
+    await Utils.callEthMethod(method, 1, params, function(result, status) {
+        console.log(result)
+        
+        assert.equal(status, 200, 'has status code');
+        assert.property(result, 'result', (result.error) ? result.error.message : 'error');
+        assert.isString(result.result, 'is string');
+        assert.match(result.result, /^0x/, 'should be HEX starting with 0x');
+        assert.isNumber(+result.result, 'can be converted to a number');
+
+        assert.equal(+result.result, expectedResult, 'should have balance '+ expectedResult);
+        console.log(result)
+    });
+};
+
+var asyncErrorTest = async function(){
+    await Utils.callEthMethod(method, 1, [],
+        function(result, status) {
             assert.equal(status, 200, 'has status code');
-            assert.property(result, 'result', (result.error) ? result.error.message : 'error');
-            assert.isString(result.result, 'is string');
-            assert.match(result.result, /^0x/, 'should be HEX starting with 0x');
-            assert.isNumber(+result.result, 'can be converted to a number');
+            assert.property(result, 'error');
+            assert.equal(result.error.code, -32602);
+        });
+};
 
-            assert.equal(+result.result, expectedResult, 'should have balance '+ expectedResult);
-        })
-    })
+describe(method, async function () {
+    const key = "F0C05464f12cB2a011d21dE851108a090C95c755"
+    it('should return the correct balance at defaultBlock "latest" at address 0x' + key, async function () {
+        await asyncTest(['0x' + key, 'latest'], 98475700180000000000)
+    });
 
-})
+    it('should return the correct balance at defaultBlock 0 at address 0x' + key, async function () {
+        await asyncTest(['0x' + key, '0x0'], 0);
+    });
+
+    it('should return an error when no parameter is passed', async function () {
+        await asyncErrorTest();
+    });
+});
