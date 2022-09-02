b2ac9cacb08fe9d363c157b57141d113c40cb628
michel <michel@zilliqa.com>
2022-09-02 15:21:01 +0200

    ZIL-4707 added get gas price api call

---
+++ tests/EvmAcceptanceTests/test/rpc/eth_gasPrice.js
@@ -0,0 +1,24 @@
+const { ZilliqaHelper } = require('../../helper/ZilliqaHelper');
+assert = require('chai').assert;
+
+const METHOD = 'eth_gasPrice';
+
+describe("Calling " + METHOD, function () {
+  const helper = new ZilliqaHelper()
+
+  it("should return the gasPrice as specified in the ethereum protocol", async function () {
+    const expectedGasPrice = 2000000000 // default ganache gas price in wei
+
+    await helper.callEthMethod(METHOD, 1, [
+    ], (result, status) => {
+      assert.equal(status, 200, 'has status code');
+      assert.property(result, 'result', (result.error) ? result.error.message : 'error');
+      assert.isString(result.result, 'is string');
+      assert.match(result.result, /^0x/, 'should be HEX starting with 0x');
+      assert.isNumber(+result.result, 'can be converted to a number');
+
+      assert.equal(+result.result, expectedGasPrice, 'should have a gas price' + expectedGasPrice);
+    })
+  })
+
+})
