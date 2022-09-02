bea6d539b0d264e0e08c4dbd7c32286b02a02052
michel <michel@zilliqa.com>
2022-09-02 12:47:57 +0200

    ZIL-4724 add a reasonable balance value to get the current eth balance in wei

--- tests/EvmAcceptanceTests/test/rpc/eth_getBalance.js
+++ tests/EvmAcceptanceTests/test/rpc/eth_getBalance.js
@@ -6,18 +6,18 @@ const METHOD = 'eth_getBalance';
 describe("Calling " + METHOD, function () {
     const helper = new ZilliqaHelper()
 
-    it("should return the balance as specified in the ethereum protocol", async function() {
-        const expectedResult = 0
+    it("should return the balance as specified in the ethereum protocol", async function () {
+        const expectedResult = 0xDE0B6B3A7640000 // should be 1 eth in wei         //// todo calculate balance from 1 / 10^18 in a bignumber
         await helper.callEthMethod(METHOD, 1, [
             "0xF0C05464f12cB2a011d21dE851108a090C95c755", "latest"
-        ],(result, status) => {
+        ], (result, status) => {
             assert.equal(status, 200, 'has status code');
             assert.property(result, 'result', (result.error) ? result.error.message : 'error');
             assert.isString(result.result, 'is string');
             assert.match(result.result, /^0x/, 'should be HEX starting with 0x');
             assert.isNumber(+result.result, 'can be converted to a number');
 
-            assert.equal(+result.result, expectedResult, 'should have balance '+ expectedResult);
+            assert.equal(+result.result, expectedResult, 'should have balance ' + expectedResult);
         })
     })
 
