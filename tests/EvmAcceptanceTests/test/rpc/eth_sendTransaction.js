20764041eda18b54c090115eff3fa201d36d3150
michel <michel@zilliqa.com>
2022-09-08 11:53:03 +0200

    ZIL-4740 cleanup

--- tests/EvmAcceptanceTests/test/rpc/eth_sendTransaction.js
+++ tests/EvmAcceptanceTests/test/rpc/eth_sendTransaction.js
@@ -12,12 +12,12 @@ describe("Calling " + METHOD, function () {
     })
 
     it("should return a send transaction", async function () {
-      await helper.callEthMethod(METHOD, 1, [{ //
-        "data": "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675", //
-        "from": "0xF0C05464f12cB2a011d21dE851108a090C95c755", //
-        "gas": "", //
-        "gasPrice": "", //
-        "to": "0xd46e8dd67c5d32be8058bb8eb970870f07244567", //
+      await helper.callEthMethod(METHOD, 1, [{
+        "data": "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675",
+        "from": "0xF0C05464f12cB2a011d21dE851108a090C95c755",
+        "gas": "",
+        "gasPrice": "",
+        "to": "0xd46e8dd67c5d32be8058bb8eb970870f07244567",
         "value": "0x9184e72a"
       }],
         (result, status) => {
@@ -30,5 +30,4 @@ describe("Calling " + METHOD, function () {
         })
     })
   });
-
 })
