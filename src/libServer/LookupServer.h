84f7819d3fdb42dbb481790859685514567dfe98
Sasha Zezulinsky <alexander.zezulinsky@gmail.com>
2022-09-09 10:26:38 +0200

    fix function return type

--- src/libServer/LookupServer.h
+++ src/libServer/LookupServer.h
@@ -727,8 +727,7 @@ class LookupServer : public Server,
   std::string GetNumTxnsDSEpoch();
   std::string GetNumTxnsTxEpoch();
 
-  void GetBlockByTransactionHash(const std::string& txnhash,
-                                 TxBlock& txBlock);
+  TxBlock GetBlockByTransactionHash(const std::string& txnhash);
   // Eth calls
   Json::Value GetEthTransactionReceipt(const std::string& txnhash);
   Json::Value GetEthBlockByNumber(const std::string& blockNumberStr,
