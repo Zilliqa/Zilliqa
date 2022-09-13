53b5deb7c8e5edfb2a64b972fd18a349efc02c8c
michel <michel@zilliqa.com>
2022-09-13 09:05:41 +0200

    ZIL-4796

--- tests/EvmAcceptanceTests/test/rpc/eth_getBalance.js
+++ tests/EvmAcceptanceTests/test/rpc/eth_getBalance.js
@@ -1,57 +1,120 @@
 const helper = require('../../helper/GeneralHelper');
+const { ZilliqaHelper } = require('../../helper/ZilliqaHelper');
+const { ethers, web3 } = require("hardhat")
 assert = require('chai').assert;
 
 const METHOD = 'eth_getBalance';
+const initialBalance = 10_000;
+let receiverAccount;
 
-describe("Calling " + METHOD, function () {
-    it("should return the balance as specified in the ethereum protocol", async function () {
-        const expectedBalance = 1000000 * (10 ** 12) // should be 1,000,000 ZIL in wei
+describe("Calling " + METHOD, async function () {
+    this.beforeEach("Initialize unique receiver account with balance from sender account", async function () {
+        let zHelper = new ZilliqaHelper();
+        // check sender account has enough balance to move
+        const senderAddress = zHelper.getPrimaryAccountAddress();
+        console.log("Primary address:", senderAddress);
+        const senderBalance = await web3.eth.getBalance(senderAddress);
+        console.log("Sender balance:", senderBalance);
+        assert.isAbove(ethers.BigNumber.from(senderBalance), initialBalance, 'sender has enough balance to move');
 
-        await helper.callEthMethod(METHOD, 1, [
-            "0xF0C05464f12cB2a011d21dE851108a090C95c755", // public address
-            "latest"],
-            (result, status) => {
-                assert.equal(status, 200, 'has status code');
-                assert.property(result, 'result', (result.error) ? result.error.message : 'error');
-                assert.isString(result.result, 'is string');
-                assert.match(result.result, /^0x/, 'should be HEX starting with 0x');
-                assert.isNumber(+result.result, 'can be converted to a number');
+        // create unique receiver account
+        receiverAccount = await web3.eth.accounts.create();
+        console.log("New Account address:", receiverAccount.address);
 
-                assert.equal(+result.result, expectedBalance, 'should have balance ' + expectedBalance);
-            })
+        await zHelper.moveFunds(initialBalance, receiverAccount.address);
+    });
 
-        await helper.callEthMethod(METHOD, 1, [
-            "0xF0C05464f12cB2a011d21dE851108a090C95c755", // public address
-            "earliest"],
-            (result, status) => {
-                assert.equal(status, 200, 'has status code');
-                assert.property(result, 'result', (result.error) ? result.error.message : 'error');
-                assert.isString(result.result, 'is string');
-                assert.match(result.result, /^0x/, 'should be HEX starting with 0x');
-                assert.isNumber(+result.result, 'can be converted to a number');
+    describe("When tag is 'latest'", async function () {
+        it("should return the latest balance as specified in the ethereum protocol", async function () {
+            console.log("Requesting balance for account:", receiverAccount.address);
+            await helper.callEthMethod(METHOD, 1, [
+                receiverAccount.address, // public address
+                "latest"],
+                (result, status) => {
+                    console.log(result);
 
-                assert.equal(+result.result, expectedBalance, 'should have balance ' + expectedBalance);
-            })
-
-        await helper.callEthMethod(METHOD, 1, [
-            "0xF0C05464f12cB2a011d21dE851108a090C95c755", // public address
-            "pending"],
-            (result, status) => {
-                assert.equal(status, 200, 'has status code');
-                assert.property(result, 'result', (result.error) ? result.error.message : 'error');
-                assert.isString(result.result, 'is string');
-                assert.match(result.result, /^0x/, 'should be HEX starting with 0x');
-                assert.isNumber(+result.result, 'can be converted to a number');
-
-                assert.equal(+result.result, expectedBalance, 'should have balance ' + expectedBalance);
-            })
-
-        await helper.callEthMethod(METHOD, 1, [
-            "0xF0C05464f12cB2a011d21dE851108a090C95c755",   // public address
-            "unknown tag"],                                 // not supported tag should give an error
-            (result, status) => {
-                assert.equal(status, 200, 'has status code');
-                // todo check the error result to expect              assert.property(result.error, 'error');
-            })
+                    assert.equal(status, 200, 'has status code');
+                    assert.property(result, 'result', (result.error) ? result.error.message : 'error');
+                    assert.isString(result.result, 'is string');
+                    assert.match(result.result, /^0x/, 'should be HEX starting with 0x');
+                    assert.isNumber(+result.result, 'can be converted to a number');
+                    assert.equal(+result.result, initialBalance, 'Has result:' + result + ' should have balance ' + initialBalance);
+                })
+        })
     })
+
+    describe("When tag is 'earliest'", async function () {
+        describe("When on Zilliqa network", async function () {
+            before(async function () {
+                if (!helper.isZilliqaNetworkSelected()) {
+                    this.skip();
+                }
+            });
+
+            it("should return the earliest balance as specified in the ethereum protocol", async function () {
+                await helper.callEthMethod(METHOD, 1, [
+                    receiverAccount.address, // public address
+                    "earliest"],
+                    (result, status) => {
+                        console.log(result);
+                        assert.equal(status, 200, 'has status code');
+                        assert.property(result, 'result', (result.error) ? result.error.message : 'error');
+                        assert.isString(result.result, 'is string');
+                        assert.match(result.result, /^0x/, 'should be HEX starting with 0x');
+                        assert.isNumber(+result.result, 'can be converted to a number');
+
+                        assert.equal(+result.result, initialBalance, 'Has result:' + result + ' should have balance ' + initialBalance);
+                    })
+            })
+        })
+    })
+
+    describe("When tag is 'pending'", function () {
+        describe("When on Zilliqa network", async function () {
+            before(async function () {
+                if (!helper.isZilliqaNetworkSelected()) {
+                    this.skip();
+                }
+            });
+
+            it("should return the pending balance as specified in the ethereum protocol", async function () {
+                await helper.callEthMethod(METHOD, 1, [
+                    receiverAccount.address, // public address
+                    "pending"],
+                    (result, status) => {
+                        console.log(result);
+                        assert.equal(status, 200, 'has status code');
+                        assert.property(result, 'result', (result.error) ? result.error.message : 'error');
+                        assert.isString(result.result, 'is string');
+                        assert.match(result.result, /^0x/, 'should be HEX starting with 0x');
+                        assert.isNumber(+result.result, 'can be converted to a number');
+
+                        assert.equal(+result.result, initialBalance, 'Has result:' + result + ' should have balance ' + initialBalance);
+                    })
+            })
+        })
+    })
+
+    //    describe("When getBalance tag is invalid tag", function () {
+    //        let expectedErrorMessage = ""
+    //        before(async function () {
+    //            if (helper.isZilliqaNetworkSelected()) {
+    //                expectedErrorMessage = "Unable To Process, invalid tag"
+    //            }
+    //            else {
+    //                expectedErrorMessage = "Cannot wrap string value \"unknown tag\" as a json-rpc type; strings must be prefixed with \"0x\"."
+    //            }
+    //        })
+    //
+    //        it("should return an error requesting the balance due to invalid tag", async function () {
+    //            await helper.callEthMethod(METHOD, 1, [
+    //                "0x2a2ce8aFd5AfBBFb76B720D5e6048EFb056177E5",   // public address
+    //                "unknown tag"],                                 // not supported tag should give an error
+    //                (result, status) => {
+    //                    assert.equal(status, 200, 'has status code');
+    //                    assert.equal(result.error.code, -32700);
+    //                    assert.equal(result.error.message, expectedErrorMessage);
+    //                })
+    //        })
+    //    })
 })
