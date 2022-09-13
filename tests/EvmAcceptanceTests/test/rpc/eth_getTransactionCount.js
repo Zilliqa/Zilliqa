08efcab052c39bdcf83785fd3f934e0f9381d53a
michel <michel@zilliqa.com>
2022-09-13 10:23:51 +0200

    ZIL-4734 test for all tag types

--- tests/EvmAcceptanceTests/test/rpc/eth_getTransactionCount.js
+++ tests/EvmAcceptanceTests/test/rpc/eth_getTransactionCount.js
@@ -7,8 +7,8 @@ const METHOD = 'eth_getTransactionCount';
 let zHelper = new ZilliqaHelper();
 
 describe("Calling " + METHOD, function () {
-  // Test that we get no error and that the api call returns a transaction count greater than 0.
-  it("Should return the latest transaction count greater than 0", async function () {
+  // Test that we get no error and that the api call returns a transaction count >= 0.
+  it("Should return the latest transaction count >= 0", async function () {
     await helper.callEthMethod(METHOD, 1, [zHelper.getPrimaryAccountAddress(), "latest"],
       (result, status) => {
         console.log(result);
@@ -19,7 +19,37 @@ describe("Calling " + METHOD, function () {
         assert.isNumber(+result.result, 'can be converted to a number');
 
         const expectedTransactionCount = 0;
-        assert.isAbove(+result.result, expectedTransactionCount, 'should have a transaction count greater than:' + expectedTransactionCount);
+        assert.isAtLeast(+result.result, expectedTransactionCount, 'should have a transaction count >=:' + expectedTransactionCount);
+      })
+  })
+
+  it("Should return the pending transaction count >= 0", async function () {
+    await helper.callEthMethod(METHOD, 1, [zHelper.getPrimaryAccountAddress(), "pending"],
+      (result, status) => {
+        console.log(result);
+        assert.equal(status, 200, 'has status code');
+        assert.property(result, 'result', (result.error) ? result.error.message : 'error');
+        assert.isString(result.result, 'is string');
+        assert.match(result.result, /^0x/, 'should be HEX starting with 0x');
+        assert.isNumber(+result.result, 'can be converted to a number');
+
+        const expectedTransactionCount = 0;
+        assert.isAtLeast(+result.result, expectedTransactionCount, 'should have a transaction count >=:' + expectedTransactionCount);
+      })
+  })
+
+  it("Should return the earliest transaction count >= 0", async function () {
+    await helper.callEthMethod(METHOD, 1, [zHelper.getPrimaryAccountAddress(), "earliest"],
+      (result, status) => {
+        console.log(result);
+        assert.equal(status, 200, 'has status code');
+        assert.property(result, 'result', (result.error) ? result.error.message : 'error');
+        assert.isString(result.result, 'is string');
+        assert.match(result.result, /^0x/, 'should be HEX starting with 0x');
+        assert.isNumber(+result.result, 'can be converted to a number');
+
+        const expectedTransactionCount = 0;
+        assert.isAtLeast(+result.result, expectedTransactionCount, 'should have a transaction count >=:' + expectedTransactionCount);
       })
   })
 })
