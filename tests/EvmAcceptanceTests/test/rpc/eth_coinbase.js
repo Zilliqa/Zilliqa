459178c801469aea535b22ce79f496012e68faf7
michel <michel@zilliqa.com>
2022-09-20 15:36:30 +0200

    ZIL-4762 only on zilliqa

--- tests/EvmAcceptanceTests/test/rpc/eth_coinbase.js
+++ tests/EvmAcceptanceTests/test/rpc/eth_coinbase.js
@@ -4,19 +4,27 @@ assert = require('chai').assert;
 const METHOD = 'eth_coinbase';
 
 describe("Calling " + METHOD, function () {
-  it("should return the coin base", async function () {
+  describe("When on Zilliqa network", async function () {
+    before(async function () {
+      if (!helper.isZilliqaNetworkSelected()) {
+        this.skip();
+      }
+    })
 
-    await helper.callEthMethod(METHOD, 1, [],
-      (result, status) => {
-        console.log(result);
-        assert.equal(status, 200, 'has status code');
-        assert.property(result, 'result', (result.error) ? result.error.message : 'error');
-        assert.isString(result.result, 'is string');
-        assert.match(result.result, /^0x/, 'should be HEX starting with 0x');
-        assert.isNumber(+result.result, 'can be converted to a number');
+    it("should return the coin base", async function () {
 
-        const expectedCoinBase = "0x0000000000000000000000000000000000000000"
-        assert.equal(+result.result, expectedCoinBase, 'should have a coin base ' + expectedCoinBase);
-      })
+      await helper.callEthMethod(METHOD, 1, [],
+        (result, status) => {
+          console.log(result);
+          assert.equal(status, 200, 'has status code');
+          assert.property(result, 'result', (result.error) ? result.error.message : 'error');
+          assert.isString(result.result, 'is string');
+          assert.match(result.result, /^0x/, 'should be HEX starting with 0x');
+          assert.isNumber(+result.result, 'can be converted to a number');
+
+          const expectedCoinBase = "0x0000000000000000000000000000000000000000"
+          assert.equal(+result.result, expectedCoinBase, 'should have a coin base ' + expectedCoinBase);
+        })
+    })
   })
 })
