51754b8edfeeebf0e041663a89a8c9ece04cdcb3
bzawisto <bartosz@zilliqa.com>
2022-09-23 09:35:10 +0000

    Test 4

--- tests/EvmAcceptanceTests/helper/Web3Helper.js
+++ tests/EvmAcceptanceTests/helper/Web3Helper.js
@@ -1,21 +1,24 @@
-const { web3 } = require("hardhat");
-const general_helper = require('./GeneralHelper')
+const {web3} = require("hardhat");
+const general_helper = require("./GeneralHelper");
 
 var web3_helper = {
-    primaryAccount: web3.eth.accounts.privateKeyToAccount(general_helper.getPrivateAddressAt(0)),
-    auxiliaryAccount: web3.eth.accounts.privateKeyToAccount(general_helper.getPrivateAddressAt(1)),
+  primaryAccount: web3.eth.accounts.privateKeyToAccount(general_helper.getPrivateAddressAt(0)),
+  auxiliaryAccount: web3.eth.accounts.privateKeyToAccount(general_helper.getPrivateAddressAt(1)),
 
-    deploy: async function(contractName, ...arguments) {
-        const ContractRaw = hre.artifacts.readArtifactSync(contractName)
-        const contract = new web3.eth.Contract(ContractRaw.abi)
-        return contract.deploy({ data: ContractRaw.bytecode, arguments: arguments })
-            .send({
-                from: this.primaryAccount.address,
-                ...(general_helper.isZilliqaNetworkSelected()) && { gas: 35_000 },
-                ...(general_helper.isZilliqaNetworkSelected()) && { gasPrice: 2_000_000_000_000 },
-            })
-            .on('error', function (error) { console.log(error) })
-    }
-}
+  deploy: async function (contractName, ...arguments) {
+    const ContractRaw = hre.artifacts.readArtifactSync(contractName);
+    const contract = new web3.eth.Contract(ContractRaw.abi);
+    return contract
+      .deploy({data: ContractRaw.bytecode, arguments: arguments})
+      .send({
+        from: this.primaryAccount.address,
+        ...(general_helper.isZilliqaNetworkSelected() && {gas: 35_000}),
+        ...(general_helper.isZilliqaNetworkSelected() && {gasPrice: 4_000_000_000_000})
+      })
+      .on("error", function (error) {
+        console.log(error);
+      });
+  }
+};
 
-module.exports = web3_helper
+module.exports = web3_helper;
