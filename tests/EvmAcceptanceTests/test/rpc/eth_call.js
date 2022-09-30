4aca333c9695f882ab33269e0434ad3c65611235
Michel <107615499+moboware@users.noreply.github.com>
2022-09-30 08:02:24 +0200

    [master] Zil 4729 add eth call test (#3059)
    * ZIL-4729
    
    * ZIL-4729
    
    * ZIL-4729 call eth_call and check return
    
    * ZIL-4729 review change

---
+++ tests/EvmAcceptanceTests/test/rpc/eth_call.js
@@ -0,0 +1,28 @@
+const helper = require('../../helper/GeneralHelper');
+const zilliqa_helper = require("../../helper/ZilliqaHelper");
+const { hardhat } = require("hardhat");
+assert = require('chai').assert;
+
+const METHOD = 'eth_call';
+
+describe("Calling " + METHOD, function () {
+  it("should return the from eth call", async function () {
+
+    const contractFactory = await hre.ethers.getContractFactory("SimpleContract");
+
+    await helper.callEthMethod(METHOD, 2, [{
+      "to": zilliqa_helper.getPrimaryAccountAddress(),
+      "data": contractFactory.bytecode,
+      "gas": 1000000
+    }, "latest"],
+      (result, status) => {
+        console.log(result);
+        assert.equal(status, 200, 'has status code');
+        assert.property(result, 'result', (result.error) ? result.error.message : 'error');
+        assert.isString(result.result, 'is string');
+        assert.match(result.result, /^0x/, 'should be HEX starting with 0x');
+        assert.isNumber(+result.result, 'can be converted to a number');
+        assert.equal(result.result, "0x");
+      })
+  })
+})
