ba388e9e6a0075b1288632f14d9baceb5452e385
n-hutton <nathan@zilliqa.com>
2022-10-03 19:48:13 +0100

    thing

--- scripts/integration_test_js.sh
+++ scripts/integration_test_js.sh
@@ -58,11 +58,10 @@ else
 
     # Just to check evm-ds has been built
     if [[ -d /home/jenkins ]]; then
-        pwd
         ls /home/jenkins/agent/workspace/ZilliqaCIJenkinsfile_PR-*/evm-ds/target/release/evm-ds
 
         # For convenience move the required files to tmp directory
-        cp /home/jenkins/agent/workspace/*/evm-ds/target/release/evm-ds /tmp || exit 1
+        cp /home/jenkins/agent/workspace/*/evm-ds/target/release/evm-ds
         cp /home/jenkins/agent/workspace/*/evm-ds/log4rs.yml /tmp
 
         # Modify constants.xml for use by isolated server
@@ -73,10 +72,6 @@ else
         sed -i 's/.EVM_LOG_CONFIG.*/<EVM_LOG_CONFIG>\/tmp\/log4rs.yml<\/EVM_LOG_CONFIG>/g' constants.xml
     fi
 
-    #echo "sleeping..."
-    #sleep 8000
-
-
     echo "Starting isolated server..."
     ./build/bin/isolatedServer -f isolated-server-accounts.json -u 999 -t 3000 &
 
@@ -84,6 +79,9 @@ else
     npm install
     npm install --save-dev "@ethersproject/providers@^5.4.7" "@nomicfoundation/hardhat-network-helpers@^1.0.0" "@nomicfoundation/hardhat-chai-matchers@^1.0.0" "@nomiclabs/hardhat-ethers@^2.0.0" "@nomiclabs/hardhat-etherscan@^3.0.0" "@types/chai@^4.2.0" "@types/mocha@^9.1.0" "@typechain/ethers-v5@^10.1.0" "@typechain/hardhat@^6.1.2" "chai@^4.2.0" "ethers@^5.4.7" "hardhat-gas-reporter@^1.0.8" "solidity-coverage@^0.7.21" "ts-node@>=8.0.0" "typechain@^8.1.0" "typescript@>=4.5.0"
     npx hardhat test
+
+    echo "sleeping...."
+    sleep 8000
 
     retVal=$?
     if [ $retVal -ne 0 ]; then
