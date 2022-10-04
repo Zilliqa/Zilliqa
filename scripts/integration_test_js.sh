a83efe4c08b942934909616313add1299586fd8c
n-hutton <nathan.cameron.hutton@gmail.com>
2022-10-05 00:57:54 +0100

    Update integration_test_js.sh

--- scripts/integration_test_js.sh
+++ scripts/integration_test_js.sh
@@ -80,9 +80,6 @@ else
     npm install --save-dev "@ethersproject/providers@^5.4.7" "@nomicfoundation/hardhat-network-helpers@^1.0.0" "@nomicfoundation/hardhat-chai-matchers@^1.0.0" "@nomiclabs/hardhat-ethers@^2.0.0" "@nomiclabs/hardhat-etherscan@^3.0.0" "@types/chai@^4.2.0" "@types/mocha@^9.1.0" "@typechain/ethers-v5@^10.1.0" "@typechain/hardhat@^6.1.2" "chai@^4.2.0" "ethers@^5.4.7" "hardhat-gas-reporter@^1.0.8" "solidity-coverage@^0.7.21" "ts-node@>=8.0.0" "typechain@^8.1.0" "typescript@>=4.5.0"
     npx hardhat test
 
-    echo "sleeping...."
-    sleep 8000
-
     retVal=$?
     if [ $retVal -ne 0 ]; then
         echo "!!!!!! Error with JS integration test !!!!!!"
