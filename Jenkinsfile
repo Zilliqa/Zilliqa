ede50990c13c80f61aba7a55608b559dd79ca7d2
Francesco Medas <francesco@zilliqa.com>
2022-10-05 09:45:58 +0400

    feat: DEVOPS-209 add linting daily run

--- Jenkinsfile
+++ Jenkinsfile
@@ -62,15 +62,16 @@ timestamps {
               }
               stage('Build') {
                   sh "git config --global --add safe.directory '*'"
-                  sh "export VCPKG_ROOT=${env.VCPKG_ROOT} && ./scripts/ci_build.sh"
+                  sh "export VCPKG_ROOT=${env.VCPKG_ROOT} && ./scripts/ci_lint.sh"
+                  // sh "export VCPKG_ROOT=${env.VCPKG_ROOT} && ./scripts/ci_build.sh"
               }
-              stage('Integration test') {
-                  sh "scripts/integration_test.sh --setup-env"
-              }
-              stage('Report coverage') {
-                  // Code coverage is currently only implemented for GCC builds, so OSX is currently excluded from reporting
-                  sh "./scripts/ci_report_coverage.sh"
-              }
+              // stage('Integration test') {
+              //     sh "scripts/integration_test.sh --setup-env"
+              // }
+              // stage('Report coverage') {
+              //     // Code coverage is currently only implemented for GCC builds, so OSX is currently excluded from reporting
+              //     sh "./scripts/ci_report_coverage.sh"
+              // }
           }
         } catch (err) {
           if (err.getMessage() == skipciMessage)
---
+++ scripts/ci_lint.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# Copyright (C) 2019 Zilliqa
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# This script is dedicated for CI use
+#
+
+set -e
+
+# fix to make script work locally, please remove and fix to original intent.
+if [ -f /scilla/0 ]
+  then
+    ls /scilla/0/
+fi
+
+
+re="\\bNOCI\\b"
+if [[ "$TRAVIS_COMMIT_MESSAGE" =~ $re ]]
+then
+    exit 0
+fi
+
+# set n_parallel to fully utilize the resources
+os=$(uname)
+case $os in
+    'Linux')
+        n_parallel=$(nproc)
+        ;;
+    'Darwin')
+        n_parallel=$(sysctl -n hw.ncpu)
+        ;;
+    *)
+        n_parallel=2
+        ;;
+esac
+
+echo "n_parallel=${n_parallel}"
+
+echo "ccache configuration"
+ccache --version
+ccache -p
+
+ccache -z
+echo "ccache status"
+ccache -s
+
+dir=build
+
+CMAKE_EXTRA_OPTIONS=""
+
+if [ "$os" = "Linux" ]
+then
+    CMAKE_EXTRA_OPTIONS="$CMAKE_EXTRA_OPTIONS -DLLVM_EXTRA_TOOLS=ON"
+fi
+
+# assume that it is run from project root directory
+cmake -H. -B${dir} ${CMAKE_EXTRA_OPTIONS} -DCMAKE_BUILD_TYPE=Debug -DTESTS=ON -DENABLE_COVERAGE=ON -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-linux-dynamic
+cmake --build ${dir} -- -j${n_parallel}
+
+# remember to append `|| exit` after the commands added in if-then-else
+if [ "$os" = "Linux" ]
+then
+    #./scripts/integration_test.sh || exit 1
+    # ./scripts/ci_xml_checker.sh constants.xml || exit 1
+    # ./scripts/ci_xml_checker.sh constants_local.xml || exit 1
+    # ./scripts/license_checker.sh || exit 1
+    # ./scripts/depends/check_guard.sh || exit 1
+    cmake --build ${dir} --target clang-format || exit 1
+    cmake --build ${dir} --target clang-tidy || exit 1
+    # The target Zilliqa_coverage already includes "ctest" command, see cmake/CodeCoverage.cmake
+    # cmake --build ${dir} --target Zilliqa_coverage || exit 1
+else
+    cd build && ctest --output-on-failure -j1 || exit 1
+fi
+
+echo "ccache status"
+ccache -s
