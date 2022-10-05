06376965b26af6b43f580e2ee9faa60e9667156a
michel <michel@zilliqa.com>
2022-10-05 08:33:11 +0200

    ZIL-4827 review changes

--- tests/EvmAcceptanceTests/test/ContractRevert.js
+++ tests/EvmAcceptanceTests/test/ContractRevert.js
@@ -1,19 +1,17 @@
-const { expect } = require("chai");
-const { ethers, web3 } = require("hardhat");
-const general_helper = require("../helper/GeneralHelper");
+const { web3 } = require("hardhat");
 const web3_helper = require("../helper/Web3Helper");
 const zilliqa_helper = require("../helper/ZilliqaHelper");
 assert = require('chai').assert;
 
 
-describe("Revert Contract Deployment", function () {
+describe("Revert Contract Call", function () {
   let contract;
   before(async function () {
     contract = await web3_helper.deploy("Revert");
   });
 
 
-  it("Will revert the contract", async function () {
+  it("Will revert the contract when revert is called", async function () {
 
     function onCallViewFinished(receipt) {
       assert.fail("Failure: Should not be successful");
