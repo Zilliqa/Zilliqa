ad2319fdf66f681b1fb973bfb0932f367eeb1cec
michel <michel@zilliqa.com>
2022-10-18 14:41:12 +0200

    ZIL-4871 contract test logging and fix

--- tests/EvmAcceptanceTests/test/BlockchainInstructions.js
+++ tests/EvmAcceptanceTests/test/BlockchainInstructions.js
@@ -1,6 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+// these tests assume that the block_tag is 'latest'
 describe("Blockchain information for smart contracts", function () {
   let contract;
   before(async function () {
@@ -14,20 +15,26 @@ describe("Blockchain information for smart contracts", function () {
 
   it("Should return the owner address when getOrigin function is called", async function () {
     const [owner] = await ethers.getSigners();
-    expect(await contract.getOrigin()).to.be.eq(owner.address);
+    let origin = await contract.getOrigin();
+    hre.logDebug("Origin:", origin);
+    expect(origin).to.be.eq(owner.address);
   });
 
   it("Should return the block coinBase when getBlockCoinbase function is called", async function () {
-    expect(await contract.getBlockCoinbase()).to.be.eq("0x0000000000000000000000000000000000000000");
+    let blockCoinbase = await contract.getBlockCoinbase();
+    hre.logDebug("BlockCoinbase:", blockCoinbase);
+    expect(blockCoinbase).to.be.eq("0x0000000000000000000000000000000000000000");
   });
 
   it("Should return the tx gas price when getTxGasprice function is called", async function () {
-    expect(await contract.getTxGasprice()).to.be.eq("0x0000000000000000000000000000000000000000");
+    let txGasPrice = await contract.getTxGasprice();
+    hre.logDebug("TX gasprice:", txGasPrice);
+    expect(txGasPrice).to.be.eq("0x0000000000000000000000000000000000000000");
   });
 
   it("Should return the block hash for block '0' when getBlockHash function is called", async function () {
     let hash = await contract.getBlockHash(0);
-    hre.logDebug(hash);
+    hre.logDebug("Block hash:", hash);
     assert.isString(hash, "is string");
     assert.match(hash, /^0x/, "should be HEX starting with 0x");
     assert.isNumber(+hash, "can be converted to a number");
@@ -35,26 +42,26 @@ describe("Blockchain information for smart contracts", function () {
 
   it("Should return the latest block number when getBlockNumber function is called", async function () {
     let blockNumber = await contract.getBlockNumber();
-    hre.logDebug(blockNumber.value);
-    assert.isString(blockNumber.value, "is not a string");
-    //assert.match(blockNumber, /^0x/, "should be HEX starting with 0x");
-    //assert.isNumber(+blockNumber, "can be converted to a number");
+    hre.logDebug("Block number:", blockNumber);
+    expect(blockNumber).to.be.gte(0x0);
   });
 
   it("Should return the latest block difficulty when getBlockDifficulty function is called", async function () {
-    let blockDifficulty = await contract.getBlockDifficulty();//   ??? expect(await contract.getDifficulty()).to.be.lt(0x0);
-    hre.logDebug(blockDifficulty);
-    assert.isString(blockDifficulty.value, "is not a string");
-    //assert.match(blockDifficulty, /^0x/, "should be HEX starting with 0x");
-    //assert.isNumber(+blockDifficulty, "can be converted to a number");
+    let blockDifficulty = await contract.getBlockDifficulty();
+    hre.logDebug("Block difficulty:", blockDifficulty);
+    expect(blockDifficulty).to.be.gte(0x0);
   });
 
   it("Should return the block timestamp for latest block when getBlockTimestamp function is called", async function () {
-    expect(await contract.getBlockTimestamp()).to.be.gt(0x0);
+    let timeStamp = await contract.getBlockTimestamp();
+    hre.logDebug("Block timestamp:", timeStamp);
+    expect(timeStamp).to.be.gt(0x0);
   });
 
   it("Should return the block gas limit for latest block when getBlockGaslimit function is called", async function () {
-    expect(await contract.getBlockGaslimit()).to.be.gte(0x0);
+    let blockGaslimit = await contract.getBlockGaslimit();
+    hre.logDebug("Block Gaslimit :", blockGaslimit);
+    expect(blockGaslimit).to.be.gte(0x0);
   });
 
 });
