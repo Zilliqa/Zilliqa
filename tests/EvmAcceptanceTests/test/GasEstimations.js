90a9833063d0b0abd4a4fa0249dabd0c2b7e9a53
bzawisto <bartosz@zilliqa.com>
2022-10-20 11:02:20 +0200

    Addressed review feedback

--- tests/EvmAcceptanceTests/test/GasEstimations.js
+++ tests/EvmAcceptanceTests/test/GasEstimations.js
@@ -1,21 +1,20 @@
 const {expect} = require("chai");
 const {web3} = require("hardhat");
 const web3_helper = require("../helper/Web3Helper");
-const zilliqa_helper = require("../helper/ZilliqaHelper");
 
 describe("Gas estimation with web3.js", function () {
   let contract;
-  const tranferFundMinGas = 21000;
-  const create2MinGas = 32000;
+  const TRANSFER_FUN_MIN_GAS = 21000;
+  const CREATE2_MIN_MAS = 32000;
 
   describe("When a fund transfer is made", function () {
     it("should return proper estimation [@transactional]", async function () {
       const gasAmount = await web3.eth.estimateGas({
-        to: zilliqa_helper.getSecondaryAccountAddress(),
-        from: zilliqa_helper.getPrimaryAccountAddress(),
+        to: web3_helper.getSecondaryAccountAddress(),
+        from: web3_helper.getPrimaryAccountAddress(),
         value: web3.utils.toWei("300", "gwei")
       });
-      expect(gasAmount).to.be.at.least(tranferFundMinGas);
+      expect(gasAmount).to.be.at.least(TRANSFER_FUN_MIN_GAS);
     });
   });
 
@@ -28,7 +27,7 @@ describe("Gas estimation with web3.js", function () {
 
     it("Should return proper gas estimation [@transactional]", async function () {
       const gasAmount = await contract.methods.installChild(123).estimateGas();
-      expect(gasAmount).to.be.at.least(create2MinGas);
+      expect(gasAmount).to.be.at.least(CREATE2_MIN_MAS);
     });
   });
 });
