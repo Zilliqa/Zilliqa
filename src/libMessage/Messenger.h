6a51e603a956f92243f80c4452d44173faa190e2
Yaron Hirsch <yaron@zilliqa.com>
2022-10-31 16:14:29 +0800

    ZIL-4957: remove BloomFilters since it's unused.

--- src/libMessage/Messenger.h
+++ src/libMessage/Messenger.h
@@ -23,7 +23,6 @@
 #include "common/BaseType.h"
 #include "common/Serializable.h"
 #include "common/TxnStatus.h"
-#include "libData/AccountData/BloomFilter.h"
 #include "libData/AccountData/MBnForwardedTxnEntry.h"
 #include "libData/BlockData/Block.h"
 #include "libData/CoinbaseData/CoinbaseStruct.h"
@@ -250,11 +249,6 @@ class Messenger {
   static bool GetDiagnosticDataCoinbase(const bytes& src,
                                         const unsigned int offset,
                                         DiagnosticDataCoinbase& entry);
-
-  static bool SetBloomFilter(bytes& dst, const unsigned int offset,
-                             const BloomFilter& filter);
-  static bool GetBloomFilter(const bytes& src, const unsigned int offset,
-                             BloomFilter& filter);
 
   // ============================================================================
   // Peer Manager messages
