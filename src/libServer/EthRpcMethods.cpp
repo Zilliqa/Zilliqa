83ce22e7a73652cc8e37ce97f6b28d7c74cfea6e
Valeriy Zamaraiev <valeriy@zilliqa.com>
2022-11-01 17:38:28 +0100

    Handling Reverts specially to provide more compatible error handling

--- src/libServer/EthRpcMethods.cpp
+++ src/libServer/EthRpcMethods.cpp
@@ -751,6 +751,10 @@ std::string EthRpcMethods::GetEthEstimateGas(const Json::Value& json) {
     }
     LOG_GENERAL(WARNING, "Gas estimated: " << retGas);
     return (boost::format("0x%x") % retGas).str();
+  } else if (response.Revert()) {
+    // Error code 3 is a special case. It is practially documented only in geth and its clones,
+    // e.g. here: https://github.com/ethereum/go-ethereum/blob/9b9a1b677d894db951dc4714ea1a46a2e7b74ffc/internal/ethapi/api.go#L1026
+    throw JsonRpcException(3, "execution reverted", "0x" + response.ReturnedBytes());
   } else {
     throw JsonRpcException(ServerBase::RPC_MISC_ERROR, response.ExitReason());
   }
@@ -841,11 +845,15 @@ string EthRpcMethods::GetEthCallImpl(const Json::Value& _json,
     throw JsonRpcException(ServerBase::RPC_MISC_ERROR, "Unable to process");
   }
 
-  if (!success) {
+  if (success) {
+    return "0x" + response.ReturnedBytes();
+  } else if (response.Revert()) {
+    // Error code 3 is a special case. It is practially documented only in geth and its clones,
+    // e.g. here: https://github.com/ethereum/go-ethereum/blob/9b9a1b677d894db951dc4714ea1a46a2e7b74ffc/internal/ethapi/api.go#L1026
+    throw JsonRpcException(3, "execution reverted", "0x" + response.ReturnedBytes());
+  } else {
     throw JsonRpcException(ServerBase::RPC_MISC_ERROR, response.ExitReason());
   }
-
-  return "0x" + response.ReturnedBytes();
 }
 
 std::string EthRpcMethods::GetWeb3ClientVersion() {
