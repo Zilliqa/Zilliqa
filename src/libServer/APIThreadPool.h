20d6e9a6f5bee129348217847802becc930f0520
n-hutton <nathan.cameron.hutton@gmail.com>
2022-11-03 10:54:25 +0000

    sigh

--- src/libServer/APIThreadPool.h
+++ src/libServer/APIThreadPool.h
@@ -32,16 +32,16 @@ namespace evmproj {
 
 #ifdef __APPLE__
 
-inline void SetThreadName(const char *threadName){
+inline void SetThreadName(const char* threadName) {
   pthread_setname_np(threadName);
 }
 #elif defined _WIN32 || defined _WIN64
 
-inline void SetThreadName(const char *threadName){
+inline void SetThreadName(const char* threadName) {
   pthread_setname_np(pthread_self(), threadName);
 }
 #else
-inline void SetThreadName(const char *){}
+inline void SetThreadName(const char*) {}
 #endif
 
 class APIThreadPool : public std::enable_shared_from_this<APIThreadPool> {
