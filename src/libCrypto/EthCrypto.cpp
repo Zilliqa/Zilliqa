8de378faa78d7fd49d4940b0c3e29b15b695f826
Valeriy Zamaraiev <valeriy@zilliqa.com>
2022-11-09 18:37:39 +0100

    Remove debug logging

--- src/libCrypto/EthCrypto.cpp
+++ src/libCrypto/EthCrypto.cpp
@@ -448,12 +448,8 @@ zbytes GetTransmittedRLP(TransactionCoreInfo const& info, uint64_t chainId,
     rlpStreamRecreated << toAddr;
     rlpStreamRecreated << info.amount;
     if (IsNullAddress(info.toAddr)) {
-      LOG_GENERAL(INFO, "adding txn code: "
-                            << DataConversion::Uint8VecToHexStrRet(info.code));
       rlpStreamRecreated << FromEVM(info.code);
     } else {
-      LOG_GENERAL(INFO, "adding txn data: "
-                            << DataConversion::Uint8VecToHexStrRet(info.data));
       rlpStreamRecreated << info.data;
     }
 
