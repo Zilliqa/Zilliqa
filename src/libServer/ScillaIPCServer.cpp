c583c00531924aec7957677c817fb7a9e0af488d
Artem <artgor4github@gmail.com>
2022-11-19 13:54:52 +0300

    format

--- src/libServer/ScillaIPCServer.cpp
+++ src/libServer/ScillaIPCServer.cpp
@@ -15,8 +15,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include <jsonrpccpp/common/specification.h>
 #include <jsonrpccpp/common/exception.h>
+#include <jsonrpccpp/common/specification.h>
 #include <sstream>
 #include "common/Constants.h"
 #include "libUtils/GasConv.h"
