d07792b02f71f7be8672d5ada5ae467bb624fd53
n-hutton <nathan.cameron.hutton@gmail.com>
2022-12-06 10:53:56 +0000

    detect location of build file

--- build.sh
+++ build.sh
@@ -14,6 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+echo "Finding secp file..."
+find / -name libsecp256k1.so*
+
 set -e
 
 if [ -z ${VCPKG_ROOT} ]; then
