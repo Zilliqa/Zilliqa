ce55391aa3f3e2f8f30cc41e3334bbb8687e8d64
Steve White <steve.white@swdemon.com>
2022-12-08 13:36:11 +0000

    Updated for latest protobuf and otlp

--- src/libPersistence/ContractStorage.cpp
+++ src/libPersistence/ContractStorage.cpp
@@ -97,12 +97,12 @@ bool ContractStorage::DeleteInitData(const dev::h160& address) {
 template <class T>
 bool SerializeToArray(const T& protoMessage, zbytes& dst,
                       const unsigned int offset) {
-  if ((offset + protoMessage.ByteSize()) > dst.size()) {
-    dst.resize(offset + protoMessage.ByteSize());
+  if ((offset + protoMessage.ByteSizeLong()) > dst.size()) {
+    dst.resize(offset + protoMessage.ByteSizeLong());
   }
 
   return protoMessage.SerializeToArray(dst.data() + offset,
-                                       protoMessage.ByteSize());
+                                       protoMessage.ByteSizeLong());
 }
 
 string ContractStorage::GenerateStorageKey(const dev::h160& addr,
