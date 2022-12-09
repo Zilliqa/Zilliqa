3702a02d4289852bdc8fe5fbb47ccf71c494adb3
Saeed Dadkhah <saeed@zilliqa.com>
2022-12-09 13:58:37 +0330

    make transfer tests parallel friendly

--- tests/EvmAcceptanceTests/test/Transfer.js
+++ tests/EvmAcceptanceTests/test/Transfer.js
@@ -1,5 +1,6 @@
 const {expect} = require("chai");
 const {ethers} = require("hardhat");
+const ethers_helper = require("../helper/EthersHelper");
 
 const FUND = ethers.utils.parseUnits("2", "ether");
 
@@ -7,47 +8,45 @@ describe("ForwardZil contract functionality", function () {
   let contract;
 
   before(async function () {
-    const Contract = await ethers.getContractFactory("ForwardZil");
-    contract = await Contract.deploy();
+    this.contract = await ethers_helper.deployContract("ForwardZil");
+    this.signer = this.contract.signer;
   });
 
   it("Should return zero as the initial balance of the contract", async function () {
-    expect(await ethers.provider.getBalance(contract.address)).to.be.eq(0);
+    expect(await ethers.provider.getBalance(this.contract.address)).to.be.eq(0);
   });
 
   it(`Should move ${ethers.utils.formatEther(FUND)} ethers to the contract if deposit is called`, async function () {
-    expect(await contract.deposit({value: FUND})).changeEtherBalance(contract.address, FUND);
+    expect(await this.contract.deposit({value: FUND})).changeEtherBalance(this.contract.address, FUND);
   });
 
   // TODO: Add notPayable contract function test.
 
   it("Should move 1 ether to the owner if withdraw function is called so 1 ether is left for the contract itself [@transactional]", async function () {
-    const [owner] = await ethers.getSigners();
-    expect(await contract.withdraw()).to.changeEtherBalances(
-      [contract.address, owner.address],
+    expect(await this.contract.withdraw()).to.changeEtherBalances(
+      [this.contract.address, this.signer.address],
       [ethers.utils.parseEther("-1.0"), ethers.utils.parseEther("1.0")],
       {includeFee: true}
     );
   });
 
   it("should be possible to transfer ethers to the contract", async function () {
-    const [payer] = await ethers.getSigners();
     expect(
-      await payer.sendTransaction({
-        to: contract.address,
+      await this.signer.sendTransaction({
+        to: this.contract.address,
         value: FUND
       })
-    ).to.changeEtherBalance(contract.address, FUND);
+    ).to.changeEtherBalance(this.contract.address, FUND);
   });
 });
 
 describe("Transfer ethers", function () {
   it("should be possible to transfer ethers to a user account", async function () {
     const payee = ethers.Wallet.createRandom();
-    const [payer] = await ethers.getSigners();
 
+    const signer = await ethers_helper.signer();
     expect(
-      await payer.sendTransaction({
+      await signer.sendTransaction({
         to: payee.address,
         value: FUND
       })
