4337abaa6b44281de63dc0688235e58cf3f0098e
Saeed Dadkhah <saeed@zilliqa.com>
2022-12-13 15:41:52 +0330

    make blockchain instruction tests p-f

--- tests/EvmAcceptanceTests/test/BlockchainInstructions.js
+++ tests/EvmAcceptanceTests/test/BlockchainInstructions.js
@@ -1,20 +1,20 @@
 const {expect} = require("chai");
 const {ethers} = require("hardhat");
+const ethers_helper = require("../helper/EthersHelper");
 
 // TODO: Change the description to something more meaningful.
 describe("Blockchain Instructions contract", function () {
   let contract;
   before(async function () {
-    const Contract = await ethers.getContractFactory("BlockchainInstructions");
-    contract = await Contract.deploy();
+    this.contract = await ethers_helper.deployContract("BlockchainInstructions");
   });
 
   it("Should be deployed successfully", async function () {
-    expect(contract.address).to.be.properAddress;
+    expect(this.contract.address).to.be.properAddress;
   });
 
   it("Should return the owner address when getOrigin function is called", async function () {
-    const [owner] = await ethers.getSigners();
-    expect(await contract.getOrigin()).to.be.eq(owner.address);
+    const owner = this.contract.signer;
+    expect(await this.contract.getOrigin()).to.be.eq(owner.address);
   });
 });
