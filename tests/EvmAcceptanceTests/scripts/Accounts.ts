19359e2e2e316e4311eacdd33570d454406f5325
Saeed Dadkhah <saeed@zilliqa.com>
2022-12-30 14:52:17 +0330

    Move test scripts to typescript [skip ci] (#3305)

--- tests/EvmAcceptanceTests/scripts/Accounts.ts
+++ tests/EvmAcceptanceTests/scripts/Accounts.ts
@@ -1,11 +1,11 @@
-const hre = require("hardhat");
-const clc = require("cli-color");
+import hre from "hardhat";
+import clc from "cli-color";
 
 async function main() {
   const {provider} = hre.network;
 
-  const accounts = await provider.send("eth_accounts");
-  const balances = await Promise.all(accounts.map((account) => provider.send("eth_getBalance", [account, "latest"])));
+  const accounts: string[] = await provider.send("eth_accounts");
+  const balances = await Promise.all(accounts.map((account: string) => provider.send("eth_getBalance", [account, "latest"])));
   accounts.forEach((element, index) => {
     console.log(clc.bold("Account"), clc.green(element), clc.bold("Balance:"), clc.greenBright(balances[index]));
   });
--- tests/EvmAcceptanceTests/scripts/FeedDevnet.ts
+++ tests/EvmAcceptanceTests/scripts/FeedDevnet.ts
@@ -1,4 +1,4 @@
-const hre = require("hardhat");
+import hre from "hardhat";
 
 async function main() {
   while (true) {
