51478ba8e113c49ecf55b534754848837eb9b483
n-hutton <nathan@zilliqa.com>
2023-01-03 21:24:19 +0000

    WIP

--- tests/EvmAcceptanceTests/test/Transfer.js
+++ tests/EvmAcceptanceTests/test/Transfer.js
@@ -94,24 +94,24 @@ describe("Transfer ethers", function () {
     balances.forEach((el) => expect(el).to.be.eq(ACCOUNT_VALUE));
   });
 
+  async function getFee(hash) {
+      const res = await ethers.provider.getTransactionReceipt(hash);
+      return res.gasUsed;
+  }
+
   it("probably should be possible to use sent funds of smart contract", async function () {
-    const ACCOUNTS_COUNT = 1;
-    const ACCOUNT_VALUE = 0;
-    const TRANSFER_VALUE = 9;
+    const TRANSFER_VALUE = 1_000_000;
 
-    const accounts = Array.from({length: ACCOUNTS_COUNT}, (v, k) =>
-      ethers.Wallet.createRandom().connect(ethers.provider)
-    );
+    const rndAccount = ethers.Wallet.createRandom().connect(ethers.provider);
+    const randomAccount = rndAccount.address;
 
-    const addresses = accounts.map((signer) => signer.address);
-
-    let bal = await ethers.provider.getBalance(addresses[0]);
-    console.log("Initial balance of account to send is: ", bal);
+    let initialBal = await ethers.provider.getBalance(randomAccount);
+    console.log("Initial balance of account to send is: ", initialBal);
 
     const [owner] = await ethers.getSigners();
-    bal = await ethers.provider.getBalance(owner.address);
+    let InitialOwnerbal = await ethers.provider.getBalance(owner.address);
 
-    console.log("Initial balance of account sending from is: ", bal);
+    console.log("Initial balance of account sending from is: ", InitialOwnerbal);
 
     // Deploy with no funds at contract address, but send value
     const SingleTransferContract = await ethers.getContractFactory("SingleTransfer");
@@ -123,18 +123,29 @@ describe("Transfer ethers", function () {
 
     console.log("arghmeemnadf ");
 
-    const ret = await singleTransfer.doTransfer(addresses[0], TRANSFER_VALUE, {gasLimit: 25000000, value: TRANSFER_VALUE});
+    const ret = await singleTransfer.doTransfer(randomAccount, TRANSFER_VALUE, {gasLimit: 25000000, value: TRANSFER_VALUE});
 
-    console.log("arghmeemnadf ", ret);
+    console.log("retme1 ", singleTransfer);
+    console.log("retme2 ", ret);
 
-    const balances = await Promise.all(accounts.map((account) => account.getBalance()));
-    //balances.forEach((el) => expect(el).to.be.eq(ACCOUNT_VALUE));
+    const fee1 = await getFee(singleTransfer.deployTransaction.hash);
+    const fee2 = await getFee(ret.hash);
 
-    bal = await ethers.provider.getBalance(addresses[0]);
-    console.log("Final balance of account to send is: ", bal);
+    console.log("fee1 ", fee1);
+    console.log("fee2 ", fee2);
 
-    bal = await ethers.provider.getBalance(owner.address);
+    //const balances = await Promise.all(accounts.map((account) => account.getBalance()));
 
-    console.log("Final balance of account sending from is: ", bal);
+    const receivedBal = await ethers.provider.getBalance(randomAccount);
+    const senderBal = await ethers.provider.getBalance(owner.address);
+
+    console.log("Final balance of account sending from is: ", senderBal);
+    console.log("Final balance of account to send is: ", receivedBal);
+
+    const sumBal = BigInt(receivedBal) + BigInt(senderBal) + BigInt(fee1) + BigInt(fee2);
+
+    console.log("Orig: ", InitialOwnerbal.toString());
+    console.log("Sum:  ", sumBal);
+    console.log("Diff:  ", BigInt(InitialOwnerbal) - sumBal);
   });
 });
