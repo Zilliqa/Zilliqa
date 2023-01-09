769fe7f64fc27d3f6a95796258bcabfcb07b1dac
Saeed Dadkhah <saeed@zilliqa.com>
2023-01-09 11:29:03 +0330

    Add event test

--- tests/EvmAcceptanceTests/test/scilla/SetGet.ts
+++ tests/EvmAcceptanceTests/test/scilla/SetGet.ts
@@ -1,52 +1,38 @@
 import {deploy_from_file, sc_call} from "../../helper/ScillaHelper";
+import {Contract} from "@zilliqa-js/contract";
+import deepEqualInAnyOrder from "deep-equal-in-any-order";
+import chai from "chai";
 
-// async function run() {
-//   let tx = (sc = null);
-//   try {
-//     // deploy the contract
-//     const init = [{vname: "_scilla_version", type: "Uint32", value: "0"}];
-//     [tx, sc] = await deploy_from_file("contracts/scilla/SetGet.scilla", init);
-//     console.log("contract deployed @ ", sc.address);
-//     try {
-//       // call set(.) and log state
-//       const val = 16;
-//       const args = [{vname: "v", type: "Uint128", value: val.toString()}];
-//       tx = await sc_call(sc, "Set", args);
-//       const state = await sc.getState();
-//       console.log(`state after call to Set(${val}):`, state);
-//       try {
-//         // call emit() and log event emitted
-//         tx = await sc_call(sc, "Emit");
-//         console.log("event_logs[0] after call to Emit():\n", tx.receipt.event_logs[0]);
-//       } catch (err) {
-//         console.log("Emit(): ERROR\n", err);
-//       }
-//     } catch (err) {
-//       console.log("Set(.): ERROR\n", err);
-//     }
-//   } catch (err) {
-//     console.log("deploy_from_file(.): ERROR\n", err);
-//   }
-// }
+chai.use(deepEqualInAnyOrder);
 
 import {expect} from "chai";
 
 describe("Scilla SetGet contract", function () {
+  let contract: Contract;
   before(async function () {
     const init = [{vname: "_scilla_version", type: "Uint32", value: "0"}];
-    let sc = await deploy_from_file("contracts/scilla/SetGet.scilla", init);
-    this.contract = sc;
+    contract = await deploy_from_file("contracts/scilla/SetGet.scilla", init);
   });
 
   it("Should be deployed successfully", async function () {
-    expect(this.contract.address).to.be.properAddress;
+    expect(contract.address).to.be.properAddress;
   });
 
   it("Should set state correctly", async function () {
     const VALUE = 12;
     const args = [{vname: "v", type: "Uint128", value: VALUE.toString()}];
-    await sc_call(this.contract, "Set", args);
-    const state = await this.contract.getState();
+    await sc_call(contract, "Set", args);
+    const state = await contract.getState();
     expect(state.value).to.be.eq("12");
+  });
+
+  it("Should contain event data if Get function is called", async function () {
+    const tx = await sc_call(contract, "Emit");
+    const receipt = tx.getReceipt()!;
+    expect(receipt.event_logs?.length).to.be.eq(1);
+    expect(receipt.event_logs![0]._eventname).to.be.eq("Emit");
+    expect(receipt.event_logs![0].params).to.deep.contain(
+      {type: "Uint128", value: "12", vname: "value"}
+    );
   });
 });
