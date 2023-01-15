f005694c3cc151a53682faf0e3b11b84ae2bed12
n-hutton <nathan@zilliqa.com>
2023-01-15 13:02:13 +0000

    now no segv hopefully

--- src/libNode/FinalBlockProcessing.cpp
+++ src/libNode/FinalBlockProcessing.cpp
@@ -951,6 +951,11 @@ bool Node::ProcessFinalBlockCore(uint64_t& dsBlockNumber,
 
       LOG_GENERAL(WARNING, "Marker001: microblock hash succ: " << succ);
 
+      if (!succ) {
+        LOG_GENERAL(WARNING, "Marker001: skipping...");
+        continue;
+      }
+
       const auto &tranHashes = microBlockPtr->GetTranHashes();
 
       LOG_GENERAL(WARNING, "Marker001: microblock hash tran hashes: " << tranHashes.size());
