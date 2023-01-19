648d1225398ae6c50a2d089758484d17104fa1b9
n-hutton <nathan.cameron.hutton@gmail.com>
2023-01-19 12:00:37 +0000

    fix test

--- tests/EvmAcceptanceTests/contracts/BatchTransfer.sol
+++ tests/EvmAcceptanceTests/contracts/BatchTransfer.sol
@@ -2,11 +2,7 @@
 pragma solidity ^0.8.9;
 
 contract BatchTransfer {
-    constructor() payable
-    {
-    }
-
-    function batchTransfer(address payable[] memory destinations, uint256 amount) public
+    constructor(address payable[] memory destinations, uint256 amount) payable
     {
         for (uint256 i=0; i<destinations.length; i++)
         {
