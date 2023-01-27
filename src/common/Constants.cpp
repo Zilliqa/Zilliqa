c49ef995f11ed09b89f833cc5605b4866d401326
n-hutton <nathan@zilliqa.com>
2023-01-27 13:50:35 +0000

    force cps on

--- src/common/Constants.cpp
+++ src/common/Constants.cpp
@@ -747,8 +747,7 @@ const uint64_t EVM_RPC_TIMEOUT_SECONDS{
     ReadConstantUInt64("EVM_RPC_TIMEOUT_SECONDS", "node.jsonrpc.", 60)};
 const bool LAUNCH_EVM_DAEMON{
     ReadConstantString("LAUNCH_EVM_DAEMON", "node.jsonrpc.", "true") == "true"};
-const bool ENABLE_CPS{
-    ReadConstantString("ENABLE_CPS", "node.jsonrpc.", "true") == "true"};
+const bool ENABLE_CPS = true;
 const std::string METRIC_ZILLIQA_HOSTNAME{ReadConstantString(
     "METRIC_ZILLIQA_HOSTNAME", "node.metric.zilliqa.", "localhost")};
 const std::string METRIC_ZILLIQA_PROVIDER{ReadConstantString(
