29ba499de75c6ab6e809593d6319fbaff985ec2b
Steve White <steve.white@swdemon.com>
2023-02-06 16:07:57 +0000

    metricsApi Enhancements

--- src/libMetrics/internal/mixins.h
+++ src/libMetrics/internal/mixins.h
@@ -34,8 +34,6 @@ class I64Counter {
  public:
   I64Counter(const std::string &name, const std::string &description,
              const std::string &units) {
- //   Metrics::GetInstance().AddCounterSumView(GetFullName(METRIC_FAMILY, name),
- //                                            "View of the Metric");
     m_theCounter = Metrics::GetMeter()->CreateUInt64Counter(
         GetFullName(METRIC_FAMILY, name), description, units);
   }
