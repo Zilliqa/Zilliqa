a60c8a16b45f0e9367a2624b1d04730f1c50a555
Saeed Dadkhah <saeed@zilliqa.com>
2023-02-21 17:38:40 +0330

    Test string

--- tests/EvmAcceptanceTests/test/scilla/SetGet.ts
+++ tests/EvmAcceptanceTests/test/scilla/SetGet.ts
@@ -6,6 +6,8 @@ import {parallelizer} from "../../helpers";
 describe("Scilla SetGet contract", function () {
   let contract: ScillaContract;
   const VALUE = 12;
+  const STRING_VALUE = "Salam";
+  const ADDRESS_VALUE = "0x1cc45678901bbaa678cc12345ff8901234567890";
 
   before(async function () {
     if (!hre.isZilliqaNetworkSelected() || !hre.isScillaTestingEnabled()) {
@@ -27,5 +29,25 @@ describe("Scilla SetGet contract", function () {
   it("Should contain event data if emit transition is called", async function () {
     const tx = await contract.emit();
     expect(tx).to.have.eventLog("Emit");
+  });
+
+  it("Should set string state correctly", async function () {
+    await contract.set_string(STRING_VALUE);
+    expect(await contract.string_value()).to.be.eq(STRING_VALUE);
+  });
+
+  it("Should contain event data if get_string is called", async function () {
+    const tx = await contract.get_string();
+    expect(tx).to.have.eventLogWithParams("get_string", {value: STRING_VALUE, vname: "value"});
+  });
+
+  it("Should set address state correctly", async function () {
+    await contract.set_address(ADDRESS_VALUE);
+    expect(await contract.address_value()).to.be.eq(ADDRESS_VALUE);
+  });
+
+  it("Should contain event data if get_string is called", async function () {
+    const tx = await contract.get_address();
+    expect(tx).to.have.eventLogWithParams("get_address", {value: ADDRESS_VALUE, vname: "value"});
   });
 });
