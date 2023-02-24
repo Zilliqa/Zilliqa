928ca64e946e0eda0f48a8e431b7714d035d4e5e
Mauro Medda <medda.mauro@gmail.com>
2023-02-24 14:04:42 +0100

    fix: explicit ndoe version installtion

--- scripts/integration_test_js.sh
+++ scripts/integration_test_js.sh
@@ -44,8 +44,16 @@ else
 
     # install dependencies
     apt update
-    apt -y install python3 lsb-core curl dirmngr apt-transport-https lsb-release ca-certificates
-    curl -sL https://deb.nodesource.com/setup_14.x | bash -
+    apt -y install gpg python3 lsb-core curl dirmngr apt-transport-https lsb-release ca-certificates
+    ## Adding the NodeSource signing key to your keyring...
+    curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor | tee /usr/share/keyrings/nodesource.gpg >/dev/null
+
+    ## Creating apt sources list file for the NodeSource Node.js 14.x repo...
+
+    echo 'deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_14.x jammy main' > /etc/apt/sources.list.d/nodesource.list
+    echo 'deb-src [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_14.x jammy main' >> /etc/apt/sources.list.d/nodesource.list
+
+    apt update
     apt -y install nodejs
     node --version
 
