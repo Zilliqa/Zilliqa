2968b70fb3de36b4f10ecae4fc2ad9e9861dc563
Saeed Dadkhah <saeed@zilliqa.com>
2023-02-28 16:28:07 +0330

    Fix and enable contract revert tests in CI (#3469)

--- tests/EvmAcceptanceTests/test/ContractRevert.ts
+++ tests/EvmAcceptanceTests/test/ContractRevert.ts
@@ -12,14 +12,12 @@ describe("Revert Contract Call", function () {
     await expect(this.contract.revertCall({value: 1000})).to.be.reverted;
   });
 
-  // FIXME: https://zilliqa-jira.atlassian.net/browse/EM-53
-  xit("Should revert transaction with a custom message if the called function reverts with custom message", async function () {
+  it("Should revert transaction with a custom message if the called function reverts with custom message", async function () {
     const REVERT_MESSAGE = "reverted!!";
     await expect(this.contract.revertCallWithMessage(REVERT_MESSAGE, {value: 1000})).to.be.revertedWith(REVERT_MESSAGE);
   });
 
-  // FIXME: https://zilliqa-jira.atlassian.net/browse/EM-53
-  xit("Should revert with an error object if the called function reverts with custom error", async function () {
+  it("Should revert with an error object if the called function reverts with custom error", async function () {
     const owner = this.contract.signer;
     await expect(this.contract.revertCallWithCustomError({value: 1000}))
       .to.be.revertedWithCustomError(this.contract, "FakeError")
@@ -27,7 +25,7 @@ describe("Revert Contract Call", function () {
   });
 
   // FIXME: https://zilliqa-jira.atlassian.net/browse/ZIL-5001
-  xit("Should be reverted without any reason if specified gasLimit is not enough  to complete txn", async function () {
+  xit("Should be reverted without any reason if specified gasLimit is not enough to complete txn", async function () {
     await expect(this.contract.outOfGas({gasLimit: 100000})).to.be.revertedWithoutReason();
   });
 });
