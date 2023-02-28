de88984ebc4e5aa1b7223c456fea65a3033fdc79
Stephen White <93026204+Steve-White-UK@users.noreply.github.com>
2023-02-28 07:31:47 -0500

    Overflow from last PR of comments made to self. (#3468)

--- src/cmd/main.cpp
+++ src/cmd/main.cpp
@@ -241,7 +241,6 @@ int main(int argc, const char* argv[]) {
       return ERROR_IN_CONSTANTS;
     }
 
-    TRACE_ERROR("This is a startup test");
 
     Zilliqa zilliqa(make_pair(privkey, pubkey), my_network_info,
                     (SyncType)syncType, vm.count("recovery"),
