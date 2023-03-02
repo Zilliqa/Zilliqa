cb329eb43c3cd73048fcfe490d6d8cde16789928
Saeed Dadkhah <saeed@zilliqa.com>
2023-03-02 11:09:24 +0330

    update scilla path

--- scripts/integration_test_js.sh
+++ scripts/integration_test_js.sh
@@ -98,6 +98,7 @@ else
         sed -i 's/.ENABLE_EVM>.*/<ENABLE_EVM>true<\/ENABLE_EVM>/g' constants.xml
         sed -i 's/.EVM_SERVER_BINARY.*/<EVM_SERVER_BINARY>\/tmp\/evm-ds<\/EVM_SERVER_BINARY>/g' constants.xml
         sed -i 's/.EVM_LOG_CONFIG.*/<EVM_LOG_CONFIG>\/tmp\/log4rs.yml<\/EVM_LOG_CONFIG>/g' constants.xml
+        sed -i 's/.SCILLA_ROOT.*/<SCILLA_ROOT>\/scilla\/0\/<\/SCILLA_ROOT>/g' constants.xml
     fi
 
     if [[ -d /zilliqa ]]; then
