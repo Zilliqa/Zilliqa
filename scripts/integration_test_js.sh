16ac1d785bbcca3eb52f097a24c1485a5b2396df
Saeed Dadkhah <saeed@zilliqa.com>
2023-03-02 16:06:20 +0330

    change url

--- scripts/integration_test_js.sh
+++ scripts/integration_test_js.sh
@@ -101,8 +101,8 @@ else
         sed -i 's/.ENABLE_EVM>.*/<ENABLE_EVM>true<\/ENABLE_EVM>/g' constants.xml
         sed -i 's/.EVM_SERVER_BINARY.*/<EVM_SERVER_BINARY>\/tmp\/evm-ds<\/EVM_SERVER_BINARY>/g' constants.xml
         sed -i 's/.EVM_LOG_CONFIG.*/<EVM_LOG_CONFIG>\/tmp\/log4rs.yml<\/EVM_LOG_CONFIG>/g' constants.xml
-        sed -i 's/.SCILLA_ROOT.*/<SCILLA_ROOT>\/scilla\/0\/<\/SCILLA_ROOT>/g' constants.xml
-        sed -i 's/.ENABLE_SCILLA_MULTI_VERSION.*/<ENABLE_SCILLA_MULTI_VERSION>false<\/ENABLE_SCILLA_MULTI_VERSION>/g' constants.xml
+        sed -i 's/.SCILLA_ROOT.*/<SCILLA_ROOT>\/scilla<\/SCILLA_ROOT>/g' constants.xml
+    #    sed -i 's/.ENABLE_SCILLA_MULTI_VERSION.*/<ENABLE_SCILLA_MULTI_VERSION>false<\/ENABLE_SCILLA_MULTI_VERSION>/g' constants.xml
     fi
 
     if [[ -d /zilliqa ]]; then
