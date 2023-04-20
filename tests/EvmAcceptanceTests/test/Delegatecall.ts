18879d8c0ed58061052eb4dbb0b2a7e1c4723e58
n-hutton <nathan@zilliqa.com>
2023-04-20 10:35:29 +0100

    do the thing

--- tests/EvmAcceptanceTests/test/Delegatecall.ts
+++ tests/EvmAcceptanceTests/test/Delegatecall.ts
@@ -13,15 +13,25 @@ describe("Delegatecall functionality", function () {
     const NUM = 123;
 
     const owner = this.delegateContract.signer;
+
+    console.log("0");
     await this.delegateContract.setVars(this.testDelegateContract.address, NUM, {value: VALUE});
+    console.log("0");
     expect(await this.delegateContract.num()).to.be.eq(NUM);
+    console.log("0");
     expect(await this.delegateContract.value()).to.be.eq(VALUE);
+    console.log("0");
     expect(await this.delegateContract.sender()).to.be.eq(owner.address);
+    console.log("0");
     expect(await ethers.provider.getBalance(this.delegateContract.address)).to.be.eq(VALUE);
+    console.log("0");
 
     expect(await this.testDelegateContract.num()).to.be.eq(0);
+    console.log("0");
     expect(await this.testDelegateContract.value()).to.be.eq(0);
+    console.log("0");
     expect(await this.testDelegateContract.sender()).to.be.eq("0x0000000000000000000000000000000000000000");
+    console.log("0");
     expect(await ethers.provider.getBalance(this.testDelegateContract.address)).to.be.eq(0);
   });
 });
