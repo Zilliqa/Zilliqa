bf7a0b09b4f2b49cf35c4dc9307af3570debed68
n-hutton <nathan.cameron.hutton@gmail.com>
2023-04-21 17:08:10 +0100

    WIP1

--- evm-ds/src/evm_server_run.rs
+++ evm-ds/src/evm_server_run.rs
@@ -166,7 +166,7 @@ pub async fn run_evm_impl(
             },
             CpsReason::CallInterrupt(i) => {
                 let cont_id = continuations.lock().unwrap().create_continuation(runtime.machine_mut(), executor.into_state().substate());
-                build_call_result(&runtime, i, &listener, remaining_gas, cont_id)
+                build_call_result(executor, &runtime, i, &listener, remaining_gas, cont_id)
             },
             CpsReason::CreateInterrupt(i) => {
                 let cont_id = continuations.lock().unwrap().create_continuation(runtime.machine_mut(), executor.into_state().substate());
@@ -196,6 +196,65 @@ fn build_exit_result(
     result.set_exit_reason(exit_reason.clone().into());
     result.set_return_value(runtime.machine().return_value().into());
     let (state_apply, logs) = executor.into_state().deconstruct();
+    result.set_apply(
+        state_apply
+            .into_iter()
+            .map(|apply| {
+                let mut result = EvmProto::Apply::new();
+                match apply {
+                    Apply::Delete { .. } => {}
+                    Apply::Modify {
+                        address,
+                        basic,
+                        code,
+                        storage,
+                        reset_storage,
+                    } => {
+                        debug!("Modify: {:?} {:?}", address, basic);
+                        let mut modify = EvmProto::Apply_Modify::new();
+                        modify.set_address(address.into());
+                        modify.set_balance(backend.scale_eth_to_zil(basic.balance).into());
+                        modify.set_nonce(basic.nonce.into());
+                        if let Some(code) = code {
+                            modify.set_code(code.into());
+                        }
+                        modify.set_reset_storage(reset_storage);
+                        let storage_proto = storage
+                            .into_iter()
+                            .map(|(k, v)| backend.encode_storage(k, v).into())
+                            .collect();
+                        modify.set_storage(storage_proto);
+                        result.set_modify(modify);
+                    }
+                };
+                result
+            })
+            .collect(),
+    );
+    result.set_tx_trace(trace.as_string().into());
+    result.set_logs(logs.into_iter().map(Into::into).collect());
+    result.set_remaining_gas(remaining_gas);
+    result
+}
+
+fn build_call_result(
+    executor: CpsExecutor,
+    runtime: &Runtime,
+    interrupt: CpsCallInterrupt,
+    trace: &LoggingEventListener,
+    remaining_gas: u64,
+    cont_id: u64,
+) -> EvmProto::EvmResult {
+    let mut result = EvmProto::EvmResult::new();
+    result.set_return_value(runtime.machine().return_value().into());
+    let mut trap_reason = EvmProto::ExitReason_Trap::new();
+    trap_reason.set_kind(EvmProto::ExitReason_Trap_Kind::CALL);
+    let mut exit_reason = EvmProto::ExitReason::new();
+
+    let (state_apply, _) = executor.into_state().deconstruct();
+
+    // We need to apply the changes made to the state so subsequent calls can
+    // see the changes.
     result.set_apply(
         state_apply
             .into_iter()
@@ -235,24 +294,7 @@ fn build_exit_result(
             })
             .collect(),
     );
-    result.set_tx_trace(trace.as_string().into());
-    result.set_logs(logs.into_iter().map(Into::into).collect());
-    result.set_remaining_gas(remaining_gas);
-    result
-}
 
-fn build_call_result(
-    runtime: &Runtime,
-    interrupt: CpsCallInterrupt,
-    trace: &LoggingEventListener,
-    remaining_gas: u64,
-    cont_id: u64,
-) -> EvmProto::EvmResult {
-    let mut result = EvmProto::EvmResult::new();
-    result.set_return_value(runtime.machine().return_value().into());
-    let mut trap_reason = EvmProto::ExitReason_Trap::new();
-    trap_reason.set_kind(EvmProto::ExitReason_Trap_Kind::CALL);
-    let mut exit_reason = EvmProto::ExitReason::new();
     exit_reason.set_trap(trap_reason);
     result.set_exit_reason(exit_reason);
     result.set_tx_trace(trace.as_string().into());
