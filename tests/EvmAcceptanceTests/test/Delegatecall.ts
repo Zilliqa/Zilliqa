3a5d3ba13cbfdd93ec74180479782fc265a91f77
n-hutton <nathan@zilliqa.com>
2023-04-26 15:01:17 +0100

    WIP

--- tests/EvmAcceptanceTests/test/Delegatecall.ts
+++ tests/EvmAcceptanceTests/test/Delegatecall.ts
@@ -4,8 +4,14 @@ import {ethers} from "hardhat";
 
 describe("Delegatecall functionality", function () {
   before(async function () {
-    this.delegateContract = await parallelizer.deployContract("Delegatecall");
+    // Final level
     this.testDelegateContract = await parallelizer.deployContract("TestDelegatecall");
+
+    // Second level
+    this.delegateContract = await parallelizer.deployContract("Delegatecall");
+
+    // First level
+    this.baseDelegator = await parallelizer.deployContract("BaseDelegator");
   });
 
   it("should delegate function call correctly", async function () {
@@ -24,5 +30,16 @@ describe("Delegatecall functionality", function () {
     expect(await this.testDelegateContract.value()).to.be.eq(0);
     expect(await this.testDelegateContract.sender()).to.be.eq("0x0000000000000000000000000000000000000000");
     expect(await ethers.provider.getBalance(this.testDelegateContract.address)).to.be.eq(0);
+  });
+
+  it("Can see the values of delegated calls chained multiple times", async function () {
+    const VALUE = 1000000;
+
+    //await this.baseDelegator.setOwnerAndCheck(this.delegateContract.address, this.testDelegateContract.address, {value: VALUE});
+
+    // Set up the ID mapping
+    await this.testDelegateContract.setOwnerAndCheck();
+
+    //await this.baseDelegator.checkOwnerTwoWays(this.testDelegateContract.address, this.delegateContract.address);
   });
 });
