38bb252d1f6a22b38ad3ed79d901643f251a9077
James Hinshelwood <jameshinshelwood1@gmail.com>
2023-05-02 12:55:14 +0100

    ZIL-5214: Revert priority mutex implementation for AccountStore

--- src/libServer/IsolatedServer.cpp
+++ src/libServer/IsolatedServer.cpp
@@ -610,9 +610,6 @@ Json::Value IsolatedServer::CreateTransaction(const Json::Value& _json) {
     {
       shared_lock<shared_timed_mutex> lock(
           AccountStore::GetInstance().GetPrimaryMutex());
-      AccountStore::GetInstance().GetPrimaryWriteAccessCond().wait(lock, [] {
-        return AccountStore::GetInstance().GetPrimaryWriteAccess();
-      });
 
       const Account* sender = AccountStore::GetInstance().GetAccount(fromAddr);
 
@@ -658,10 +655,6 @@ Json::Value IsolatedServer::CreateTransaction(const Json::Value& _json) {
         {
           shared_lock<shared_timed_mutex> lock(
               AccountStore::GetInstance().GetPrimaryMutex());
-          AccountStore::GetInstance().GetPrimaryWriteAccessCond().wait(
-              lock, [] {
-                return AccountStore::GetInstance().GetPrimaryWriteAccess();
-              });
 
           const Account* account =
               AccountStore::GetInstance().GetAccount(tx.GetToAddr());
