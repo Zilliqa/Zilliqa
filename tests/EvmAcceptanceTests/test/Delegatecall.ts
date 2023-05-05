22f691ff2b7d3e49ea1b3d09b4cb9b37501a14f9
Richard Watts <richard@zilliqa.com>
2023-05-05 16:40:48 +0100

    WIP

--- tests/EvmAcceptanceTests/test/Delegatecall.ts
+++ tests/EvmAcceptanceTests/test/Delegatecall.ts
@@ -5,37 +5,41 @@ import {ethers} from "hardhat";
 describe("Delegatecall functionality", function () {
   before(async function () {
     // Final level
+    console.log(`X`)
     this.testDelegateContract = await parallelizer.deployContract("TestDelegatecall");
 
     // Second level
+    console.log(`Y`)
     this.delegateContract = await parallelizer.deployContract("Delegatecall");
 
     // First level
+    console.log(`Z`)
     this.baseDelegator = await parallelizer.deployContract("BaseDelegator");
   });
 
-  //it("should delegate function call correctly", async function () {
-  //  const VALUE = 1000000;
-  //  const NUM = 3735931646; // 0xDEADCAFE
+  it("should delegate function call correctly", async function () {
+    const VALUE = 1000000;
+    const NUM = 3735931646; // 0xDEADCAFE
 
-  //  const owner = this.delegateContract.signer;
-  //  await this.delegateContract.setVars(this.testDelegateContract.address, NUM, {value: VALUE});
-
-  //  expect(await this.delegateContract.num()).to.be.eq(NUM);
-  //  expect(await this.delegateContract.value()).to.be.eq(VALUE);
-  //  expect(await this.delegateContract.sender()).to.be.eq(owner.address);
-  //  expect(await ethers.provider.getBalance(this.delegateContract.address)).to.be.eq(VALUE);
-
-  //  expect(await this.testDelegateContract.num()).to.be.eq(0);
-  //  expect(await this.testDelegateContract.value()).to.be.eq(0);
-  //  expect(await this.testDelegateContract.sender()).to.be.eq("0x0000000000000000000000000000000000000000");
-  //  expect(await ethers.provider.getBalance(this.testDelegateContract.address)).to.be.eq(0);
-  //});
+    console.log(`A`);
+    const owner = this.delegateContract.signer;
+    await this.delegateContract.setVars(this.testDelegateContract.address, NUM, {value: VALUE});
+    console.log(`B`);
+    expect(await this.delegateContract.num()).to.be.eq(NUM);
+    expect(await this.delegateContract.value()).to.be.eq(VALUE);
+    expect(await this.delegateContract.sender()).to.be.eq(owner.address);
+    expect(await ethers.provider.getBalance(this.delegateContract.address)).to.be.eq(VALUE);
+    console.log(`C`);
+    expect(await this.testDelegateContract.num()).to.be.eq(0);
+    expect(await this.testDelegateContract.value()).to.be.eq(0);
+    expect(await this.testDelegateContract.sender()).to.be.eq("0x0000000000000000000000000000000000000000");
+    expect(await ethers.provider.getBalance(this.testDelegateContract.address)).to.be.eq(0);
+  });
 
   it("Can see the values of delegated calls chained multiple times", async function () {
     const VALUE = 1000000;
 
-    //await this.baseDelegator.setOwnerAndCheck(this.delegateContract.address, this.testDelegateContract.address, {value: VALUE});
+    await this.baseDelegator.setOwnerAndCheck(this.delegateContract.address, this.testDelegateContract.address, {value: VALUE});
 
     // Set up the ID mapping
     await this.testDelegateContract.setOwnerAndCheck();
