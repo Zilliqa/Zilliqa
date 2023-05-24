4eb997d4b7edf4e36e85e28794d13a0df2e16891
n-hutton <nathan@zilliqa.com>
2023-05-25 00:11:16 +0100

    hope it wasn't this...

--- tests/EvmAcceptanceTests/contracts/BatchTransfer.sol
+++ tests/EvmAcceptanceTests/contracts/BatchTransfer.sol
@@ -2,23 +2,16 @@
 pragma solidity ^0.8.9;
 
 contract BatchTransfer {
-    event DebugMe(string);
-
     constructor() payable
     {
     }
 
     function batchTransfer(address payable[] memory destinations, uint256 amount) public
     {
-        //for (uint256 i=0; i<destinations.length-1; i++)
-        //{
-        //    emit DebugMe("Make the transfer!!!");
-        //    destinations[i].transfer(amount);
-        //}
-        emit DebugMe("Make the transfer!!!");
-        destinations[0].transfer(amount);
-
-        emit DebugMe("about to self destruct!!!");
+        for (uint256 i=0; i<destinations.length; i++)
+        {
+            destinations[i].transfer(amount);
+        }
         selfdestruct(payable(msg.sender));
     }
 }
