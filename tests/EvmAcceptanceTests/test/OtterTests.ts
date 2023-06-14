1718b18e2247bb7ce053a9d05aad7b1a56ed6508
S4eed <saeed@zilliqa.com>
2023-06-14 12:02:23 +0330

    [release/v9.2] Disable Otterscan tests in the release (#3663)
    * Disable Otterscan tests in the release

--- tests/EvmAcceptanceTests/test/OtterTests.ts
+++ tests/EvmAcceptanceTests/test/OtterTests.ts
@@ -4,7 +4,8 @@ import {expectRevert} from "@openzeppelin/test-helpers";
 import sendJsonRpcRequest from "../helpers/JsonRpcHelper";
 import {parallelizer} from "../helpers";
 
-describe("Otterscan api tests", function () {
+// These tests are disabled only in release/v9.2
+describe.skip("Otterscan api tests", function () {
 
   it("When we revert the TX, we can get the tx error ", async function () {
     const METHOD = "ots_getTransactionError";
