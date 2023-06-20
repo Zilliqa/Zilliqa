d1f028a7a3bbf65e585e885b9fa8544038647378
bzawisto <bartosz@zilliqa.com>
2023-06-20 10:48:29 +0200

    Remove unneeded method

--- src/libUtils/GasConv.h
+++ src/libUtils/GasConv.h
@@ -30,10 +30,6 @@ class GasConv {
     return gasLimit * GetScalingFactor();
   }
 
-  static uint64_t GasUnitsFromEthToCoreCeil(uint64_t gasLimit) {
-    return gasLimit / GetScalingFactor() + 1;
-  }
-
   static uint64_t GetScalingFactor() { return MIN_ETH_GAS / NORMAL_TRAN_GAS; }
 };
 
