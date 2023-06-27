b22a3b05871f1e420ef3989918fd967e793e4dc5
Saeed Dadkhah <saeed@zilliqa.com>
2023-06-27 22:13:57 +0330

    Add init tests script. [skip ci]

---
+++ tests/EvmAcceptanceTests/scripts/InitTests.ts
@@ -0,0 +1,74 @@
+import { getAddressFromPrivateKey } from "@zilliqa-js/crypto";
+import { BN, Zilliqa, bytes, toChecksumAddress } from "@zilliqa-js/zilliqa";
+import clc from "cli-color";
+import { ethers } from "ethers";
+import Long from "long";
+import hre from "hardhat"
+
+
+async function main() {
+  let zilliqa = new Zilliqa(hre.getNetworkUrl());
+  const msgVersion = 1; // current msgVersion
+  const VERSION = bytes.pack(hre.getZilliqaChainId(), msgVersion);
+
+  const privateKey = process.env.PRIMARY_ACCOUNT;
+  if (privateKey === undefined) {
+    console.log(clc.bold.red('Please set PRIMARY_ACCOUNT environment variable before running this script.'))
+    return;
+  }
+
+  const address = getAddressFromPrivateKey(privateKey);
+
+  console.log(`Private key: ${privateKey}`)
+  console.log(`Address: ${address}`)
+  const balanceResult = await zilliqa.blockchain.getBalance(address);
+
+  if (balanceResult.error) {
+    console.log(clc.bold.red(balanceResult.error.message))
+    return;
+  }
+
+  const balance = new BN(balanceResult.result.balance);
+  console.log(`Balance: ${clc.bold.green(balance)}`)
+
+  if (balance.isZero()) {
+    console.log(clc.bold.red("Provided account doesn't have enough fund."))
+    return;
+  }
+
+  zilliqa.wallet.addByPrivateKey(privateKey);
+  for (const element of hre.network["config"]["accounts"]) {
+    const wallet = new ethers.Wallet(element);
+    let ethAddrConverted = toChecksumAddress(wallet.address); // Zil checksum
+    const tx = await zilliqa.blockchain.createTransactionWithoutConfirm(
+      zilliqa.transactions.new(
+        {
+          version: VERSION,
+          toAddr: ethAddrConverted,
+          amount: new BN(1_000_000), // Sending an amount in Zil (1) and converting the amount to Qa
+          gasPrice: new BN(2000000000), // Minimum gasPrice veries. Check the `GetMinimumGasPrice` on the blockchain
+          gasLimit: Long.fromNumber(2100)
+        },
+        false
+      )
+    );
+
+    if (tx.id) {
+      const confirmedTxn = await tx.confirm(tx.id);
+      const receipt = confirmedTxn.getReceipt();
+      if (receipt && receipt.success) {
+        console.log(`${ethAddrConverted}` + clc.bold.green(' funded.'))
+        continue;
+      }
+    }
+
+    console.log(clc.red(`Failed to fund ${ethAddrConverted}.`))
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
