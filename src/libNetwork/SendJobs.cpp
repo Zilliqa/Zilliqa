cd5066286daa0b612d591c2593cb62d8fd2d1de6
Yaron <yaron@zilliqa.com>
2023-07-03 19:45:56 +0800

    [master] ZIL-5298: add connection timer and make reconnection time configurable (#3696)
    * Add configurable constants for socket connection timeout &
       reconnection interval (in milliseconds).
    * Use the timer before socket connection to enforce a timeout.
    * Add metrics for reconnection to peers.

--- src/libNetwork/SendJobs.cpp
+++ src/libNetwork/SendJobs.cpp
@@ -44,6 +44,7 @@ class SendJobsVariables {
   std::atomic<int> sendMessageToPeerFailed = 0;
   std::atomic<int> sendMessageToPeerSyncCount = 0;
   std::atomic<int> activePeersSize = 0;
+  std::atomic<int> reconnectionToPeerCount = 0;
 
  public:
   std::unique_ptr<Z_I64GAUGE> temp;
@@ -68,16 +69,26 @@ class SendJobsVariables {
     activePeersSize = amount;
   }
 
+  void AddReconntionToPeerCount(int count) {
+    Init();
+    reconnectionToPeerCount += count;
+  }
+
   void Init() {
     if (!temp) {
       temp = std::make_unique<Z_I64GAUGE>(Z_FL::BLOCKS, "sendjobs.gauge",
                                           "Send Jobs metrics", "calls", true);
 
       temp->SetCallback([this](auto&& result) {
-        result.Set(sendMessageToPeerCount.load(), {{"counter", "SendMessageToPeerCount"}});
-        result.Set(sendMessageToPeerFailed.load(), {{"counter", "SendMessageToPeerFailed"}});
-        result.Set(sendMessageToPeerSyncCount.load(), {{"counter", "SendMessageToPeerSyncCount"}});
+        result.Set(sendMessageToPeerCount.load(),
+                   {{"counter", "SendMessageToPeerCount"}});
+        result.Set(sendMessageToPeerFailed.load(),
+                   {{"counter", "SendMessageToPeerFailed"}});
+        result.Set(sendMessageToPeerSyncCount.load(),
+                   {{"counter", "SendMessageToPeerSyncCount"}});
         result.Set(activePeersSize.load(), {{"counter", "ActivePeersSize"}});
+        result.Set(reconnectionToPeerCount.load(),
+                   {{"counter", "ReconnectionToPeerCount"}});
       });
     }
   }
@@ -116,7 +127,9 @@ inline bool IsHostHavingNetworkIssue(const ErrorCode& ec) {
           ec == NETWORK_UNREACHABLE);
 }
 
-inline bool IsNodeNotRunning(const ErrorCode& ec) { return (ec == TIMED_OUT || ec == CONN_REFUSED); }
+inline bool IsNodeNotRunning(const ErrorCode& ec) {
+  return (ec == TIMED_OUT || ec == CONN_REFUSED);
+}
 
 inline Milliseconds Clock() {
   return std::chrono::duration_cast<Milliseconds>(
@@ -125,21 +138,22 @@ inline Milliseconds Clock() {
 
 /// Waits for timer and calls a member function of Object,
 /// Timer must be in the scope of object (for pointers validity)
-template <typename Object, typename Time>
-void WaitTimer(SteadyTimer& timer, Time delay, Object* obj,
-               void (Object::*OnTimer)()) {
+template <typename F, typename Time>
+void WaitTimer(SteadyTimer& timer, Time delay, F onTimerHandler) {
   ErrorCode ec;
   timer.expires_at(
       boost::asio::steady_timer::clock_type::time_point(Clock() + delay), ec);
 
   if (ec) {
-    LOG_GENERAL(FATAL, "Cannot set timer");
+    LOG_GENERAL(FATAL,
+                "Cannot set timer: " << ec.message() << " (" << ec << ')');
     return;
   }
 
-  timer.async_wait([obj, OnTimer](const ErrorCode& error) {
-    if (!error) {
-      (obj->*OnTimer)();
+  timer.async_wait([onTimer = std::move(onTimerHandler)](const ErrorCode& ec) {
+    LOG_GENERAL(DEBUG, "Timer expired: " << ec.message() << " (" << ec << ')');
+    if (!ec) {
+      onTimer();
     }
   });
 }
@@ -250,12 +264,16 @@ class PeerSendQueue : public std::enable_shared_from_this<PeerSendQueue> {
 
     LOG_GENERAL(DEBUG, "Connecting to " << m_peer);
 
-    m_socket.async_connect(m_endpoint,
-                           [self = shared_from_this()](const ErrorCode& ec) {
-                             if (ec != OPERATION_ABORTED) {
-                               self->OnConnected(ec);
-                             }
-                           });
+    WaitTimer(m_timer, Milliseconds{CONNECTION_TIMEOUT_IN_MS},
+              [this]() { OnConnected(TIMED_OUT); });
+    m_socket.async_connect(m_endpoint, [self = shared_from_this()](
+                                           const ErrorCode& ec) {
+      LOG_GENERAL(DEBUG, "Connection to " << self->m_endpoint << ": "
+                                          << ec.message() << " (" << ec << ')');
+      if (ec != OPERATION_ABORTED) {
+        self->OnConnected(ec);
+      }
+    });
   }
 
   void OnConnected(const ErrorCode& ec) {
@@ -280,7 +298,10 @@ class PeerSendQueue : public std::enable_shared_from_this<PeerSendQueue> {
 
     boost::asio::async_write(
         m_socket, boost::asio::const_buffer(msg.data.get(), msg.size),
-        [self = shared_from_this()](const ErrorCode& ec, size_t) {
+        [self = shared_from_this()](const ErrorCode& ec, size_t count) {
+          LOG_GENERAL(DEBUG, "Wrote " << count << " bytes to "
+                                      << self->m_endpoint << ": "
+                                      << ec.message() << " (" << ec << ')');
           if (ec != OPERATION_ABORTED) {
             self->OnWritten(ec);
           }
@@ -364,13 +385,17 @@ class PeerSendQueue : public std::enable_shared_from_this<PeerSendQueue> {
 
     assert(ec);
 
-    WaitTimer(m_timer, Milliseconds(1000), this, &PeerSendQueue::Reconnect);
+    zil::local::variables.AddReconntionToPeerCount(1);
+    WaitTimer(m_timer, Milliseconds{RECONNECT_INTERVAL_IN_MS},
+              [this]() { Reconnect(); });
   }
 
   void Reconnect() {
     if (!CheckAgainstBlacklist() || ExpiredOrDone()) {
       return;
     }
+
+    LOG_GENERAL(INFO, "Reconnecting to " << m_endpoint << "...");
 
     // TODO the current protocol is weird and it assumes reconnecting every
     // time. This should be changed!!!
@@ -469,10 +494,10 @@ class SendJobsImpl : public SendJobs,
 
   void OnNewJob(Peer&& peer, RawMessage&& msg, bool allow_relaxed_blacklist) {
     if (IsBlacklisted(peer, allow_relaxed_blacklist)) {
-      LOG_GENERAL(INFO,
-                  "Ignoring blacklisted peer " <<
-                  peer.GetPrintableIPAddress()
-                  << "allow relaxed blacklist " << allow_relaxed_blacklist);
+      LOG_GENERAL(INFO, "Ignoring blacklisted peer "
+                            << peer.GetPrintableIPAddress()
+                            << "allow relaxed blacklist "
+                            << allow_relaxed_blacklist);
       return;
     }
 
@@ -486,7 +511,6 @@ class SendJobsImpl : public SendJobs,
   }
 
   void OnPeerQueueFinished(const Peer& peer, ErrorCode ec) {
-
     if (ec) {
       LOG_GENERAL(
           INFO, "Peer queue finished, peer=" << peer.GetPrintableIPAddress()
