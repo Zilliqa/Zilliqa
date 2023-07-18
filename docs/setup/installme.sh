3feaa9dbcec24143b0b7ee33b14c25ec2ceee8a8
Richard Watts <108257153+rrw-zilliqa@users.noreply.github.com>
2023-07-18 13:34:18 +0100

    (fix) ZIL-5315: Fix license headers on setup scripts. (#3726)
    (fix) ZIL-5315: fix grammar in license checker script.

--- docs/setup/installme.sh
+++ docs/setup/installme.sh
@@ -1,4 +1,19 @@
 #! /usr/bin/bash
+# Copyright (C) 2023 Zilliqa
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 # This is a shell script because one of the things it might want to do is upgrade python
 
 TARGET_DIR=~/src/zilliqa
