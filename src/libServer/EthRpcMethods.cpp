49bc91f9ec57c834406e259dda8d9f8b138f0073
lucac-zilliqa <lucachoteborsky@zilliqa.com>
2023-07-28 12:14:06 +0200

    Fixed function so correctly returns GasLimit

--- src/libServer/EthRpcMethods.cpp
+++ src/libServer/EthRpcMethods.cpp
@@ -2240,8 +2240,6 @@ Json::Value EthRpcMethods::GetBlockDetails(const uint64_t blockNumber) {
   uint128_t fees = (isVacuous ? 0 : txBlock.GetHeader().GetRewards() * EVM_ZIL_SCALING_FACTOR);
   auto jsonBlock = GetEthBlockCommon(txBlock, false);
 
-  jsonBlock["gasLimit"] = "0x1";
-
   jsonBlock.removeMember("transactions");
   jsonBlock["transactionCount"] = txBlock.GetHeader().GetNumTxs();
   jsonBlock["logsBloom"] = Json::nullValue;
