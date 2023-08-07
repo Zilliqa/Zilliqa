def6da42f54ab0cb908085d32833bada6a36c1e2
Saeed Dadkhah <saeed@zilliqa.com>
2023-08-07 15:39:50 +0330

    add time measurements

--- tests/EvmAcceptanceTests/scripts/ParallelTests.ts
+++ tests/EvmAcceptanceTests/scripts/ParallelTests.ts
@@ -1,15 +1,21 @@
 import { ethers } from "hardhat";
 import {ContractInfo, deployContracts} from "../helpers"
 import {runScenarios} from "../helpers"
-import {forwardZilScenario} from "../parallel-tests/Transfer"
+import {forwardZilScenario, moveZilScenario} from "../parallel-tests/Transfer"
 import {withUintScenario} from "../parallel-tests/ContractDeployment"
 import {parentScenario} from "../parallel-tests/Parent"
+import clc from "cli-color";
+import { performance } from "perf_hooks";
 
 async function main() {
     const FUND = ethers.utils.parseUnits("1", "gwei");
     const NUMBER = 1234;
 
     const contractsToDeploy: ContractInfo[] = [
+        {
+            name: "ForwardZil",
+            args: []
+        },
         {
             name: "ForwardZil",
             args: []
@@ -26,12 +32,17 @@ async function main() {
     ]
 
     // Deploy Contracts in parallel
-    let [forwardZil, withUint, parentContract] = await deployContracts(...contractsToDeploy);
+    const start = performance.now();
+    console.log(clc.bold("Contracts are being deployed..."));
+    let [forwardZil, moveZilToContract, withUint, parentContract] = await deployContracts(...contractsToDeploy);
+    const end = performance.now();
+    console.log(clc.blackBright("  done "), clc.bold.green(`${((end - start) / 1000).toPrecision(2)} s`));
 
     await runScenarios(
         withUintScenario(withUint, NUMBER),
         parentScenario(parentContract, FUND),
-        forwardZilScenario(forwardZil),
+        await forwardZilScenario(forwardZil),
+        await moveZilScenario(moveZilToContract),
     );
 }
 
