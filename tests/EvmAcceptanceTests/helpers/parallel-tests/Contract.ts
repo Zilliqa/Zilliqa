3da246f5ea834b1fa98a83d2e40fa5ff20c2ba8a
Saeed Dadkhah <saeed@zilliqa.com>
2023-08-09 10:51:32 +0330

    Use signer pool to deploy contracts

--- tests/EvmAcceptanceTests/helpers/parallel-tests/Contract.ts
+++ tests/EvmAcceptanceTests/helpers/parallel-tests/Contract.ts
@@ -1,20 +1,23 @@
 import { BigNumber, Contract } from "ethers";
 import { ethers } from "hardhat";
+import SignerPool from "../SignerPool";
 
 export type ContractInfo = {
     name: string;
-    args: any[];
+    args?: any[];
     value?: BigNumber
 };
 
 export const deployContracts = async function (...contracts: ContractInfo[]) {
+    let signer_pool = new SignerPool();
+    await signer_pool.initSigners(contracts.length, 0.1);
     let contractsPromises: Promise<Contract>[] = [];
-    const [signer] = await ethers.getSigners();
-    let nonce = await signer.getTransactionCount();
-    for (const contract of contracts) {
+    for (const index in contracts) {
+        const contract = contracts[index];
+        const signer = await signer_pool.takeSigner();
         const Contract = await ethers.getContractFactory(contract.name);
-        contractsPromises.push(Contract.deploy(...contract.args, { value: contract.value, nonce: nonce }));
-        nonce += 1;
+        let args = contract.args || [];
+        contractsPromises.push(Contract.connect(signer).deploy(...args, { value: contract.value }));
     }
 
     return Promise.all(contractsPromises);
