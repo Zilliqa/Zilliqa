b44ce5f70656a05721b4d03fb9b0bf1185d8cc2a
Saeed Dadkhah <saeed@zilliqa.com>
2023-08-11 11:27:53 +0330

    make delegate call parallel

--- tests/EvmAcceptanceTests/test/Delegatecall.ts
+++ tests/EvmAcceptanceTests/test/Delegatecall.ts
@@ -1,28 +1,33 @@
+import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
 import {expect} from "chai";
-import {parallelizer} from "../helpers";
-import {ethers} from "hardhat";
+import {Contract} from "ethers";
+import hre, {ethers} from "hardhat";
 
-describe("Delegatecall functionality", function () {
+describe("Delegatecall functionality #parallel", function () {
+  let delegateContract: Contract;
+  let testDelegateContract: Contract;
+  let signer: SignerWithAddress;
   before(async function () {
-    this.delegateContract = await parallelizer.deployContract("Delegatecall");
-    this.testDelegateContract = await parallelizer.deployContract("TestDelegatecall");
+    signer = hre.signer_pool.takeSigner();
+    delegateContract = await hre.deployContractWithSigner("Delegatecall", signer);
+    testDelegateContract = await hre.deployContractWithSigner("TestDelegatecall", signer);
   });
 
-  it("should delegate function call correctly", async function () {
+  it("should delegate function call correctly @block-1", async function () {
     const VALUE = 1000000;
     const NUM = 3735931646; // 0xDEADCAFE
 
-    const owner = this.delegateContract.signer;
-    await this.delegateContract.setVars(this.testDelegateContract.address, NUM, {value: VALUE});
+    const owner = signer;
+    await delegateContract.setVars(testDelegateContract.address, NUM, {value: VALUE});
 
-    expect(await this.delegateContract.num()).to.be.eq(NUM);
-    expect(await this.delegateContract.value()).to.be.eq(VALUE);
-    expect(await this.delegateContract.sender()).to.be.eq(owner.address);
-    expect(await ethers.provider.getBalance(this.delegateContract.address)).to.be.eq(VALUE);
+    expect(await delegateContract.num()).to.be.eq(NUM);
+    expect(await delegateContract.value()).to.be.eq(VALUE);
+    expect(await delegateContract.sender()).to.be.eq(owner.address);
+    expect(await ethers.provider.getBalance(delegateContract.address)).to.be.eq(VALUE);
 
-    expect(await this.testDelegateContract.num()).to.be.eq(0);
-    expect(await this.testDelegateContract.value()).to.be.eq(0);
-    expect(await this.testDelegateContract.sender()).to.be.eq("0x0000000000000000000000000000000000000000");
-    expect(await ethers.provider.getBalance(this.testDelegateContract.address)).to.be.eq(0);
+    expect(await testDelegateContract.num()).to.be.eq(0);
+    expect(await testDelegateContract.value()).to.be.eq(0);
+    expect(await testDelegateContract.sender()).to.be.eq("0x0000000000000000000000000000000000000000");
+    expect(await ethers.provider.getBalance(testDelegateContract.address)).to.be.eq(0);
   });
 });
