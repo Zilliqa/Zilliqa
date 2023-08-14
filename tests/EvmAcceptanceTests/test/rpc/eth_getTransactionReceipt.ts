4efa1d2fe297e7e44be925750b1fa916f6038256
Saeed Dadkhah <saeed@zilliqa.com>
2023-08-14 17:10:04 +0330

    Fix test

--- tests/EvmAcceptanceTests/test/rpc/eth_getTransactionReceipt.ts
+++ tests/EvmAcceptanceTests/test/rpc/eth_getTransactionReceipt.ts
@@ -17,10 +17,12 @@ describe(`Calling ${METHOD} #parallel`, function () {
     let amount = 10_000;
     // send amount from primary to secondary account
     const to = ethers.Wallet.createRandom();
-    const {response} = await hre.sendTransaction({
+    const signer = hre.signer_pool.takeSigner();
+    const response = await signer.sendTransaction({
       to: to.address,
       value: amount
     });
+    hre.signer_pool.releaseSigner(signer);
     const transactionHash = response.hash;
     await response.wait();
 
