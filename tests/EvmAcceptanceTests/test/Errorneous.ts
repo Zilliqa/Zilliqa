03cbf22c620896a42180b26040fd37ad0a632f2b
Bartosz Zawistowski <39065214+bzawisto@users.noreply.github.com>
2023-08-16 10:47:19 +0200

    [q4-working-branch] Q4 working branch bartosz (#3745)
    * Fixing test
    
    * Increasing POW window
    
    * Fixing errorneous test
    
    ---------
    
    Co-authored-by: bzawisto <bartosz@zilliqa.com>

--- tests/EvmAcceptanceTests/test/Errorneous.ts
+++ tests/EvmAcceptanceTests/test/Errorneous.ts
@@ -12,7 +12,8 @@ describe("While Calling a method on erroneous contract with given gas limit", fu
   it("it should return to the client and nonce/balance should be affected", async function () {
     const funds = await signer.getBalance();
     const nonce = await signer.getTransactionCount();
-    await this.contract.connect(signer).foo({gasLimit: 5000000});
+    const tx = await this.contract.connect(signer).foo({gasLimit: 400000});
+    await expect(tx.wait()).to.be.rejected;
     expect(funds).to.be.greaterThan(await signer.getBalance());
     expect(nonce).to.be.lessThan(await signer.getTransactionCount());
   });
