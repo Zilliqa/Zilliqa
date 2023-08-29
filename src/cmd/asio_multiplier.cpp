297759c1396d1bf5a7f2c3149a90aa69dff244b7
stephen <steve.white@swdemon.com>
2023-08-29 23:33:22 +0100

    CHeckpoint of framework for a new multiplier. Upgraded with script from Yaron, massaged a little for new multiplier.

---
+++ src/cmd/asio_multiplier.cpp
@@ -0,0 +1,286 @@
+/*
+ * Copyright (C) 2023 Zilliqa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <random>
+#include <set>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include <arpa/inet.h>
+#include <cpr/cpr.h>
+#include <boost/asio.hpp>
+#include <boost/asio/signal_set.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/program_options.hpp>
+#include <boost/uuid/uuid.hpp>
+#include "libCrypto/Sha2.h"
+#include "libMetrics/Tracing.h"
+#include "libNetwork/P2P.h"
+#include "libUtils/DetachedFunction.h"
+#include "libUtils/HardwareSpecification.h"
+#include "libUtils/IPConverter.h"
+#include "libUtils/Logger.h"
+#include "libZilliqa/Zilliqa.h"
+
+using namespace zil::p2p;
+std::chrono::high_resolution_clock::time_point startTime;
+
+#define SUCCESS 0
+#define ERROR_IN_COMMAND_LINE -1
+#define ERROR_HARDWARE_SPEC_MISMATCH_EXCEPTION -2
+#define ERROR_UNHANDLED_EXCEPTION -3
+#define ERROR_IN_CONSTANTS -4
+
+using VectorOfPeer = std::vector<Peer>;
+namespace beast = boost::beast;
+namespace http = beast::http;
+
+namespace zil::multiplier::utils {
+
+std::vector<std::string> split(const std::string& s, char delimiter) {
+  std::vector<std::string> tokens;
+  std::string token;
+  std::istringstream tokenStream(s);
+  while (std::getline(tokenStream, token, delimiter)) {
+    tokens.push_back(token);
+  }
+  return tokens;
+}
+
+std::vector<std::string> removeEmptyAddr(
+    const std::vector<std::string>& addresses) {
+  std::vector<std::string> result;
+  for (const std::string& address : addresses) {
+    if (!address.empty()) {
+      result.push_back(address);
+    }
+  }
+  return result;
+}
+
+std::set<std::string> reportDifference(
+    const std::vector<std::string>& newAddresses,
+    const std::vector<std::string>& oldAddresses,
+    const std::set<std::string>& addressStore) {
+  std::set<std::string> difference;
+  for (const std::string& address : newAddresses) {
+    if (std::find(oldAddresses.begin(), oldAddresses.end(), address) ==
+            oldAddresses.end() &&
+        addressStore.find(address) == addressStore.end()) {
+      difference.insert(address);
+    }
+  }
+  return difference;
+}
+
+bool fetchDownstreams(const std::string downstreamURL,
+                      std::vector<std::string>& mirrorAddresses,
+                      std::set<std::string>& addressStore) {
+  std::cout << "Requesting [" << downstreamURL << "]" << std::endl;
+  cpr::Response r = cpr::Get(cpr::Url{downstreamURL});
+
+  if (r.status_code == 200) {
+    std::cout << "Success: " << r.status_code << std::endl;
+    std::cout << "Header Type: " << r.header["content-type"]
+              << std::endl;                                 // application/json;
+                                                            // charset=utf-8
+    std::cout << "Text : [" << r.text << "]" << std::endl;  // JSON text string
+  } else {
+    std::cout << "Error: " << r.status_code << std::endl;
+    return false;
+  };
+  // Process the response
+  if (r.status_code == 200) {
+    std::string contents = r.text;
+    std::vector<std::string> oldAddresses = mirrorAddresses;
+    std::vector<std::string> newAddresses =
+        removeEmptyAddr(split(contents, '\n'));
+    std::set<std::string> diffAddresses =
+        reportDifference(newAddresses, oldAddresses, addressStore);
+  } else {
+    std::cout << "DownstreamURL " << downstreamURL
+              << " may not be available at this moment" << std::endl;
+  }
+  return true;
+}
+
+};  // namespace zil::multiplier::utils
+
+class registeredPeers {
+ public:
+  const VectorOfPeer& getPeers() { return m_peers; }
+  void setPeers(std::vector<Peer> newPeers) { m_peers = newPeers; }
+  void addPeer(Peer newPeer) { m_peers.push_back(newPeer); }
+  void removePeer(Peer oldPeer) {
+    for (size_t i = 0; i < m_peers.size(); i++) {
+      if (m_peers[i].m_ipAddress == oldPeer.m_ipAddress &&
+          m_peers[i].m_listenPortHost == oldPeer.m_listenPortHost) {
+        m_peers.erase(m_peers.begin() + i);
+      }
+    }
+  }
+  void removePeer(int index) { m_peers.erase(m_peers.begin() + index); }
+  void clearPeers() { m_peers.clear(); }
+  int size() { return m_peers.size(); }
+  Peer getPeer(int index) { return m_peers[index]; }
+  void setPeer(int index, Peer newPeer) { m_peers[index] = newPeer; }
+  void printPeers() {
+    for (size_t i = 0; i < m_peers.size(); i++) {
+      std::cout << "Peer " << i << ": " << m_peers[i].m_ipAddress << std::endl;
+    }
+  }
+  std::vector<Peer> m_peers;
+};
+
+void process_message(std::shared_ptr<zil::p2p::Message> message,
+                     registeredPeers& peers) {
+  LOG_MARKER();
+
+  if (message->msg.size() < 10) {
+    LOG_GENERAL(INFO, "Received message '"
+                          << (char*)&message->msg.at(0) << "' at port "
+                          << message->from.m_listenPortHost << " from address "
+                          << message->from.m_ipAddress);
+  } else {
+    std::chrono::duration<double, std::milli> time_span =
+        std::chrono::high_resolution_clock::now() - startTime;
+    LOG_GENERAL(INFO, "Received " << message->msg.size() / (1024 * 1024)
+                                  << " MB message in " << time_span.count()
+                                  << " ms");
+    LOG_GENERAL(INFO, "Benchmark: " << (1000 * message->msg.size()) /
+                                           (time_span.count() * 1024 * 1024)
+                                    << " MBps");
+  }
+  zil::p2p::GetInstance().SendMessage(peers.getPeers(), message->msg,
+                                      message->startByte, false);
+}
+
+namespace po = boost::program_options;
+
+int main(int argc, char* argv[]) {
+  using namespace zil::multiplier::utils;
+  Peer my_network_info;
+
+  std::string url;
+  std::string logpath(std::filesystem::absolute("./").string());
+  uint128_t ip;
+  registeredPeers our_peers{};
+  std::set<std::string> addressStore;
+  std::vector<std::string> mirrorAddresses;
+  int port;
+
+  po::options_description desc("Options");
+  desc.add_options()("help,h", "Print help messages")(
+      "listen,l", po::value<int>(&port)->required()->default_value(30300),
+      "Specifies port to bind to")(
+      "logpath,g", po::value<std::string>(&logpath),
+      "customized log path, could be relative path (e.g., \"./logs/\"), or "
+      "absolute path (e.g., \"/usr/local/test/logs/\")")(
+      "url,s", po::value<std::string>(&url)->required(),
+      "url of list of nodes to poll for connections")(
+      "version,v", "Displays the Zilliqa Multiplier version information");
+
+  po::variables_map vm;
+
+  try {
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+
+    /** --help option
+     */
+    if (vm.count("help")) {
+      SWInfo::LogBrandBugReport();
+      std::cout << desc << std::endl;
+      return SUCCESS;
+    }
+
+    if (vm.count("version")) {
+      std::cout << VERSION_TAG << std::endl;
+      return SUCCESS;
+    }
+
+    po::notify(vm);
+
+    if ((port < 0) || (port > 65535)) {
+      SWInfo::LogBrandBugReport();
+      std::cerr << "Invalid or missing port number" << std::endl;
+      return ERROR_IN_COMMAND_LINE;
+    }
+    if (url.empty()) {
+      SWInfo::LogBrandBugReport();
+      std::cerr << "Invalid or missing url" << std::endl;
+      return ERROR_IN_COMMAND_LINE;
+    }
+  } catch (boost::program_options::required_option& e) {
+    SWInfo::LogBrandBugReport();
+    std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+    std::cout << desc;
+    return ERROR_IN_COMMAND_LINE;
+  } catch (boost::program_options::error& e) {
+    SWInfo::LogBrandBugReport();
+    std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+    return ERROR_IN_COMMAND_LINE;
+  }
+
+  INIT_FILE_LOGGER("asio_multiplier", std::filesystem::current_path());
+  LOG_DISPLAY_LEVEL_ABOVE(INFO);
+
+  auto func = [port, &our_peers]() mutable -> void {
+    boost::asio::io_context ctx(1);
+    boost::asio::signal_set sig(ctx, SIGINT, SIGTERM);
+    sig.async_wait([&](const boost::system::error_code&, int) { ctx.stop(); });
+
+    auto dispatcher = [&our_peers](std::shared_ptr<zil::p2p::Message> message) {
+      process_message(std::move(message), our_peers);
+    };
+
+    zil::p2p::GetInstance().StartServer(ctx, port, 0, std::move(dispatcher));
+    ctx.run();
+  };
+
+  //DetachedFunction(1, func);
+
+  // Add signal handlers etc.
+
+  while (1) {
+    if (false && fetchDownstreams(url, mirrorAddresses, addressStore)) {
+      for (const std::string& address : mirrorAddresses) {
+        std::vector<std::string> address_pair;
+        address_pair = split(address, ':');
+        if (address_pair.size() != 2) {
+          std::cout << "Invalid address: " << address << std::endl;
+          continue;
+        }
+
+        if (addressStore.find(address) == addressStore.end()) {
+          std::cout << "Adding " << address << " to addressStore" << std::endl;
+          addressStore.insert(address);
+          std::cout << "Adding " << address << " to our_peers" << std::endl;
+          struct in_addr ip_addr {};
+          inet_pton(AF_INET, address_pair[0].c_str(), &ip_addr);
+          our_peers.addPeer({ip_addr.s_addr, static_cast<u_int32_t >(std::stoi(address_pair[1]))});
+        }
+      }
+    }
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+  }
+  return 0;
+}
