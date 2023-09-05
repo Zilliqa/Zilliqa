0209f5e2a68881d39b0732c740ee015185ef7499
Chetan Phirke <chetan@zilliqa.com>
2023-09-05 14:51:40 +0530

    Revert "Fix codehash to get only contract code hash (#3744) (#3747)"
    This reverts commit 1e715f5c3c4b3f0afb37758113adba22140ac612.

--- src/libPersistence/ContractStorage.cpp
+++ src/libPersistence/ContractStorage.cpp
@@ -432,12 +432,9 @@ bool ContractStorage::FetchExternalStateValue(
       type = "ByStr20";
     }
   } else if (query.name() == "_codehash") {
-    dev::h256 codeHash;
-    if (account->GetContractCodeHash(codeHash)) {
-      special_query = "\"0x" + codeHash.hex() + "\"";
-      type = "ByStr32";
-    } else
-      return false;
+    dev::h256 codeHash = account->GetCodeHash();
+    special_query = "\"0x" + codeHash.hex() + "\"";
+    type = "ByStr32";
   } else if (query.name() == "_code") {
     // Get the code directly from the account storage.
     zbytes code = account->GetCode();
