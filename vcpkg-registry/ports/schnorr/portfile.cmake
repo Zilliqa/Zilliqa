5794eac22463f2b4d436a6e93156dab1955cfd95
Yaron <yaron@zilliqa.com>
2023-09-07 16:28:52 +0700

    Update some ports to use OpenSSL 3.

--- vcpkg-registry/ports/schnorr/portfile.cmake
+++ vcpkg-registry/ports/schnorr/portfile.cmake
@@ -4,20 +4,17 @@ vcpkg_from_github(
     REF c54f4cadc88234d58bfdf83a4d7348444ea7845d #v8.2.0
     SHA512 7f445c407fd1049ab41ad580b91263698af405b6015ec4a33715a40e488459afb5f2a8f06e6db9759b18b4f9ba443e2da746408821931ccd31e4caf4101048a7
     HEAD_REF master
+    PATCHES
+      fix-compiler-flags.patch
 )
 
-if (UNIX AND NOT APPLE)
-  vcpkg_cmake_configure(
-      SOURCE_PATH ${SOURCE_PATH}
-      OPTIONS
-        -DCMAKE_CXX_FLAGS=-Wno-dev
-        -DCMAKE_C_FLAGS=-Wno-dev
-  )
-else()
-  vcpkg_cmake_configure(
-      SOURCE_PATH ${SOURCE_PATH}
-  )
-endif()
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+      -DCMAKE_CXX_STANDARD=20
+      -DCMAKE_CXX_FLAGS="-Wall -Werror -Wextra -Wno-dev -Wno-deprecated-declarations"
+      -DCMAKE_C_FLAGS="-Wall -Werror -Wextra -Wno-dev -Wno-deprecated-declarations"
+)
 
 vcpkg_cmake_install()
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
