aaa3953c62d8ff37cb31b3d4035a663fb1e0e04e
Saeed Dadkhah <saeed@zilliqa.com>
2023-09-14 10:17:04 +0330

    Disable parallel on contract deployment tests

--- tests/EvmAcceptanceTests/test/contract-deployment/ContractDeploymentEthers.ts
+++ tests/EvmAcceptanceTests/test/contract-deployment/ContractDeploymentEthers.ts
@@ -3,7 +3,7 @@ import hre, {ethers} from "hardhat";
 import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
 import { Contract } from "ethers";
 
-describe("Contract Deployment using Ethers.js #parallel", function () {
+describe("Contract Deployment using Ethers.js", function () {
   describe("Contract with zero parameter constructor", function () {
     let signer: SignerWithAddress;
     let nonceBeforeDeploy: number;
