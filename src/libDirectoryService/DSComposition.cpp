a83959a40d9857921bcddc2fecd0041a8a480e6a
bzawisto <bartosz@zilliqa.com>
2023-10-12 09:44:24 +0200

    Overflow fix

--- src/libDirectoryService/DSComposition.cpp
+++ src/libDirectoryService/DSComposition.cpp
@@ -127,10 +127,10 @@ void UpdateDSCommitteeCompositionCore(const PubKey& selfKeyPub,
 
   // Remove that many members so that new dsComm.size() == COMM_SIZE
   const auto toRemoveCount =
-      dsComm.size() - COMM_SIZE ? dsComm.size() - COMM_SIZE : 0;
+      dsComm.size() > COMM_SIZE ? dsComm.size() - COMM_SIZE : 0;
   LOG_GENERAL(INFO,
-              "Since new dsComm is larger than expected COMM_SIZE, number of "
-              "nodes to be trimmed: "
+              "Since new dsComm might not equal expected COMM_SIZE, number of "
+              "nodes to be trimmed is: "
                   << toRemoveCount);
   for (uint32_t i = 0; i < toRemoveCount; ++i) {
     // One item is always removed every winner, with removal priority given to
