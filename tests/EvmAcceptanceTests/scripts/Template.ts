1974a1762a048cbf4c91640fe9f89356c8afa6d8
Saeed Dadkhah <saeed@zilliqa.com>
2023-10-16 13:44:24 +0330

    run linter

--- tests/EvmAcceptanceTests/scripts/Template.ts
+++ tests/EvmAcceptanceTests/scripts/Template.ts
@@ -1,19 +1,18 @@
-import { TransactionRequest } from "@ethersproject/providers";
-import { Contract, Transaction } from "ethers";
-import { ethers } from "hardhat";
+import {TransactionRequest} from "@ethersproject/providers";
+import {Contract, Transaction} from "ethers";
+import {ethers} from "hardhat";
 
 async function deployContract(contractName: string): Promise<Contract> {
   const Contract = await ethers.getContractFactory(contractName);
   const contract = await Contract.deploy();
-  await contract.deployed(); 
+  await contract.deployed();
   return contract;
 }
 
 async function sendTransaction(tx: TransactionRequest): Promise<Transaction> {
   const [signer] = await ethers.getSigners();
-  return signer.sendTransaction(tx);  
+  return signer.sendTransaction(tx);
 }
-
 
 async function main() {
   /*
@@ -21,7 +20,7 @@ async function main() {
    * 2. Put your code inside this main function. Use `deployContract` or `sendTransaction` helper functions
    * 3. Run it like: `npx hardhat run scripts/MyScript.ts`
    */
-  console.log("IT WORKS!")
+  console.log("IT WORKS!");
 }
 
 main()
