ee0fd591ffab9b6327f2f71501e4a4aac5969b46
Saeed Dadkhah <saeed@zilliqa.com>
2023-10-18 15:27:45 +0330

    linting

--- tests/EvmAcceptanceTests/tasks/SetRewards.ts
+++ tests/EvmAcceptanceTests/tasks/SetRewards.ts
@@ -7,13 +7,12 @@ import {Account} from "@zilliqa-js/account";
 import clc from "cli-color";
 import {ethers} from "ethers";
 import Long from "long";
-const { task } = require('hardhat/config');
-
+const {task} = require("hardhat/config");
 
 const MSG_VERSION = 1; // current msgVersion
 //const VERSION = bytes.pack(hre.getZilliqaChainId(), msgVersion);
 const DEPLOYER_PRIVATE_KEY = "254d9924fc1dcdca44ce92d80255c6a0bb690f867abde80e626fbfef4d357005";
-const WELL_KNOWN_CONTRACT_ADDRESS = "0xb73DA094d60aa93AC4fA8Ae41Df5d8c13925b0BD".toLowerCase(); 
+const WELL_KNOWN_CONTRACT_ADDRESS = "0xb73DA094d60aa93AC4fA8Ae41Df5d8c13925b0BD".toLowerCase();
 
 // NB: This test uses a hardwired key. It does this because the address of the contract is hardwired into
 // the constants.xml file, and thus the nonce and identity of the deployer have to be known statically.
@@ -23,18 +22,18 @@ interface ContractContext {
   zilliqa: Zilliqa;
   version: String;
   hre: HardhatRuntimeEnvironment;
-  deployerAccount: Account
-};
+  deployerAccount: Account;
+}
 
 async function ensureContractDeployment(hre): ContractContext {
-    // This is just one of the isolated server pubkeys incremented by 1.
-    const funderPrivateKey = process.env.PRIMARY_ACCOUNT;
-    if (funderPrivateKey == undefined) {
-      throw Error(clc.bold.red("Please set PRIMARY_ACCOUNT environment variable before running this script."));
-    }
-  const zilliqa = new Zilliqa(hre.getNetworkUrl())
+  // This is just one of the isolated server pubkeys incremented by 1.
+  const funderPrivateKey = process.env.PRIMARY_ACCOUNT;
+  if (funderPrivateKey == undefined) {
+    throw Error(clc.bold.red("Please set PRIMARY_ACCOUNT environment variable before running this script."));
+  }
+  const zilliqa = new Zilliqa(hre.getNetworkUrl());
   const version = bytes.pack(hre.getZilliqaChainId(), MSG_VERSION);
-  initZilliqa(hre.getNetworkUrl(), hre.getZilliqaChainId(), [ funderPrivateKey ], 30);
+  initZilliqa(hre.getNetworkUrl(), hre.getZilliqaChainId(), [funderPrivateKey], 30);
   const funderAddress = getAddressFromPrivateKey(funderPrivateKey);
   const deployerAddress = getAddressFromPrivateKey(DEPLOYER_PRIVATE_KEY);
   console.log(`Funding from: ${funderAddress}`);
@@ -43,19 +42,23 @@ async function ensureContractDeployment(hre): ContractContext {
   // Let's check for the (known) contract address.
   const contractBalanceResult = await zilliqa.blockchain.getBalance(WELL_KNOWN_CONTRACT_ADDRESS);
   if (!contractBalanceResult.error) {
-    console.log(clc.green("Contract already deployed. Yay!"))
+    console.log(clc.green("Contract already deployed. Yay!"));
     const contract = await hre.interactWithScillaContract(WELL_KNOWN_CONTRACT_ADDRESS);
-    return { contract, deployerAddress, zilliqa, version, hre, deployerAccount };
+    return {contract, deployerAddress, zilliqa, version, hre, deployerAccount};
   }
 
   console.log(`Contract not present; deploying .. `);
   let deployerNeedsFunding = true;
   {
-    const deployerBalance  = await zilliqa.blockchain.getBalance(deployerAddress);
+    const deployerBalance = await zilliqa.blockchain.getBalance(deployerAddress);
     if (!deployerBalance.error) {
       // console.log(`Deployer nonce is ${deployerBalance.result.nonce}`);
       if (deployerBalance.result.nonce >= 1) {
-        console.log(`Deployer nonce is ${clc.bold.red(deployerBalance.result.nonce)} - cannot deploy known contract. Try again with a new chain`);
+        console.log(
+          `Deployer nonce is ${clc.bold.red(
+            deployerBalance.result.nonce
+          )} - cannot deploy known contract. Try again with a new chain`
+        );
         throw Error("Contract already deployed");
       }
       let bal = new BN(deployerBalance.result.balance);
@@ -81,15 +84,17 @@ async function ensureContractDeployment(hre): ContractContext {
   if (deployerNeedsFunding) {
     console.log(`Funding .. `);
     const tx = await zilliqa.blockchain.createTransactionWithoutConfirm(
-    zilliqa.transactions.new(
-      { version,
-        toAddr: deployerAddress,
-        amount: new BN("1_000_000_000_00"),
-        gasPrice: new BN(2000000000), // Minimum gasPrice veries. Check the `GetMinimumGasPrice` on the blockchain
-        gasLimit: Long.fromNumber(2100)
-      },
-      false
-    ));
+      zilliqa.transactions.new(
+        {
+          version,
+          toAddr: deployerAddress,
+          amount: new BN("1_000_000_000_00"),
+          gasPrice: new BN(2000000000), // Minimum gasPrice veries. Check the `GetMinimumGasPrice` on the blockchain
+          gasLimit: Long.fromNumber(2100)
+        },
+        false
+      )
+    );
     if (tx.id) {
       const confirmedTxn = await tx.confirm(tx.id);
       const receipt = confirmedTxn.getReceipt();
@@ -107,17 +112,16 @@ async function ensureContractDeployment(hre): ContractContext {
   let contract = await hre.deployScillaContract("RewardsParams", deployerAddress);
   console.log(`Result ${JSON.stringify(contract)}`);
   console.log(`Contract deployed at ${contract.address}`);
-  return { contract, deployerAddress, zilliqa, version, hre, deployerAccount };
-};
+  return {contract, deployerAddress, zilliqa, version, hre, deployerAccount};
+}
 
 async function checkContractDeployment() {
-  const contractBalance  = await zilliqa.blockchain.getBalance(WELL_KNOWN_CONTRACT_ADDRESS);
+  const contractBalance = await zilliqa.blockchain.getBalance(WELL_KNOWN_CONTRACT_ADDRESS);
   console.log(`Bal ${JSON.stringify(contractBalance)}`);
   if (contractBalance.error !== undefined) {
     throw Error(`Control contract not deployed! ${JSON.stringify(contractBalance)}`);
   }
 }
-
 
 async function adjustRewards(context: ContractContext) {
   let promises = [];
@@ -141,7 +145,7 @@ async function main(hre: HardhatRuntimeEnvironment) {
   await adjustRewards(context);
 }
 
-task('set-rewards', 'Sets the chain reward parameters')
+task("set-rewards", "Sets the chain reward parameters")
   .addOptionalParam("coinbaserewardperds", "Set the coinbase reward to .. ")
   .addOptionalParam("baserewardscaledpercent", "Set the base reward to (DANGER! Check the percent_precision first!)")
   .addOptionalParam("lookuprewardscaledpercent", "Set the lookup (SSN) reward (DANGER! Check percent_precision first!)")
@@ -183,6 +187,4 @@ task('set-rewards', 'Sets the chain reward parameters')
       let result = await connected.ChangeBaseRewardMulInMillis(newReward);
       console.log(clc.green(`${JSON.stringify(result)}`));
     }
-    
   });
-
