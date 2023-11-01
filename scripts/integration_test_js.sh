6b85e7e4b4de7ab131a8b3397a885f50d024003e
Saeed Dadkhah <saeed@zilliqa.com>
2023-11-01 11:02:52 +0330

    Install through nvm

--- scripts/integration_test_js.sh
+++ scripts/integration_test_js.sh
@@ -50,7 +50,8 @@ else
 
     # Install nodejs
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-    source ~/.bashrc
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
     if ! command -v nvm &> /dev/null
     then
