8f442d21b0fb86c1361b496158c1517b9af5b518
Saeed Dadkhah <saeed@zilliqa.com>
2023-11-02 11:49:15 +0330

    Update setup task

--- tests/EvmAcceptanceTests/tasks/Setup.ts
+++ tests/EvmAcceptanceTests/tasks/Setup.ts
@@ -1,12 +1,18 @@
 import clc from "cli-color";
 import {task} from "hardhat/config";
 import {terminal} from "terminal-kit";
-import {getEthAddress, getEthBalance, getZilAddress, getZilBalance} from "./helpers";
+import {getEthAddress, getEthBalance, getZilBalance} from "../helpers/SignersHelper";
 import {BN} from "@zilliqa-js/zilliqa";
 
 enum AddressType {
   EvmBased,
   ZilBased
+}
+
+enum CreationStrategy{
+  CreateNewSigners = 0,   // Replace previous one
+  AppendToPreviousSigners = 1,
+  RefundPreviousSigners = 2
 }
 
 const displayTitle = (title: string) => {
@@ -25,13 +31,16 @@ const askAddressType = async (): Promise<AddressType | undefined> => {
   return out.canceled ? undefined : out.selectedIndex;
 };
 
-const askAppendNewSignersToFile = async (): Promise<boolean | undefined> => {
+const askWhatYouWantToDo = async (): Promise<CreationStrategy | undefined> => {
   console.clear();
-  var items = ["Create a new signers file(Replaces the previous ones)", "Append new signers to the previous ones"];
+  var items = ["Create a set of signers (Replaces the previous ones)",
+               "Append a set of new signers to the previous ones",
+               "Refund existing signers"];
 
-  displayTitle("Create a new signers file?");
+  displayTitle("What do you want to do?");
   let out = await terminal.singleColumnMenu(items, {cancelable: true}).promise;
-  return out.canceled ? undefined : out.selectedIndex === 1;
+
+  return out.canceled ? undefined : out.selectedIndex as CreationStrategy;
 };
 
 const askPrivateKey = async (): Promise<string | undefined> => {
@@ -68,6 +77,12 @@ const askBalance = async (): Promise<number | undefined> => {
 };
 
 task("setup", "A task to setup test suite").setAction(async (taskArgs, hre) => {
+  const strategy = await askWhatYouWantToDo();
+
+  if (strategy === undefined) {
+    return;
+  }
+
   const address_type = await askAddressType();
   if (address_type === undefined) {
     return;
@@ -79,10 +94,15 @@ task("setup", "A task to setup test suite").setAction(async (taskArgs, hre) => {
     return;
   }
 
-  const signersCount = await askSignersCount();
+  let signersCount = 0;
+  if (strategy !== CreationStrategy.RefundPreviousSigners) {
+    const count = await askSignersCount();
+  
+    if (count === undefined) {
+      return;
+    }
 
-  if (signersCount === undefined) {
-    return;
+    signersCount = count;
   }
 
   const eachSignerBalance = await askBalance();
@@ -91,15 +111,10 @@ task("setup", "A task to setup test suite").setAction(async (taskArgs, hre) => {
     return;
   }
 
-  const append = await askAppendNewSignersToFile();
-
-  if (append === undefined) {
-    return;
-  }
-
+  let neededBalance = 0;
   if (address_type == AddressType.EvmBased) {
     // *2 because we both fund zil/eth address.
-    const neededBalance = eachSignerBalance * 2 * signersCount;
+    neededBalance = eachSignerBalance * 2 * signersCount;
     const [address, balance] = await getEthBalance(hre, private_key);
     if (balance.isZero()) {
       console.log(
@@ -107,10 +122,9 @@ task("setup", "A task to setup test suite").setAction(async (taskArgs, hre) => {
       );
       return;
     }
-    await hre.run("init-signers", {from: private_key, count: "30", balance: eachSignerBalance.toString(), append});
   } else if (address_type == AddressType.ZilBased) {
     // *2 because we both fund zil/eth address. +1 to fund the private key eth-address as well.
-    const neededBalance = eachSignerBalance * 2 * (signersCount + 1);
+    neededBalance = eachSignerBalance * 2 * (signersCount + 1);
 
     const [address, balance] = await getZilBalance(hre, private_key);
     if (balance.lt(new BN(neededBalance))) {
@@ -127,13 +141,12 @@ task("setup", "A task to setup test suite").setAction(async (taskArgs, hre) => {
       amount: neededBalance.toString(),
       fromAddressType: "zil"
     });
+  }
 
-    // Then fund them in parallel
-    await hre.run("init-signers", {
-      from: private_key,
-      count: signersCount.toString(),
-      balance: eachSignerBalance.toString(),
-      append
-    });
+  if (strategy === CreationStrategy.RefundPreviousSigners) {
+    await hre.run("refund-signers", {from: private_key, amount: eachSignerBalance.toString()});
+  } else {
+    const append = strategy === CreationStrategy.AppendToPreviousSigners;
+    await hre.run("init-signers", {from: private_key, count: "30", balance: eachSignerBalance.toString(), append});
   }
 });
