f4ac44a8d028b2eac279d896d8966a4e590502c4
bzawisto <bartosz@zilliqa.com>
2023-12-01 11:50:47 +0100

    some experiment

--- src/cmd/buildTxBlockHashesToNums.cpp
+++ src/cmd/buildTxBlockHashesToNums.cpp
@@ -22,47 +22,24 @@
 #include <libBlockchain/TxBlock.h>
 
 int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " PERSISTENCE_PATH" << std::endl;
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " PERSISTENCE_PATH" << " DB_NAME" << std::endl;
     exit(1);
   }
   const std::string persistencePath = argv[1];
+  const std::string dbName = argv[2];
   const std::string EMPTY_SUBDIR{};
 
-  const std::string dbCommonSubPath = persistencePath + "/";
-
   // Pass explicitly subdir as an empty std::string type to invoke proper ctor
-  LevelDB txBlockchainDB{"txBlocks", persistencePath, EMPTY_SUBDIR};
-  LevelDB txBlockchainAuxDB{"txBlocksAux", persistencePath, EMPTY_SUBDIR};
-  LevelDB txBlockHashToNumDB{"txBlockHashToNum", persistencePath, EMPTY_SUBDIR};
+  LevelDB stateDelta{dbName, persistencePath, EMPTY_SUBDIR};
 
-  // Algo is as follows:
-  // MaxBlockNum = 0
-  // For each (blockNum, block) in txBlocks:
-  //   Insert (block->GetBlockHash(), blockNum) into TxBlockHashToNum
-  //   MaxBlockNum = max(MaxBlockNum, blockNum)
-  // Insert ('MaxTxBlockNumber', MaxBlockNum) into txBlocksAux
-  //
-
-  uint64_t maxKnownBlockNum = 0;
-
-  std::cerr << "Starting main loop" << std::endl;
-
-  const auto it = std::unique_ptr<leveldb::Iterator>(
-      txBlockchainDB.GetDB()->NewIterator(leveldb::ReadOptions()));
-  for (it->SeekToFirst(); it->Valid(); it->Next()) {
-    const uint64_t blockNum = std::stoull(it->key().ToString());
-    const auto blockString = txBlockchainDB.Lookup(blockNum);
-    TxBlock block;
-    block.Deserialize(zbytes(blockString.begin(), blockString.end()), 0);
-    txBlockHashToNumDB.Insert(block.GetBlockHash(), std::to_string(blockNum));
-    maxKnownBlockNum = std::max(maxKnownBlockNum, blockNum);
+  if(stateDelta.GetDB() == nullptr) {
+    std::cerr << "Unable to open provided db..." << std::endl;
+    exit(1);
   }
+  std::cerr << "Starting compact action on db: " <<  dbName << std::endl;
 
-  std::cerr << "Greatest block number found: " << maxKnownBlockNum << std::endl;
-
-  txBlockchainAuxDB.Insert(leveldb::Slice(MAX_TX_BLOCK_NUM_KEY),
-                           leveldb::Slice(std::to_string(maxKnownBlockNum)));
+  stateDelta.GetDB()->CompactRange(nullptr, nullptr);
 
   return 0;
 }
