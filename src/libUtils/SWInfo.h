800c1c500954307c0787740bac1fbf8846c020d2
Chetan Phirke <chetan@zilliqa.com>
2023-12-06 15:39:49 +0530

    Update version to v9.3.0rc16

--- src/libUtils/SWInfo.h
+++ src/libUtils/SWInfo.h
@@ -22,7 +22,7 @@
 #include <iostream>
 #include "common/Serializable.h"
 
-const std::string VERSION_TAG = "v9.3.0rc15";
+const std::string VERSION_TAG = "v9.3.0rc16";
 const std::string ZILLIQA_BRAND = "Copyright (C) Zilliqa. Version " +
                                   VERSION_TAG + ".  <https://www.zilliqa.com/>";
 
