0e45d5d3fb2054b8b4782e7a9925a12491948245
Richard Watts <108257153+rrw-zilliqa@users.noreply.github.com>
2023-12-11 10:16:41 +0000

    [master] ZIL-5488: acceptance test fixes (#3889)
    * (feat) Define network in environment variables
    (fix)  Transfer enough from a zil account to account for gas fees on the called contract.
    (fix)  Set the default wallet address in Transfer.ts so we don't end up accidentally using a different one.
    
    * (fix) Write the filename we're expecting when CHAIN_NAME is set. Grotty, but what about this isn't?
    
    * (fix) Disable tests that won't pass anyway.
    
    * (fix) ZIL-5488: Fix whitespace. Swearing elided.
    (fix) ZIL-5488: Remove spurious console.log
    
    * (feat) ZIL-5488: Add docs on env var use to README

--- tests/EvmAcceptanceTests/helpers/parallel-tests/SignerPool.ts
+++ tests/EvmAcceptanceTests/helpers/parallel-tests/SignerPool.ts
@@ -50,12 +50,12 @@ export default class SignerPool {
 }
 
 function shuffleArray(array: any[]) {
-    for (let i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
+  for (let i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
 
-    return array;
+  return array;
 }
