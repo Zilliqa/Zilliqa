b08050b3c1ec039d2b644ac56fe073443602bade
Chetan Phirke <chetan@zilliqa.com>
2024-05-31 15:50:11 +0800

    update

--- scripts/auto_otterDb_backup.py
+++ scripts/auto_otterDb_backup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (C) 2019 Zilliqa
+# Copyright (C) 2024 Zilliqa
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
