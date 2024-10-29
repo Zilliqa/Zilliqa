502b27e8b992090adaca91518d484d1aac419bde
Chetan Phirke <chetan@zilliqa.com>
2024-10-29 08:12:21 +0800

    Update TXMETADATADEPTH to 72000 blocks

--- src/libEth/filters/APICache.cpp
+++ src/libEth/filters/APICache.cpp
@@ -28,7 +28,7 @@ namespace filters {
 namespace {
 
 // TODO this is to be a parameter from constants.xml
-const size_t TXMETADATADEPTH = 17150;  // ~7 days
+const size_t TXMETADATADEPTH = 72000;  // ~7 days
 
 }  // namespace
 
